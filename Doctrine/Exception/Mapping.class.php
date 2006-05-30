19ac1e7961760700624664c13886a11924a8c4b4
doctrine <doctrine@625475ce-881a-0410-a577-b389adb331d8>
2006-05-30 08:59:33 +0000

    Added a folder remotely

---
+++ Doctrine/Exception/Mapping.class.php
@@ -0,0 +1,11 @@
+<?php
+Doctrine::autoload("Doctrine_Exception");
+/**
+ * thrown when user tries to get a foreign key object but the mapping is not done right
+ */
+class Doctrine_Mapping_Exception extends Doctrine_Exception { 
+    public function __construct($message = "An error occured in the mapping logic.") {
+        parent::__construct($message,Doctrine::ERR_MAPPING);
+    }
+}
+?>
---
+++ Doctrine/Exception/PrimaryKey.class.php
@@ -0,0 +1,11 @@
+<?php
+Doctrine::autoload("Doctrine_Exception");
+/**
+ * thrown when Doctrine_Record is loaded and there is no primary key field
+ */
+class Doctrine_PrimaryKey_Exception extends Doctrine_Exception {
+    public function __construct() {
+        parent::__construct("No primary key column found. Each data set must have primary key column.", Doctrine::ERR_NO_PK);
+    }
+}
+?>
