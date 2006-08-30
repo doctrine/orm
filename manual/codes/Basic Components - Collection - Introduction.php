3b8b1a4e88e5c7fce80f2fdd167d87fe784f6168
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-08-30 22:43:11 +0000

    Added connection & collection introductions + first code examples for Doctrine_View

--- manual/codes/Basic Components - Collection - Introduction.php
+++ manual/codes/Basic Components - Collection - Introduction.php
@@ -1,9 +1,18 @@
 <?php
-$coll = new Doctrine_Collection('User');
+$conn = Doctrine_Manager::getInstance()
+    ->openConnection(new PDO("dsn", "username", "pw"));
 
+// initalizing a new collection
+$users = new Doctrine_Collection($conn->getTable('User'));
+
+// alternative (propably easier)
+$users = new Doctrine_Collection('User');
+
+// adding some data
 $coll[0]->name = 'Arnold';
 
 $coll[1]->name = 'Somebody';
 
+// finally save it!
 $coll->save();
 ?>
