77a69bc57eb6fb1b66fb76eafd6318f06ae4e295
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-08-30 22:40:03 +0000

    Docs for autoincrement and composite identifiers

---
+++ manual/docs/Getting started - Record identifiers - Autoincremented.php
@@ -0,0 +1,2 @@
+Autoincrement primary key is the most basic identifier and its usage is strongly encouraged. Sometimes you may want to use some other name than 'id'
+for your autoinc primary key. It can be specified as follows:
