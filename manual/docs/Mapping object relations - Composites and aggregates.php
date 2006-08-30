186b4465ad6f869a84a0d3b55d57422b4b5ef317
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-08-30 11:23:22 +0000

    Docs updated

--- manual/docs/Mapping object relations - Composites and aggregates.php
+++ manual/docs/Mapping object relations - Composites and aggregates.php
@@ -1,8 +1,9 @@
-Doctrine supports aggregates and composites. When binding composites you can use methods Doctrine_Table::ownsOne() and Doctrine_Table::ownsMany(). When binding
-aggregates you can use methods Doctrine_Table::hasOne() and Doctrine_Table::hasMany().
+Doctrine supports aggregates and composites. When binding composites you can use methods Doctrine_Record::ownsOne() and Doctrine_Record::ownsMany(). When binding
+aggregates you can use methods Doctrine_Record::hasOne() and Doctrine_Record::hasMany(). Basically using the owns* methods is like adding a database level ON CASCADE DELETE
+constraint on related component with an exception that doctrine handles the deletion in application level.
 <br \><br \>
-In Doctrine if you bind an Email to a User using ownsOne or ownsMany methods, everytime User record calls save or delete the associated
-Email record is also saved/deleted.
+In Doctrine if you bind an Email to a User using ownsOne or ownsMany methods, everytime User record calls delete the associated
+Email record is also deleted.
 <br \><br \>
-Then again if you bind an Email to a User using hasOne or hasMany methods, everytime User record calls save or delete the associated
-Email record is NOT saved/deleted.
+Then again if you bind an Email to a User using hasOne or hasMany methods, everytime User record calls delete the associated
+Email record is NOT deleted.
