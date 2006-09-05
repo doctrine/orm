946e3f1c5d3aede3799350581dba601b2c3424f0
amadeus <amadeus@625475ce-881a-0410-a577-b389adb331d8>
2006-09-05 20:53:44 +0000

    Fixed to prevent custom message from being overidden by default

--- Doctrine/Table/Exception.php
+++ Doctrine/Table/Exception.php
@@ -1,14 +1,14 @@
 <?php
 Doctrine::autoload('Doctrine_Exception');
 /**
- * thrown when user tries to initialize a new instance of Doctrine_Table, 
+ * thrown when user tries to initialize a new instance of Doctrine_Table,
  * while there already exists an instance of that table
  */
 class Doctrine_Table_Exception extends Doctrine_Exception {
-    public function __construct() {
-        parent::__construct("Couldn't initialize table. One instance of this
+    public function __construct($message = "Couldn't initialize table. One instance of this
                             table already exists. Always use Doctrine_Session::getTable(\$name)
-                            to get on instance of a Doctrine_Table.",Doctrine::ERR_TABLE_INSTANCE);
+                            to get on instance of a Doctrine_Table.") {
+        parent::__construct($message,Doctrine::ERR_TABLE_INSTANCE);
     }
 }
 
