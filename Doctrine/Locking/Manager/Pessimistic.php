46bc40f73276a756406fb4a5aa4309133cc36e8c
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-09-15 21:04:06 +0000

    Fixed pessimistic locking manager

--- Doctrine/Locking/Manager/Pessimistic.php
+++ Doctrine/Locking/Manager/Pessimistic.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.com>.
+ */
 /**
  * Class for handling the pessimistic offline locking of {@link Doctrine_Record}s.
  * Offline locking of records comes in handy where you need to make sure that
@@ -37,10 +56,10 @@ class Doctrine_Locking_Manager_Pessimistic
         if($this->_dataSource->getAttribute(Doctrine::ATTR_CREATE_TABLES) === true)
         {
             $columns = array();
-            $columns['object_type']        = array('string', 50, array('notnull','primary'));
-            $columns['object_key']         = array('string', 250, array('notnull','primary'));
-            $columns['user_ident']         = array('string', 50, array('notnull'));
-            $columns['timestamp_obtained'] = array('integer', 10, array('notnull'));
+            $columns['object_type']        = array('string',  50, array('notnull' => true, 'primary'));
+            $columns['object_key']         = array('string', 250, array('notnull' => true, 'primary'));
+            $columns['user_ident']         = array('string',  50, array('notnull' => true));
+            $columns['timestamp_obtained'] = array('integer', 10, array('notnull' => true));
             
             $dataDict = new Doctrine_DataDict($this->_dataSource->getDBH());
             $dataDict->createTable($this->_lockTable, $columns);
