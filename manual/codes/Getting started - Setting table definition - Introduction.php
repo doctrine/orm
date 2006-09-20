edbe5cd3ac4416c9ef9f53dd8402e70a20c4168f
roger <roger@625475ce-881a-0410-a577-b389adb331d8>
2006-09-20 15:51:33 +0000

    fixed syntax errors

--- manual/codes/Getting started - Setting table definition - Introduction.php
+++ manual/codes/Getting started - Setting table definition - Introduction.php
@@ -9,24 +9,25 @@ class Email extends Doctrine_Record {
                          "200",             // column length
                          array("notblank" => true,
                                "email"    => true  // validators / constraints
-                               );
-                               
+                               )
+                         );
+
 
         $this->hasColumn("address2",         // name of the column
                          "string",          // column type
                          "200",             // column length
-                         // validators / constraints without arguments can be 
+                         // validators / constraints without arguments can be
                          // specified also as as string with | separator
-                         "notblank|email",
+                         "notblank|email"
                          );
-                         
-        // Doctrine even supports the following format for 
+
+        // Doctrine even supports the following format for
         // validators / constraints which have no arguments:
-        
+
         $this->hasColumn("address3",         // name of the column
                          "string",          // column type
                          "200",             // column length
-                         array("notblank", "email"),
+                         array("notblank", "email")
                          );
     }
 }
