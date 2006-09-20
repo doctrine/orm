20f8581c83b4f3b362db5bad03c0280d3dd131a1
elliot <elliot@625475ce-881a-0410-a577-b389adb331d8>
2006-09-20 11:38:50 +0000

    Cleaned up deprecated functions

--- Doctrine/Validator/Email.php
+++ Doctrine/Validator/Email.php
@@ -1,5 +1,7 @@
 <?php
+
 class Doctrine_Validator_Email {
+
     /**
      * @link http://iamcal.com/publish/articles/php/parsing_email/pdf/
      * @param Doctrine_Record $record
@@ -33,50 +35,6 @@ class Doctrine_Validator_Email {
 
         return (bool)preg_match("!^$addr_spec$!", $value);
     }
-    /**
-     * validateEmail
-     *
-     * method not used anymore (should be removed)
-     *
-     * @deprecated
-     * @param string $value
-     *
-    public static function validateEmail($value) {
-        $parts = explode("@", $value);
 
-        if(count($parts) != 2) 
-            return false;
-
-        if(strlen($parts[0]) < 1 || strlen($parts[0]) > 64)
-            return false;
-
-        if(strlen($parts[1]) < 1 || strlen($parts[1]) > 255)
-            return false;
-
-        $local_array = explode(".", $parts[0]);
-        for ($i = 0; $i < sizeof($local_array); $i++) {
-            if (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $parts[$i])) {
-                return false;
-            }
-        }
-        if (!ereg("^\[?[0-9\.]+\]?$", $parts[1])) { // Check if domain is IP. If not, it should be valid domain name
-            $domain_array = explode(".", $parts[1]);
-            if (count($domain_array) < 2) {
-                return false; // Not enough parts to domain
-            }
-            for ($i = 0; $i < sizeof($domain_array); $i++) {
-                if (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
-                    return false;
-                }
-            }
-        }
-        if(function_exists("checkdnsrr")) {
-            if( ! checkdnsrr($parts[1], "MX"))
-                return false;
-        }
-
-        return true;
-    }
-    */
 }
 
