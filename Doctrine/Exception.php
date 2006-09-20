89e1215296575d3a90f07ad14b17a1916db12f74
elliot <elliot@625475ce-881a-0410-a577-b389adb331d8>
2006-09-20 09:26:26 +0000

    Changed DQLException, InvalidKeyException and InvalidTypeException to inherit Doctrine_Exception

--- Doctrine/Exception.php
+++ Doctrine/Exception.php
@@ -1,6 +1,6 @@
 <?php
-class InvalidKeyException extends Exception { }
-class InvalidTypeException extends Exception { }
+
 class Doctrine_Exception extends Exception { }   
 class DQLException extends Doctrine_Exception { }
-
+class InvalidKeyException extends Doctrine_Exception { }
+class InvalidTypeException extends Doctrine_Exception { }
