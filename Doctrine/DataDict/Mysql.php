f19a17b02be49644fa9b5994de4a8f5e157b207e
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-09-25 21:16:08 +0000

    added datadict mysql driver, fixes #114 Ticket: 114

---
+++ Doctrine/DataDict/Mysql.php
@@ -0,0 +1,154 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.com>.
+ */
+
+/**
+ * @package     Doctrine
+ * @url         http://www.phpdoctrine.com
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @author      Konsta Vesterinen
+ * @version     $Id$
+ */
+ 
+class Doctrine_DataDict_Mysql extends Doctrine_DataDict {
+    /**
+     * lists all databases
+     *
+     * @return array
+     */
+    public function listDatabases() {
+
+    }
+    /**
+     * lists all availible database functions
+     *
+     * @return array
+     */
+    public function listFunctions() {
+    
+    }
+    /**
+     * lists all database triggers
+     *
+     * @param string|null $database
+     * @return array
+     */
+    public function listTriggers($database = null) {
+
+    }
+    /**
+     * lists all database sequences
+     *
+     * @param string|null $database
+     * @return array
+     */
+    public function listSequences($database = null) { 
+    
+    }
+    /**
+     * lists table constraints
+     *
+     * @param string $table     database table name
+     * @return array
+     */
+    public function listTableConstraints($table) {
+    
+    }
+    /**
+     * lists table constraints
+     *
+     * @param string $table     database table name
+     * @return array
+     */
+    public function listTableColumns($table) { 
+        $sql = "DESCRIBE $table";
+        $result = $this->dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $description = array();
+        foreach ($result as $key => $val) {
+            $description[$val['field']] = array(
+                'name'    => $val['field'],
+                'type'    => $val['type'],
+                'primary' => (strtolower($val['key']) == 'pri'),
+                'default' => $val['default'],
+                'notnull' => (bool) ($val['null'] != 'YES'),
+
+            );
+            $columns[$val['field']] = new Doctrine_Schema_Column($description);
+        }
+
+
+        return $columns;
+    }
+    /**
+     * lists table constraints
+     *
+     * @param string $table     database table name
+     * @return array
+     */
+    public function listTableIndexes($table) {
+    
+    }
+    /**
+     * lists tables
+     *
+     * @param string|null $database
+     * @return array
+     */
+    public function listTables($database = null) {
+        $sql = "SHOW TABLES";
+        
+        return $this->dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    /**
+     * lists table triggers
+     *
+     * @param string $table     database table name
+     * @return array
+     */
+    public function listTableTriggers($table) { 
+    
+    }
+    /**
+     * lists table views
+     *
+     * @param string $table     database table name
+     * @return array
+     */
+    public function listTableViews($table) { 
+    
+    }
+    /**
+     * lists database users
+     *
+     * @return array
+     */
+    public function listUsers() { 
+    
+    }
+    /**
+     * lists database views
+     *
+     * @param string|null $database
+     * @return array
+     */
+    public function listViews($database = null) { 
+    
+    }
+}
