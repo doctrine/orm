716a91e511799b8fd4b124a10d4ffa4b7c6d2a54
pookey <pookey@625475ce-881a-0410-a577-b389adb331d8>
2006-10-15 17:15:16 +0000

    improving (and breaking) find() test cases, refs #168

--- tests/TableTestCase.php
+++ tests/TableTestCase.php
@@ -112,7 +112,32 @@ class Doctrine_TableTestCase extends Doctrine_UnitTestCase {
         $this->assertTrue($record instanceof Doctrine_Record);
         
         try {
+            $record = $this->objTable->find('4');
+            $this->assertTrue($record instanceof Doctrine_Record);
+        } catch(Exception $e) {
+            $this->assertTrue($e instanceOf Doctrine_Find_Exception);
+        }
+
+        try {
             $record = $this->objTable->find(123);
+            // should have thrown exception...
+            $this->assertTrue(false);
+        } catch(Exception $e) {
+            $this->assertTrue($e instanceOf Doctrine_Find_Exception);
+        }
+
+        try {
+            $record = $this->objTable->find(null);
+            // should have thrown exception...
+            $this->assertTrue(false);
+        } catch(Exception $e) {
+            $this->assertTrue($e instanceOf Doctrine_Find_Exception);
+        }
+
+        try {
+            $record = $this->objTable->find(false);
+            // should have thrown exception...
+            $this->assertTrue(false);
         } catch(Exception $e) {
             $this->assertTrue($e instanceOf Doctrine_Find_Exception);
         }
