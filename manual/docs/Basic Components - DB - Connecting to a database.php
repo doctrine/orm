7dabc662605a71dbd3fa97beebb689ee64ffad02
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-10-19 19:22:10 +0000

    updated database connecting docs

--- manual/docs/Basic Components - DB - Connecting to a database.php
+++ manual/docs/Basic Components - DB - Connecting to a database.php
@@ -1,10 +1,14 @@
 <?php
 
-// using PDO dsn for connecting sqlite memory table
+// using PDO like dsn for connecting sqlite memory table
 
-//$dbh = Doctrine_DB::getConnection('sqlite::memory:');
+$dbh = Doctrine_DB::getConnection('sqlite::memory:');
+
+// using PDO like dsn for connecting pgsql database
+
+$dbh = Doctrine_DB::getConnection('pgsql://root:password@localhost/mydb');
 
 // using PEAR like dsn for connecting mysql database
 
-//$dbh = Doctrine_DB::getConnection('mysql://root:password@localhost/test');
+$dbh = Doctrine_DB::getConnection('mysql://root:password@localhost/test');
 ?>
