a6bdf19cb92c14ca95ec0eda81cc2c0c39d3d8bd
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-11-12 22:57:18 +0000

    fixes #229

--- lib/Doctrine/Validator/Unique.php
+++ lib/Doctrine/Validator/Unique.php
@@ -1,4 +1,35 @@
 <?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.com>.
+ */
+
+/**
+ * Doctrine_Validator_Unique
+ *
+ * @package     Doctrine
+ * @category    Object Relational Mapping
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.phpdoctrine.com
+ * @since       1.0
+ * @version     $Revision$
+ * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
+ */
 class Doctrine_Validator_Unique {
     /**
      * @param Doctrine_Record $record
@@ -9,9 +40,10 @@ class Doctrine_Validator_Unique {
      */
     public function validate(Doctrine_Record $record, $key, $value, $args) {
         $table = $record->getTable();
-        $sql   = "SELECT id FROM ".$table->getTableName()." WHERE ".$key." = ?";
-        $stmt  = $table->getConnection()->getDBH()->prepare($sql);
+        $sql   = 'SELECT ' . $table->getIdentifier() . ' FROM ' . $table->getTableName() . ' WHERE ' . $key . ' = ?';
+        $stmt  = $table->getConnection()->getDbh()->prepare($sql);
         $stmt->execute(array($value));
+
         return ( ! is_array($stmt->fetch()));
     }
 }
