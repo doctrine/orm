faa5e603f026ae305f3274a26c9c867659e29270
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-11-29 22:34:14 +0000

    Sqlite datadict driver tests

--- tests/DataDict/SqliteTestCase.php
+++ tests/DataDict/SqliteTestCase.php
@@ -35,6 +35,85 @@ class Doctrine_DataDict_Sqlite_TestCase extends Doctrine_Driver_UnitTestCase {
     public function testYearMapsToIntegerAndDate() {
          $this->assertDeclarationType('year', array('integer','date'));
     }
+    public function testGetNativeDefinitionSupportsIntegerType() {
+        $a = array('type' => 'integer', 'length' => 20, 'fixed' => false);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'BIGINT');
+
+        $a['length'] = 4;
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'INTEGER');
+
+        $a['length'] = 2;
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'SMALLINT');
+    }
+
+    public function testGetNativeDefinitionSupportsFloatType() {
+        $a = array('type' => 'float', 'length' => 20, 'fixed' => false);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'DOUBLE');
+    }
+    public function testGetNativeDefinitionSupportsBooleanType() {
+        $a = array('type' => 'boolean', 'fixed' => false);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'BOOLEAN');
+    }
+    public function testGetNativeDefinitionSupportsDateType() {
+        $a = array('type' => 'date', 'fixed' => false);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'DATE');
+    }
+    public function testGetNativeDefinitionSupportsTimestampType() {
+        $a = array('type' => 'timestamp', 'fixed' => false);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'DATETIME');
+    }
+    public function testGetNativeDefinitionSupportsTimeType() {
+        $a = array('type' => 'time', 'fixed' => false);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'TIME');
+    }
+    public function testGetNativeDefinitionSupportsClobType() {
+        $a = array('type' => 'clob');
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'LONGTEXT');
+    }
+    public function testGetNativeDefinitionSupportsBlobType() {
+        $a = array('type' => 'blob');
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'LONGBLOB');
+    }
+    public function testGetNativeDefinitionSupportsCharType() {
+        $a = array('type' => 'char', 'length' => 10);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'CHAR(10)');
+    }
+    public function testGetNativeDefinitionSupportsVarcharType() {
+        $a = array('type' => 'varchar', 'length' => 10);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'VARCHAR(10)');
+    }
+    public function testGetNativeDefinitionSupportsArrayType() {
+        $a = array('type' => 'array', 'length' => 40);
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'VARCHAR(40)');
+    }
+    public function testGetNativeDefinitionSupportsStringType() {
+        $a = array('type' => 'string');
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'TEXT');
+    }
+    public function testGetNativeDefinitionSupportsArrayType2() {
+        $a = array('type' => 'array');
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'TEXT');
+    }
+    public function testGetNativeDefinitionSupportsObjectType() {
+        $a = array('type' => 'object');
+
+        $this->assertEqual($this->dataDict->getNativeDeclaration($a), 'TEXT');
+    }
     public function testSomething() {
         /**
 
