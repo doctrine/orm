50226697e043e537e5a52a4e7a4d02efa432bcd4
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-12-14 12:25:34 +0000

    added reserved keyword list

--- lib/Doctrine/DataDict/Mysql.php
+++ lib/Doctrine/DataDict/Mysql.php
@@ -30,6 +30,83 @@ Doctrine::autoload('Doctrine_DataDict');
  * @since       1.0
  */
 class Doctrine_DataDict_Mysql extends Doctrine_DataDict {
+    protected $keywords = array(
+                          'ADD', 'ALL', 'ALTER',
+                          'ANALYZE', 'AND', 'AS',
+                          'ASC', 'ASENSITIVE', 'BEFORE',
+                          'BETWEEN', 'BIGINT', 'BINARY',
+                          'BLOB', 'BOTH', 'BY',
+                          'CALL', 'CASCADE', 'CASE',
+                          'CHANGE', 'CHAR', 'CHARACTER',
+                          'CHECK', 'COLLATE', 'COLUMN',
+                          'CONDITION', 'CONNECTION', 'CONSTRAINT',
+                          'CONTINUE', 'CONVERT', 'CREATE',
+                          'CROSS', 'CURRENT_DATE', 'CURRENT_TIME',
+                          'CURRENT_TIMESTAMP', 'CURRENT_USER', 'CURSOR',
+                          'DATABASE', 'DATABASES', 'DAY_HOUR',
+                          'DAY_MICROSECOND', 'DAY_MINUTE', 'DAY_SECOND',
+                          'DEC', 'DECIMAL', 'DECLARE',
+                          'DEFAULT', 'DELAYED', 'DELETE',
+                          'DESC', 'DESCRIBE', 'DETERMINISTIC',
+                          'DISTINCT', 'DISTINCTROW', 'DIV',
+                          'DOUBLE', 'DROP', 'DUAL',
+                          'EACH', 'ELSE', 'ELSEIF',
+                          'ENCLOSED', 'ESCAPED', 'EXISTS',
+                          'EXIT', 'EXPLAIN', 'FALSE',
+                          'FETCH', 'FLOAT', 'FLOAT4',
+                          'FLOAT8', 'FOR', 'FORCE',
+                          'FOREIGN', 'FROM', 'FULLTEXT',
+                          'GRANT', 'GROUP', 'HAVING',
+                          'HIGH_PRIORITY', 'HOUR_MICROSECOND', 'HOUR_MINUTE',
+                          'HOUR_SECOND', 'IF', 'IGNORE',
+                          'IN', 'INDEX', 'INFILE',
+                          'INNER', 'INOUT', 'INSENSITIVE',
+                          'INSERT', 'INT', 'INT1',
+                          'INT2', 'INT3', 'INT4',
+                          'INT8', 'INTEGER', 'INTERVAL',
+                          'INTO', 'IS', 'ITERATE',
+                          'JOIN', 'KEY', 'KEYS',
+                          'KILL', 'LEADING', 'LEAVE',
+                          'LEFT', 'LIKE', 'LIMIT',
+                          'LINES', 'LOAD', 'LOCALTIME',
+                          'LOCALTIMESTAMP', 'LOCK', 'LONG',
+                          'LONGBLOB', 'LONGTEXT', 'LOOP',
+                          'LOW_PRIORITY', 'MATCH', 'MEDIUMBLOB',
+                          'MEDIUMINT', 'MEDIUMTEXT', 'MIDDLEINT',
+                          'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'MOD',
+                          'MODIFIES', 'NATURAL', 'NOT',
+                          'NO_WRITE_TO_BINLOG', 'NULL', 'NUMERIC',
+                          'ON', 'OPTIMIZE', 'OPTION',
+                          'OPTIONALLY', 'OR', 'ORDER',
+                          'OUT', 'OUTER', 'OUTFILE',
+                          'PRECISION', 'PRIMARY', 'PROCEDURE',
+                          'PURGE', 'RAID0', 'READ',
+                          'READS', 'REAL', 'REFERENCES',
+                          'REGEXP', 'RELEASE', 'RENAME',
+                          'REPEAT', 'REPLACE', 'REQUIRE',
+                          'RESTRICT', 'RETURN', 'REVOKE',
+                          'RIGHT', 'RLIKE', 'SCHEMA',
+                          'SCHEMAS', 'SECOND_MICROSECOND', 'SELECT',
+                          'SENSITIVE', 'SEPARATOR', 'SET',
+                          'SHOW', 'SMALLINT', 'SONAME',
+                          'SPATIAL', 'SPECIFIC', 'SQL',
+                          'SQLEXCEPTION', 'SQLSTATE', 'SQLWARNING',
+                          'SQL_BIG_RESULT', 'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT',
+                          'SSL', 'STARTING', 'STRAIGHT_JOIN',
+                          'TABLE', 'TERMINATED', 'THEN',
+                          'TINYBLOB', 'TINYINT', 'TINYTEXT',
+                          'TO', 'TRAILING', 'TRIGGER',
+                          'TRUE', 'UNDO', 'UNION',
+                          'UNIQUE', 'UNLOCK', 'UNSIGNED',
+                          'UPDATE', 'USAGE', 'USE',
+                          'USING', 'UTC_DATE', 'UTC_TIME',
+                          'UTC_TIMESTAMP', 'VALUES', 'VARBINARY',
+                          'VARCHAR', 'VARCHARACTER', 'VARYING',
+                          'WHEN', 'WHERE', 'WHILE',
+                          'WITH', 'WRITE', 'X509',
+                          'XOR', 'YEAR_MONTH', 'ZEROFILL'
+                          );
+
     /**
      * Obtain DBMS specific SQL code portion needed to declare an text type
      * field to be used in statements like CREATE TABLE.
@@ -71,7 +148,7 @@ class Doctrine_DataDict_Mysql extends Doctrine_DataDict {
                 $fixed  = (! empty($field['fixed'])) ? $field['fixed'] : false;
 
                 return $fixed ? ($length ? 'CHAR('.$length.')' : 'CHAR(255)')
-                    : ($length ? 'VARCHAR('.$length.')' : 'TEXT');
+                    : ($length ? 'VARCHAR(' . $length . ')' : 'TEXT');
             case 'clob':
                 if (!empty($field['length'])) {
                     $length = $field['length'];
