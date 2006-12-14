7bf4a50743d631ed06213cf717756536f99a2141
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2006-12-14 22:26:39 +0000

    Removed unnecessary Db drivers

--- lib/Doctrine/Db/Exception.php
+++ lib/Doctrine/Db/Exception.php
@@ -31,56 +31,5 @@
  * @version     $Revision$
  */
 class Doctrine_Db_Exception extends Doctrine_Exception { 
-    /**
-     * @var array $errorMessages        an array containing messages for portable error codes
-     */
-    protected static $errorMessages = array(
-                Doctrine_Db::ERR                    => 'unknown error',
-                Doctrine_Db::ERR_ALREADY_EXISTS     => 'already exists',
-                Doctrine_Db::ERR_CANNOT_CREATE      => 'can not create',
-                Doctrine_Db::ERR_CANNOT_ALTER       => 'can not alter',
-                Doctrine_Db::ERR_CANNOT_REPLACE     => 'can not replace',
-                Doctrine_Db::ERR_CANNOT_DELETE      => 'can not delete',
-                Doctrine_Db::ERR_CANNOT_DROP        => 'can not drop',
-                Doctrine_Db::ERR_CONSTRAINT         => 'constraint violation',
-                Doctrine_Db::ERR_CONSTRAINT_NOT_NULL=> 'null value violates not-null constraint',
-                Doctrine_Db::ERR_DIVZERO            => 'division by zero',
-                Doctrine_Db::ERR_INVALID            => 'invalid',
-                Doctrine_Db::ERR_INVALID_DATE       => 'invalid date or time',
-                Doctrine_Db::ERR_INVALID_NUMBER     => 'invalid number',
-                Doctrine_Db::ERR_MISMATCH           => 'mismatch',
-                Doctrine_Db::ERR_NODBSELECTED       => 'no database selected',
-                Doctrine_Db::ERR_NOSUCHFIELD        => 'no such field',
-                Doctrine_Db::ERR_NOSUCHTABLE        => 'no such table',
-                Doctrine_Db::ERR_NOT_CAPABLE        => 'MDB2 backend not capable',
-                Doctrine_Db::ERR_NOT_FOUND          => 'not found',
-                Doctrine_Db::ERR_NOT_LOCKED         => 'not locked',
-                Doctrine_Db::ERR_SYNTAX             => 'syntax error',
-                Doctrine_Db::ERR_UNSUPPORTED        => 'not supported',
-                Doctrine_Db::ERR_VALUE_COUNT_ON_ROW => 'value count on row',
-                Doctrine_Db::ERR_INVALID_DSN        => 'invalid DSN',
-                Doctrine_Db::ERR_CONNECT_FAILED     => 'connect failed',
-                Doctrine_Db::ERR_NEED_MORE_DATA     => 'insufficient data supplied',
-                Doctrine_Db::ERR_EXTENSION_NOT_FOUND=> 'extension not found',
-                Doctrine_Db::ERR_NOSUCHDB           => 'no such database',
-                Doctrine_Db::ERR_ACCESS_VIOLATION   => 'insufficient permissions',
-                Doctrine_Db::ERR_LOADMODULE         => 'error while including on demand module',
-                Doctrine_Db::ERR_TRUNCATED          => 'truncated',
-                Doctrine_Db::ERR_DEADLOCK           => 'deadlock detected',
-                );
 
-    /**
-     * Return a textual error message for a Doctrine_Db error code
-     *
-     * @param   int|array   integer error code,
-                                null to get the current error code-message map,
-                                or an array with a new error code-message map
-     *
-     * @return  string  error message, or false if the error code was
-     *                  not recognized
-     */
-    public function errorMessage($value = null) {
-        return isset(self::$errorMessages[$value]) ?
-           self::$errorMessages[$value] : self::$errorMessages[Doctrine_Db::ERR];
-    }
 }
