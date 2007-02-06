4c7f91fa782a4082630f7c79e6a9e6596bf54951
runa <runa@625475ce-881a-0410-a577-b389adb331d8>
2007-02-06 14:39:47 +0000

    php syntax

--- manual/codes/Getting started - Record identifiers - Composite.php
+++ manual/codes/Getting started - Record identifiers - Composite.php
@@ -1,7 +1,7 @@
 <?php
 class Groupuser extends Doctrine_Record {
     public function setTableDefinition() {
-        $this->hasColumn("user_id", "integer" 20, "primary");
+        $this->hasColumn("user_id", "integer", 20, "primary");
         $this->hasColumn("group_id", "integer", 20, "primary");
     }
 }
