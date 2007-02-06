6e1e8fb85bb372e083a8f6e939fa9bcba32d266d
runa <runa@625475ce-881a-0410-a577-b389adb331d8>
2007-02-06 14:40:13 +0000

    php syntax

--- manual/codes/Configuration - Levels of configuration.php
+++ manual/codes/Configuration - Levels of configuration.php
@@ -7,7 +7,7 @@ $manager->setAttribute(Doctrine::ATTR_VLD, false);
 // setting a connection level attribute
 // (overrides the global level attribute on this connection)
 
-$conn = $manager->openConnection(new PDO('dsn', 'username', 'pw');
+$conn = $manager->openConnection(new PDO('dsn', 'username', 'pw'));
 
 $conn->setAttribute(Doctrine::ATTR_VLD, true);
 
