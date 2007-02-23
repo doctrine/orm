31ccb6369bd74b34bbe0896cdcb97e1b0aae0337
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2007-02-23 20:54:10 +0000

    renaming some files

---
+++ manual/docs/Object relational mapping - Columns - Default values.php
@@ -0,0 +1,14 @@
+Doctrine supports default values for all data types. When default value is attached to a record column this means two of things.
+First this value is attached to every newly created Record.
+
+<code type="php">
+class User extends Doctrine_record {
+    public function setTableDefinition() {
+        $this->hasColumn('name', 'string', 50, array('default' => 'default name'));
+    } 
+}
+$user = new User();
+print $user->name; // default name
+</code>
+
+Also when exporting record class to database DEFAULT //value// is attached to column definition statement. 
