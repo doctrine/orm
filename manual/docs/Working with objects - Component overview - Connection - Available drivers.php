60b89fd3b5838f10689639da5cef0de03db21df1
meus <meus@625475ce-881a-0410-a577-b389adb331d8>
2007-03-15 14:49:13 +0000

    Fixed the formating of the listing

--- manual/docs/Working with objects - Component overview - Connection - Available drivers.php
+++ manual/docs/Working with objects - Component overview - Connection - Available drivers.php
@@ -1,9 +1,11 @@
 Doctrine has drivers for every PDO-supported database. The supported databases are:
-<li>FreeTDS / Microsoft SQL Server / Sybase
-<li>Firebird/Interbase 6
-<li>Informix
-<li>Mysql
-<li>Oracle
-<li>Odbc
-<li>PostgreSQL
-<li>Sqlite
+<ul>
+<li \>FreeTDS / Microsoft SQL Server / Sybase
+<li \>Firebird/Interbase 6
+<li \>Informix
+<li \>Mysql
+<li \>Oracle
+<li \>Odbc
+<li \>PostgreSQL
+<li \>Sqlite
+</ul>
