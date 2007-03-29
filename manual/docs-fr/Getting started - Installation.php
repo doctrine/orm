c3eecb9f1e623c61f3c7c4bc802dc76f525236bb
ace <ace@625475ce-881a-0410-a577-b389adb331d8>
2007-03-29 16:23:13 +0000

    French documentation testing.

---
+++ manual/docs-fr/Getting started - Installation.php
@@ -0,0 +1,17 @@
+L'installation de Doctrine est très simple. Il suffit de récupérer la dernière version de Doctrine sur http://doctrine.pengus.net/svn/trunk.
+
+Pour ce faire, vous avez besoin d'un client Subversion (SVN).
+
+Pour "emprunter" (checkout) Doctrine dans répertoire courant en utiliser l'outil de lignes de commande **svn**, utilisez le code suivant:
+<code type="bash">
+svn co http://doctrine.pengus.net/svn/trunk . 
+</code>
+
+Si vous n'avez pas de client SVN, choisissez-en un dans la liste ci-dessous.
+Trouver l'option **Checkout** et entrez http://doctrine.pengus.net/svn/trunk dans le champ **path** ou **repository url**.
+Aucune authentification n'est requise pour récupérer Doctrine.
+
+* [http://tortoisesvn.tigris.org/ TortoiseSVN] une application Windows qui s'intègre directement dans l'exporateur de fichier.
+* [http://www.apple.com/downloads/macosx/development_tools/svnx.html svnx] une interface graphique SVN pour Mac
+* Eclipse possède une intégration SVN grâce au plugin [http://subclipse.tigris.org/ subeclipse]
+
