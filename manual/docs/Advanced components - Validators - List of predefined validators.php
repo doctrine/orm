0929be645411204cf266674dfc9bf38db034dc8f
meus <meus@625475ce-881a-0410-a577-b389adb331d8>
2007-04-12 17:46:26 +0000

    Fixed broken docs

--- manual/docs/Advanced components - Validators - List of predefined validators.php
+++ manual/docs/Advanced components - Validators - List of predefined validators.php
@@ -1,124 +1,13 @@
 Here is a list of predefined validators. You cannot use these names for your custom validators.
-<table width="500">
-<tr>
-    <td>
-        <b>name</b>
-    </td>
-    <td>
-        <b>arguments</b>
-    </td>
-    <td>
-        <b>task</b>
-    </td>
-</tr>
-<tr>
-    <td>
-        email
-    </td>
-    <td>
 
-    </td>
-    <td>
-        Checks if value is valid email.
-    </td>
-</tr>
-<tr>
-    <td>
-        notblank
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value is not blank.
-    </td>
-</tr>
-<tr>
-    <td>
-        notnull
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value is not null.
-    </td>
-</tr>
-<tr>
-    <td>
-        country
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value is valid country code.
-    </td>
-</tr>
-<tr>
-    <td>
-        ip
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value is valid internet protocol address.
-    </td>
-</tr>
-<tr>
-    <td>
-        htmlcolor
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value is valid html color.
-    </td>
-</tr>
-<tr>
-    <td>
-        nospace
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value has no space chars.
-    </td>
-</tr>
-<tr>
-    <td>
-        range
-    </td>
-    <td>
-        [min, max]
-    </td>
-    <td>
-        Checks if value is in range specified by arguments.
-    </td>
-</tr>
-<tr>
-    <td>
-        unique
-    </td>
-    <td>
-
-    </td>
-    <td>
-        Checks if value is unique in its database table.
-    </td>
-</tr>
-<tr>
-    <td>
-        regexp
-    </td>
-    <td>
-        [expression]
-    </td>
-    <td>
-        Checks if value matches a given regexp.
-    </td>
-</tr>
-</table>
+|| **name** || **arguments** || **task**      ||
+|| email 		|| 							 || Check if value is valid email.|| 
+|| notblank || 							 || Check if value is not blank.||
+|| notnull  ||               || Check if value is not null.||
+|| country  ||               || Check if valid is valid country code.||
+|| ip       ||               || Checks if value is valid IP (internet protocol) address.||
+|| htmlcolor ||              || Checks if value is valid html color.||
+|| nospace  ||               || Check if value has no space chars. ||
+|| range    || [min,max]     || Checks if value is in range specified by arguments.||
+|| unique   ||							 || Checks if value is unique in its database table. ||
+|| regexp   || [expression]  || Check if valie matches a given regexp. ||
