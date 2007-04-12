eed2a37037a65d9a0fbefd3f1a8fd14511e16a38
pookey <pookey@625475ce-881a-0410-a577-b389adb331d8>
2007-04-12 19:20:30 +0000

    adding a failing test case for ticket #307

--- tests/Query/SubqueryTestCase.php
+++ tests/Query/SubqueryTestCase.php
@@ -53,5 +53,16 @@ class Doctrine_Query_Subquery_TestCase extends Doctrine_UnitTestCase
         
         $this->assertEqual($q->getQuery(), "SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id NOT IN (SELECT g.user_id AS g__user_id FROM groupuser g) AND (e.type = 0)");
     }
+
+    public function testSubqueryInSelectPart()
+    {
+        // ticket #307
+        $q = new Doctrine_Query();
+        $q->query("SELECT u.*, (SELECT p.name FROM User p WHERE p.name = u.name) name2 FROM User u WHERE u.name = 'zYne' LIMIT 1");
+
+        $users = $q->execute();
+
+        $this->assertEqual($users->count(), 1);
+    }
 }
 ?>
