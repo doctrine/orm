4fc97fe1b86caeeb0ba5bd43006d22337dc8ff22
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-04-12 16:45:13 +0000

    Added simple COUNT() example

--- manual/codes/DQL (Doctrine Query Language) - SELECT queries - Aggregate values.php
+++ manual/codes/DQL (Doctrine Query Language) - SELECT queries - Aggregate values.php
@@ -1,3 +1,9 @@
 <?php
-echo 'test';
+// SELECT u.*, COUNT(p.id) num_posts FROM User u, u.Posts p WHERE u.id = 1
+
+$query = new Doctrine_Query();
+
+$query->select('u.*, COUNT(p.id) num_posts')
+      ->from('User u, u.Posts p')
+      ->where('u.id = ?', 1)
 ?>
