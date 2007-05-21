1a4baae1df53e4fa7518d528f2144c2ad8b7ed75
lsmith <lsmith@625475ce-881a-0410-a577-b389adb331d8>
2007-05-21 13:42:09 +0000

    - typo fix

--- manual/docs/Working with objects - Component overview - Collection - Loading related records.php
+++ manual/docs/Working with objects - Component overview - Collection - Loading related records.php
@@ -1,4 +1,4 @@
-Doctrine provides means for effiently retrieving all related records for all record elements. That means
+Doctrine provides means for efficiently retrieving all related records for all record elements. That means
 when you have for example a collection of users you can load all phonenumbers for all users by simple calling
 the loadRelated() method.
 
