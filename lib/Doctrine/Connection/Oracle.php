1e5c0d674c6ef8146b46f9bbb58ae2a45ca1d197
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2007-05-24 22:08:54 +0000

    fixes #333

--- lib/Doctrine/Connection/Oracle.php
+++ lib/Doctrine/Connection/Oracle.php
@@ -76,7 +76,7 @@ class Doctrine_Connection_Oracle extends Doctrine_Connection
      */
     public function setDateFormat($format = 'YYYY-MM-DD HH24:MI:SS')
     {
-      $this->exec('ALTER SESSION SET NLS_DATE_FORMAT "'.$format.'";');
+      $this->exec('ALTER SESSION SET NLS_DATE_FORMAT = "' . $format . '"');
     }
     /**
      * Adds an driver-specific LIMIT clause to the query
