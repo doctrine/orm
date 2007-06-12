1b807d8e972a99d7e36bb7488074b0a7466ba6bc
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2007-06-12 11:22:15 +0000

    added doc block

--- tests/Export/PgsqlTestCase.php
+++ tests/Export/PgsqlTestCase.php
@@ -1,15 +1,50 @@
 <?php
-class Doctrine_Export_Pgsql_TestCase extends Doctrine_UnitTestCase {
-    public function testCreateDatabaseExecutesSql() {
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.com>.
+ */
+
+/**
+ * Doctrine_Export_Mysql_TestCase
+ *
+ * @package     Doctrine
+ * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @category    Object Relational Mapping
+ * @link        www.phpdoctrine.com
+ * @since       1.0
+ * @version     $Revision$
+ */
+class Doctrine_Export_Pgsql_TestCase extends Doctrine_UnitTestCase 
+{
+    public function testCreateDatabaseExecutesSql() 
+    {
         $this->export->createDatabase('db');
         $this->assertEqual($this->adapter->pop(), 'CREATE DATABASE db');
     }
-    public function testDropDatabaseExecutesSql() {
+    public function testDropDatabaseExecutesSql()
+    {
         $this->export->dropDatabase('db');
 
         $this->assertEqual($this->adapter->pop(), 'DROP DATABASE db');
     }
-    public function testCreateTableSupportsAutoincPks() {
+    public function testCreateTableSupportsAutoincPks() 
+    {
         $name = 'mytable';
         
         $fields  = array('id' => array('type' => 'integer', 'unsigned' => 1, 'autoincrement' => true));
@@ -19,7 +54,8 @@ class Doctrine_Export_Pgsql_TestCase extends Doctrine_UnitTestCase {
 
         $this->assertEqual($this->adapter->pop(), 'CREATE TABLE mytable (id SERIAL, PRIMARY KEY(id))');
     }
-    public function testCreateTableSupportsDefaultAttribute() {
+    public function testCreateTableSupportsDefaultAttribute() 
+    {
         $name = 'mytable';
         $fields  = array('name' => array('type' => 'char', 'length' => 10, 'default' => 'def'),
                          'type' => array('type' => 'integer', 'length' => 3, 'default' => 12)
@@ -30,7 +66,8 @@ class Doctrine_Export_Pgsql_TestCase extends Doctrine_UnitTestCase {
 
         $this->assertEqual($this->adapter->pop(), 'CREATE TABLE mytable (name CHAR(10) DEFAULT \'def\', type INT DEFAULT 12, PRIMARY KEY(name, type))');
     }
-    public function testCreateTableSupportsMultiplePks() {
+    public function testCreateTableSupportsMultiplePks()
+     {
         $name = 'mytable';
         $fields  = array('name' => array('type' => 'char', 'length' => 10),
                          'type' => array('type' => 'integer', 'length' => 3));
