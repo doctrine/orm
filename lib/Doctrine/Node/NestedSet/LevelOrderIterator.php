e1fb5e53458472d0dd6e1dabf1c1b2f842a2b6ef
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2007-06-13 20:22:46 +0000

    removed iterator references (those will be added when the implementation is ready)

--- lib/Doctrine/Node/NestedSet/LevelOrderIterator.php
+++ lib/Doctrine/Node/NestedSet/LevelOrderIterator.php
@@ -29,5 +29,5 @@
  * @version     $Revision$
  * @author      Joe Simms <joe.simms@websites4.com>
  */
-class Doctrine_Node_NestedSet_LevelOrderIterator implements Iterator
+class Doctrine_Node_NestedSet_LevelOrderIterator
 {}
--- lib/Doctrine/Node/NestedSet/PostOrderIterator.php
+++ lib/Doctrine/Node/NestedSet/PostOrderIterator.php
@@ -29,5 +29,5 @@
  * @version     $Revision$
  * @author      Joe Simms <joe.simms@websites4.com>
  */
-class Doctrine_Node_NestedSet_PostOrderIterator implements Iterator
+class Doctrine_Node_NestedSet_PostOrderIterator
 {}
