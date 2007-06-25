0eab525af545316df02977d6df588e9921df042f
nicobn <nicobn@625475ce-881a-0410-a577-b389adb331d8>
2007-06-25 04:19:23 +0000

    Made bundle.php more intelligent.

--- tools/cli/bundle.php
+++ tools/cli/bundle.php
@@ -3,12 +3,24 @@
  * Small command line script to bundle Doctrine classes.
  */
 if (count($argv) < 2) {
-    echo "Usage: bundle.php <Doctrine basedir> <Target dir>";
+    echo "Usage: bundle.php [Target directory] <Library directory>\n\n".
+         "Note: If the library directory is ommited, the path will be deducted\n";
     exit(1);
+} else if (count($argv) == 3) {
+    $doctrineBaseDir = $argv[2];
+} else {
+    $doctrineBaseDir = str_replace('tools/cli', 'lib', $_SERVER['PWD'], $Cnt);
+
+    if ($Cnt != 1) {
+        echo "Can't find library directory, please specify it as an argument\n";
+        exit(1);
+    }
 }
 
-$doctrineBaseDir = $argv[1];
-$targetDir = $argv[2];
+$targetDir = $argv[1];
+
+print "target: $targetDir\n";
+print "base dire: $doctrineBaseDir\n";
 
 set_include_path(get_include_path() . PATH_SEPARATOR . $doctrineBaseDir);
 
