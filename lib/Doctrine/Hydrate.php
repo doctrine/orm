91be68810cd838a79525811d8032b8d531ab6f64
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-08-14 17:01:06 +0000

    Removed unecessary change to Hydrate.php

--- lib/Doctrine/Hydrate.php
+++ lib/Doctrine/Hydrate.php
@@ -179,8 +179,6 @@ class Doctrine_Hydrate extends Doctrine_Object implements Serializable
      */
     public function getRootAlias()
     {
-        $this->getSql();
-        
         reset($this->_aliasMap);
 
         return key($this->_aliasMap);
