7fc767794e6663af42133fc59f70a0cf9ec4ebfc
jackbravo <jackbravo@625475ce-881a-0410-a577-b389adb331d8>
2007-08-24 22:00:05 +0000

    Amend: this was not yet to be commited

--- lib/Doctrine/Connection.php
+++ lib/Doctrine/Connection.php
@@ -469,10 +469,7 @@ abstract class Doctrine_Connection extends Doctrine_Configurable implements Coun
             return false;
         }
         // column names are specified as array keys
-        $cols = array();
-        foreach ($values as $key => $value) {
-            $cols[] = $this->quoteIdentifier($key);
-        }
+        $cols = array_keys($values);
 
         // build the statement
         $query = 'INSERT INTO ' . $this->quoteIdentifier($table) 
