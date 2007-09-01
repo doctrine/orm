f742b875a97ccbedd9500551e0bef8fe8f5ecaf0
jepso <jepso@625475ce-881a-0410-a577-b389adb331d8>
2007-09-01 00:28:28 +0000

    updated method that checks for pdflatex

--- vendor/Sensei/Sensei/Doc/Renderer/Pdf.php
+++ vendor/Sensei/Sensei/Doc/Renderer/Pdf.php
@@ -50,8 +50,9 @@ class Sensei_Doc_Renderer_Pdf extends Sensei_Doc_Renderer
     
     public function testRequirements()
     {
-        exec($this->_options['pdflatex_path'] . ' --version', $output);
-        if ( ! isset($output[0]) || substr($output[0], 0, 6) !== 'pdfTeX') {
+        exec($this->_options['pdflatex_path'], $output);
+        
+        if ( ! isset($output[0]) || ! preg_match('/^This is pdfTeXk/', $output[0])) {
             $message = 'pdfLaTeX does not seem to be installed, or pdflatex_path'
                      . ' option does not point to pdfLaTeX executable.';
             throw new Sensei_Doc_Renderer_Exception($message);
