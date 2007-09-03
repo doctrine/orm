dc17fba20ca19bb123249b56ad475a86170a11c4
jackbravo <jackbravo@625475ce-881a-0410-a577-b389adb331d8>
2007-09-03 15:32:30 +0000

    Error while running tests: Entity class not found

--- models/Group.php
+++ models/Group.php
@@ -2,6 +2,8 @@
 // grouptable doesn't extend Doctrine_Table -> Doctrine_Connection
 // won't initialize grouptable when Doctrine_Connection->getTable('Group') is called
 
+require_once('Entity.php');
+
 class GroupTable { }
 class Group extends Entity {
     public function setUp() {
