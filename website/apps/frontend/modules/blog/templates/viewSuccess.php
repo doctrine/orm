e7c3c9b333e91b7648891057b0c4722ea60b18c7
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-09-11 22:01:50 +0000

    Css formatting.

--- website/apps/frontend/modules/blog/templates/viewSuccess.php
+++ website/apps/frontend/modules/blog/templates/viewSuccess.php
@@ -1,9 +1,11 @@
 <?php use_helper('Date'); ?>
 
-<h1><?php echo $blogPost->getName(); ?></h1>
-<h3>Posted <?php echo distance_of_time_in_words(strtotime($blogPost->getCreatedAt())); ?> ago.</h3>
+<div class="content" id="blog_post">
+  <h1><?php echo $blogPost->getName(); ?></h1>
+  <h3>Posted <?php echo distance_of_time_in_words(strtotime($blogPost->getCreatedAt())); ?> ago.</h3>
 
-<p><?php echo $blogPost->getBody(); ?></p>
+  <p><?php echo $blogPost->getBody(); ?></p>
+</div>
 
 <?php slot('right'); ?>
   <input type="button" name="back_to_blog" value="Back to Blog" onClick="javascript: location.href = '<?php echo url_for('@blog'); ?>';" />
