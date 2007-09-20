370f6495738f35e757bc2ee12ecb746a7ec52575
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-09-20 15:03:44 +0000

    Made blank again.

--- playground/index.php
+++ playground/index.php
@@ -3,9 +3,3 @@ require_once('playground.php');
 require_once('connection.php');
 require_once('models.php');
 require_once('data.php');
-
-Doctrine_Data::exportData('data/data.yml', 'yml', $tables);
-
-//Doctrine_Data::importData('data/data.yml', 'yml', $tables);
-
-//Doctrine_Data::exportData('data/test.yml', 'yml', $tables);
