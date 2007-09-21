b574c7209e6c2efbda92b4b170e59fdaed168f99
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-09-21 20:35:09 +0000

    Clean up.

--- playground/index.php
+++ playground/index.php
@@ -3,8 +3,3 @@ require_once('playground.php');
 require_once('connection.php');
 require_once('models.php');
 require_once('data.php');
-
-$tables['Test'] = 'Test';
-
-$import = new Doctrine_Import_Schema();
-$import->importSchema('../tests/schema.yml', 'yml', 'test_models', $tables);
