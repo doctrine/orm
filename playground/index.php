1d3cdd0ab643bb6ba73496acf27251ca03c6a62b
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-09-24 22:29:52 +0000

    Removed test code.

--- playground/index.php
+++ playground/index.php
@@ -1,27 +1,5 @@
 <?php
 require_once('playground.php');
-
-$action = isset($_REQUEST['action']) ? $_REQUEST['action']:'client';
-
-if ($action == 'server') {
-    require_once('connection.php');
-    require_once('models.php');
-    require_once('data.php');
-    
-    $config = array('name'      =>  'Doctrine_Resource_Test_Server',
-                    'models'    =>  $tables);
-    
-    $server = Doctrine_Resource_Server::getInstance($config);
-    $server->run($_REQUEST);
-    
-} else {
-    $config = array('url' => 'http://localhost/~jwage/doctrine_trunk/playground/index.php?action=server');
-    
-    $client = Doctrine_Resource_Client::getInstance($config);
-    
-    //$table = $client->getTable('User');
-    
-    $query = new Doctrine_Resource_Query();
-    
-    $users = $query->from('User u, u.Phonenumber p, u.Address a, u.Book b, b.Author a')->execute();
-}
+require_once('connection.php');
+require_once('models.php');
+require_once('data.php');
