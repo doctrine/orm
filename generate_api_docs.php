c0818ef24a68a9235436bbd4ef09ae33eaeb7b05
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-10-08 15:16:49 +0000

    Removed. Not needed anymore.

--- generate_api_docs.php
+++
@@ -1,9 +0,0 @@
-<?php
-$output_directory = "api_documentation/trunk";
-$parse_directory = "lib";
-$template = 'HTML:Smarty:Doctrine';
-$title = 'Doctrine Documentation';
-
-$command = "phpdoc -pp on -s on -dn Doctrine -d $parse_directory -ti $title -t $output_directory -o $template";
-
-@exec($command);
