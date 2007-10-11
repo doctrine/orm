26945749c3ec7252f8c20b7e1de9a7835b11abfb
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-10-11 03:25:25 +0000

    Removed.

--- playground/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$dbh = new PDO('sqlite::memory:');
-$conn = Doctrine_Manager::connection($dbh);
-$manager = Doctrine_Manager::getInstance();
--- playground/playground.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'doctrine/Doctrine.php';
-
-spl_autoload_register(array('Doctrine', 'autoload'));
-
-error_reporting(E_ALL | E_STRICT);
