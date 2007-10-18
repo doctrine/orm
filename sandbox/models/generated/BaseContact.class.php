90fd42291e30bcfa83b2b1693ed892244f2209a5
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-10-18 04:03:33 +0000

    Updates to sandbox and manual.

--- sandbox/models/generated/BaseContact.class.php
+++ sandbox/models/generated/BaseContact.class.php
@@ -8,14 +8,25 @@ abstract class BaseContact extends Doctrine_Record
 
 	public function setTableDefinition()
 	{
-        $this->setTableName('contact');
-        $this->hasColumn('name', 'string', 255);
+		$this->setTableName('contact');
+		$this->hasColumn('id', 'integer', 11, array('primary' => true,
+                                              'autoincrement' => true));
+		$this->hasColumn('name', 'string', 255);
+
+
+
+
+
 	}
 
 	public function setUp()
 	{
-        $this->hasMany('User', array('local' => 'id',
-                                     'foreign' => 'contact_id'));
+		$this->hasOne('Adult', array('local' => 'id',
+                               'foreign' => 'contact_id'));
+
+		$this->hasOne('User', array('local' => 'id',
+                              'foreign' => 'contact_id'));
+
 	}
 
 }
