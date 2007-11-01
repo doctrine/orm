146098c3d6608e7b75af98c93b58cbf95731c81c
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-11-01 15:15:57 +0000

    moved around and initial entry of sluggable code.

---
+++ lib/Doctrine/Template/Listener/Sluggable.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.com>.
+ */
+
+/**
+ * Doctrine_Template_Listener_Sluggable
+ *
+ * Easily create a slug for each record based on a specified set of fields
+ *
+ * @package     Doctrine
+ * @subpackage  Template
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.phpdoctrine.com
+ * @since       1.0
+ * @version     $Revision$
+ * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
+ */
+class Doctrine_Template_Listener_Sluggable extends Doctrine_Record_Listener
+{
+    /**
+     * Array of timestampable options
+     *
+     * @var string
+     */
+    protected $_options = array('name'    =>  'slug',
+                                'type'    =>  'clob',
+                                'length'  =>  null,
+                                'options' =>  array(),
+                                'columns' =>  array());
+    
+    /**
+     * __construct
+     *
+     * @param string $array 
+     * @return void
+     */
+    public function __construct(array $options)
+    {
+        $this->_options = array_merge($options, $this->_options);
+    }
+
+    public function preInsert(Doctrine_Event $event)
+    {
+        $name = $this->_options['name'];
+
+        $record = $event->getInvoker();
+
+        $record->$name = $this->buildSlug($record);
+    }
+
+    protected function buildSlug($record)
+    {
+        if (empty($this->_columns)) {
+            $value = (string) $record;
+        } else {
+            $value = '';
+            foreach ($this->_columns as $column) {
+              $value = $record->$column . ' ';
+            }
+        }
+
+        $value = trim($value);
+        $value = strtolower($value);
+
+        // strip all non word chars
+        $value = preg_replace('/\W/', ' ', $value);
+
+        // replace all white space sections with a dash
+        $value = preg_replace('/\ +/', '-', $value);
+
+        // trim dashes
+        $value = preg_replace('/\-$/', '', $value);
+        $value = preg_replace('/^\-/', '', $value);
+
+        return $value;
+    }
+}
