f81e4fdcc77f0aa33f75c4878cf3e5f827f920a6
meus <meus@625475ce-881a-0410-a577-b389adb331d8>
2007-11-09 00:19:14 +0000

    Added failing testcase references #589

---
+++ models/Ticket589.php
@@ -0,0 +1,11 @@
+<?php
+class Ticket589 extends Doctrine_Record {
+
+    public function setTableDefinition() {
+        $this->hasColumn('id', 'integer', 4, array('notnull' => true,
+            'primary' => true,
+            'unsigned' > true,
+            'autoincrement' => true));
+        $this->hasColumn('name', 'string',50);
+    }
+}
