c13340367082b32c719ed7ca7758ec4476dc4dee
meus <meus@625475ce-881a-0410-a577-b389adb331d8>
2007-11-09 01:17:03 +0000

    fixed typo in model

--- models/Ticket589.php
+++ models/Ticket589.php
@@ -1,10 +1,12 @@
 <?php
 class Ticket589 extends Doctrine_Record {
 
+    public function prepareTables(){}
+
     public function setTableDefinition() {
         $this->hasColumn('id', 'integer', 4, array('notnull' => true,
             'primary' => true,
-            'unsigned' > true,
+            'unsigned' => true,
             'autoincrement' => true));
         $this->hasColumn('name', 'string',50);
     }
