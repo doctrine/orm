3829b0f2d4afb901cbd7c2fef3da79e5dc236ec4
zYne <zYne@625475ce-881a-0410-a577-b389adb331d8>
2007-11-25 20:23:53 +0000

    fixed some tests

--- tests/ClassTableInheritanceTestCase.php
+++ tests/ClassTableInheritanceTestCase.php
@@ -125,12 +125,12 @@ class Doctrine_ClassTableInheritance_TestCase extends Doctrine_UnitTestCase
         $q = new Doctrine_Query();
         $q->from('CTITest c')->where("c.name = 'Jack'");
 
-        $this->assertEqual($q->getSql(), "SELECT c.id AS c__id, c2.name AS c__name, c2.verified AS c__verified, c3.added AS c__added, c.age AS c__age FROM c_t_i_test_parent4 c LEFT JOIN c_t_i_test_parent2 c2 ON c.id = c2.id LEFT JOIN c_t_i_test_parent3 c3 ON c.id = c3.id WHERE c2.name = 'Jack'");
+        $this->assertEqual($q->getSql(), "SELECT c.id AS c__id, c3.added AS c__added, c2.name AS c__name, c2.verified AS c__verified, c.age AS c__age FROM c_t_i_test_parent4 c LEFT JOIN c_t_i_test_parent2 c2 ON c.id = c2.id LEFT JOIN c_t_i_test_parent3 c3 ON c.id = c3.id WHERE c2.name = 'Jack'");
 
         $q = new Doctrine_Query();
         $q->from('CTITest c')->where("name = 'Jack'");
 
-        $this->assertEqual($q->getSql(), "SELECT c.id AS c__id, c2.name AS c__name, c2.verified AS c__verified, c3.added AS c__added, c.age AS c__age FROM c_t_i_test_parent4 c LEFT JOIN c_t_i_test_parent2 c2 ON c.id = c2.id LEFT JOIN c_t_i_test_parent3 c3 ON c.id = c3.id WHERE c2.name = 'Jack'");
+        $this->assertEqual($q->getSql(), "SELECT c.id AS c__id, c3.added AS c__added, c2.name AS c__name, c2.verified AS c__verified, c.age AS c__age FROM c_t_i_test_parent4 c LEFT JOIN c_t_i_test_parent2 c2 ON c.id = c2.id LEFT JOIN c_t_i_test_parent3 c3 ON c.id = c3.id WHERE c2.name = 'Jack'");
     }
 
     public function testFetchingCtiRecordsSupportsLimitSubqueryAlgorithm()
