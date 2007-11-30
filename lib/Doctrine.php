9f20184fc4486ebc297e547f64d38dbb058d98c2
Jonathan.Wage <Jonathan.Wage@625475ce-881a-0410-a577-b389adb331d8>
2007-11-30 23:39:32 +0000

    Updated version.

--- lib/Doctrine.php
+++ lib/Doctrine.php
@@ -36,7 +36,7 @@ final class Doctrine
     /**
      * VERSION
      */
-    const VERSION                   = '0.1.0';
+    const VERSION                   = '1.0.0';
 
     /**
      * ERROR CONSTANTS
