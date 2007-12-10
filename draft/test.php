f1653229eeac346e494a613439ba256c1998a126
jepso <jepso@625475ce-881a-0410-a577-b389adb331d8>
2007-12-10 20:40:06 +0000

    fixed new dql parser test script

--- draft/test.php
+++ draft/test.php
@@ -1,19 +1,30 @@
 <?php
-require_once 'Doctrine.php';
+function autoload($className)
+{
+    if (class_exists($className, false)) {
+        return false;
+    }
 
-spl_autoload_register(array('Doctrine', 'autoload'));
+    $class = dirname(__FILE__) . DIRECTORY_SEPARATOR
+           . str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    if (file_exists($class)) {
+        require_once($class);
+
+        return true;
+    }
+
+    return false;
+}
+
+spl_autoload_register('autoload');
 
 $n = 1000;
 
 $start = microtime(true);
 for ($i = 0; $i < $n; $i++) {
-/*    $parser = new Doctrine_Query_Parser('SELECT u.name, u.age FROM User u WHERE u.id = ?');
-    $parser->parse();*/
-    $scanner = new Doctrine_Query_Scanner('SELECT u.name, u.age FROM User u WHERE u.id = ?');
-    do {
-        $token = $scanner->scan();
-    } while ($token['type'] !== Doctrine_Query_Token::T_EOS);
-
+    $parser = new Doctrine_Query_Parser('SELECT u.name, u.age FROM User u WHERE u.id = ?');
+    $parser->parse();
 }
 $end = microtime(true);
 
