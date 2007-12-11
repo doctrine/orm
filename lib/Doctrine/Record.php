ab8819ca664c522580a986626245ea053169a600
jackbravo <jackbravo@625475ce-881a-0410-a577-b389adb331d8>
2007-12-11 18:25:34 +0000

    Related to bug #662

--- lib/Doctrine/Record.php
+++ lib/Doctrine/Record.php
@@ -1278,7 +1278,7 @@ abstract class Doctrine_Record extends Doctrine_Record_Abstract implements Count
         foreach ($array as $key => $value) {
             if ($this->getTable()->hasRelation($key)) {
                 $this->get($key)->synchronizeWithArray($value);
-            } else if ($this->getTable()->hasColumn($key)) {
+            } else if ($this->getTable()->hasField($key)) {
                 $this->set($key, $value);
             }
         }
