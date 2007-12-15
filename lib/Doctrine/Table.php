51c88e2020bfc9e05649a484598cb442ae427f2a
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2007-12-15 19:19:31 +0000

    Merged 3405 (Fixed #626) from 1.0 branch.

--- lib/Doctrine/Table.php
+++ lib/Doctrine/Table.php
@@ -1130,7 +1130,7 @@ class Doctrine_Table extends Doctrine_Configurable implements Countable
         $id = is_array($id) ? array_values($id) : array($id);
 
         return $this->createQuery()
-            ->where(implode(' = ? AND ', $this->getIdentifierColumnNames()) . ' = ?')
+            ->where(implode(' = ? AND ', (array) $this->getIdentifier()) . ' = ?')
             ->fetchOne($id, $hydrationMode);
     }
 
