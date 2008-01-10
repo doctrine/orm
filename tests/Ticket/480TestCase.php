e98035999ab2695cf9d4879449d79caa3f6a3454
lsmith <lsmith@625475ce-881a-0410-a577-b389adb331d8>
2008-01-10 13:16:00 +0000

    - ws fixes

--- tests/Ticket/480TestCase.php
+++ tests/Ticket/480TestCase.php
@@ -30,36 +30,33 @@
  * @since       1.0
  * @version     $Revision$
  */
- 
+
 class stComment extends Doctrine_Record
 {
-	public function setTableDefinition()
-	{
-		$this->setTableName('st_comment');
-		$this->hasColumn('title', 'string', 100, array());
-		$this->hasColumn('body', 'string', 1000, array());
-
-	}
+    public function setTableDefinition()
+    {
+        $this->setTableName('st_comment');
+        $this->hasColumn('title', 'string', 100, array());
+        $this->hasColumn('body', 'string', 1000, array());
+    }
 }
 
-class Doctrine_Ticket_480_TestCase extends Doctrine_UnitTestCase 
+class Doctrine_Ticket_480_TestCase extends Doctrine_UnitTestCase
 {
-	
-	public function testInit()
-	{
+    public function testInit()
+    {
                 $this->dbh = new Doctrine_Adapter_Mock('oracle');
                 $this->conn = Doctrine_Manager::getInstance()->openConnection($this->dbh);
-	}
-	
-	public function testTicket()
-	{
-	    $this->conn->export->exportClasses(array('stComment'));
-	    $queries = $this->dbh->getAll();
+    }
 
-	    // (2nd|1st except transaction init.) executed query must be CREATE TABLE or CREATE SEQUENCE, not CREATE TRIGGER
-	    // Trigger can be created after both CREATE TABLE and CREATE SEQUENCE
-	    $this->assertFalse(preg_match('~^CREATE TRIGGER.*~', $queries[1]));
-	    $this->assertFalse(preg_match('~^CREATE TRIGGER.*~', $queries[2]));
+    public function testTicket()
+    {
+        $this->conn->export->exportClasses(array('stComment'));
+        $queries = $this->dbh->getAll();
 
-	}
+        // (2nd|1st except transaction init.) executed query must be CREATE TABLE or CREATE SEQUENCE, not CREATE TRIGGER
+        // Trigger can be created after both CREATE TABLE and CREATE SEQUENCE
+        $this->assertFalse(preg_match('~^CREATE TRIGGER.*~', $queries[1]));
+        $this->assertFalse(preg_match('~^CREATE TRIGGER.*~', $queries[2]));
+    }
 }
