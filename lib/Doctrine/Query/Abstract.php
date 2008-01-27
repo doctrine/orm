ebbfcf4caddfa6d342a1fcc5d59e097b824508ef
pookey <pookey@625475ce-881a-0410-a577-b389adb331d8>
2008-01-27 15:02:45 +0000

    a previous commit of mine broke method chaining

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -1139,7 +1139,7 @@ abstract class Doctrine_Query_Abstract
 
         // if there's no params, return (else we'll get a WHERE IN (), invalid SQL)
         if (!count($params))
-          return;
+          return $this;
 
         $a = array();
         foreach ($params as $k => $value) {
