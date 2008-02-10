57a5101e0f195fd6ac046b19261fdd5d5279ebe1
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2008-02-10 15:10:29 +0000

    changes on the new test suite.

--- tests/Orm/Component/TestTest.php
+++ tests/Orm/Component/TestTest.php
@@ -12,4 +12,19 @@ class Orm_Component_TestTest extends Doctrine_OrmTestCase
     {
         $this->assertEquals(0, 0);
     }
+    
+    public function testFixture()
+    {
+        $forumUsers = $this->sharedFixture['connection']->query("FROM ForumUser u");
+        $this->assertEquals(2, count($forumUsers));
+        $forumUsers[0]->delete();
+        unset($forumUsers[0]);
+        $this->assertEquals(1, count($forumUsers));
+    }
+    
+    public function testFixture2()
+    {
+        $forumUsers = $this->sharedFixture['connection']->query("FROM ForumUser u");
+        $this->assertEquals(2, count($forumUsers));
+    }
 }
