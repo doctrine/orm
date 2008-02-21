499eaf6cb238d84bd2a6e642b48166c3e87035a8
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-02-21 15:18:30 +0000

    Merged r3868 in 0.10 and trunk

--- lib/Doctrine/Builder/Record.php
+++ lib/Doctrine/Builder/Record.php
@@ -286,7 +286,7 @@ END;
 
         $i = 0;
 
-        if (isset($definition['inheritance']['extends']) && !(isset($definition['override_parent']) && $definition['override_parent'] == false)) {
+        if (isset($definition['inheritance']['extends']) && ! (isset($definition['override_parent']) && $definition['override_parent'] == true)) {
             $ret[$i] = "    parent::setTableDefinition();";
             $i++;
         }
@@ -343,7 +343,7 @@ END;
         $ret = array();
         $i = 0;
 
-        if (isset($definition['inheritance']['extends']) && !(isset($definition['override_parent']) && $definition['override_parent'] == false)) {
+        if (isset($definition['inheritance']['extends']) && ! (isset($definition['override_parent']) && $definition['override_parent'] == true)) {
             $ret[$i] = "    parent::setUp();";
             $i++;
         }
@@ -710,7 +710,7 @@ END;
             $baseClass = $definition;
             $baseClass['className'] = 'Base' . $baseClass['className'];
             $baseClass['abstract'] = true;
-            $baseClass['override_parent'] = true;
+            $baseClass['override_parent'] = false;
             $baseClass['is_base_class'] = true;
 
             $this->writeDefinition($baseClass);
