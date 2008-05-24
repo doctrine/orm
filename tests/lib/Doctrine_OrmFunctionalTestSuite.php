f8017c9c735cc34087c0c2a0bbc8114939a2cdc6
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2008-05-24 17:37:26 +0000

    Added two missing classes.

---
+++ tests/lib/Doctrine_OrmFunctionalTestSuite.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * The outermost test suite for all orm related testcases & suites.
+ *
+ * Currently the orm suite uses a normal connection object.
+ * Upon separation of the DBAL and ORM package this suite should just use a orm
+ * connection/session/manager instance as the shared fixture.
+ */
+class Doctrine_OrmFunctionalTestSuite extends Doctrine_OrmTestSuite
+{
+    protected function setUp()
+    {
+        $this->sharedFixture['em'] = new Doctrine_EntityManager(
+                Doctrine_TestUtil::getConnection());
+    }
+    
+    protected function tearDown()
+    {} 
+}
