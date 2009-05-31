de7dfb4fcba0ee7a1996ea4dc85085b9587d49ce
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2009-05-31 11:51:36 +0000

    [2.0] Cleaned up TestInit include path.

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -10,13 +10,8 @@ require_once '../lib/Doctrine/Common/ClassLoader.php';
 
 $classLoader = new \Doctrine\Common\ClassLoader();
 
-$modelDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'models';
 set_include_path(
     get_include_path()
     . PATH_SEPARATOR . __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib'
-    . PATH_SEPARATOR . $modelDir . DIRECTORY_SEPARATOR . 'cms'
-    . PATH_SEPARATOR . $modelDir . DIRECTORY_SEPARATOR . 'company'
-    . PATH_SEPARATOR . $modelDir . DIRECTORY_SEPARATOR . 'ecommerce'
-    . PATH_SEPARATOR . $modelDir . DIRECTORY_SEPARATOR . 'forum'
 );
 
