7a79785dffaaf22d1d1620aef676e0b7f623782b
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-07-15 15:04:59 +0000

    [2.0] Removing old unused code from Addendum

--- lib/Doctrine/Common/Annotations/Annotation.php
+++ lib/Doctrine/Common/Annotations/Annotation.php
@@ -5,33 +5,11 @@ namespace Doctrine\Common\Annotations;
 class Annotation
 {
     public $value;
-    private static $creationStack = array();
 
     public final function __construct(array $data)
     {
-        $reflection = new \ReflectionClass($this);
-        $class = $reflection->getName();
-        if (isset(self::$creationStack[$class])) {
-            trigger_error("Circular annotation reference on '$class'", E_USER_ERROR);
-            return;
-        }
-        self::$creationStack[$class] = true;
         foreach ($data as $key => $value) {
             $this->$key = $value;
         }
-        unset(self::$creationStack[$class]);
     }
-
-    private function createName($target)
-    {
-        if ($target instanceof ReflectionMethod) {
-            return $target->getDeclaringClass()->getName().'::'.$target->getName();
-        } else if ($target instanceof ReflectionProperty) {
-            return $target->getDeclaringClass()->getName().'::$'.$target->getName();
-        } else {
-            return $target->getName();
-        }
-    }
-
-    //protected function checkConstraints($target) {}
 }
