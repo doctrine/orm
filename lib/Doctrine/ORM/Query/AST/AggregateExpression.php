3949fddcf572dd0b4d38cedb67868e39c3568a11
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2009-08-06 15:35:14 +0000

    [2.0] Added missing quoteIdentifiers to SqlWalker

--- lib/Doctrine/ORM/Query/AST/AggregateExpression.php
+++ lib/Doctrine/ORM/Query/AST/AggregateExpression.php
@@ -28,30 +28,15 @@ namespace Doctrine\ORM\Query\AST;
  */
 class AggregateExpression extends Node
 {
-    private $_functionName;
-    private $_pathExpression;
-    private $_isDistinct = false; // Some aggregate expressions support distinct, eg COUNT
+    public $functionName;
+    public $pathExpression;
+    public $isDistinct = false; // Some aggregate expressions support distinct, eg COUNT
 
     public function __construct($functionName, $pathExpression, $isDistinct)
     {
-        $this->_functionName = $functionName;
-        $this->_pathExpression = $pathExpression;
-        $this->_isDistinct = $isDistinct;
-    }
-
-    public function getPathExpression()
-    {
-        return $this->_pathExpression;
-    }
-
-    public function isDistinct()
-    {
-        return $this->_isDistinct;
-    }
-
-    public function getFunctionName()
-    {
-        return $this->_functionName;
+        $this->functionName = $functionName;
+        $this->pathExpression = $pathExpression;
+        $this->isDistinct = $isDistinct;
     }
 
     public function dispatch($sqlWalker)
