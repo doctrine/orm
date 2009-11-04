705a477067e7a68c834ed65605d8d230677b5eaf
beberlei <beberlei@625475ce-881a-0410-a577-b389adb331d8>
2009-11-04 22:16:22 +0000

    [2.0] Fix bin/doctrine.php using unknown ClassLoader to use GlobalClassLoader instead

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -1,8 +1,9 @@
 <?php
 
-require 'Doctrine/Common/ClassLoader.php';
+require 'Doctrine/Common/GlobalClassLoader.php';
 
-$classLoader = new \Doctrine\Common\ClassLoader();
+$classLoader = new \Doctrine\Common\GlobalClassLoader();
+$classLoader->register();
 
 $cli = new \Doctrine\ORM\Tools\Cli();
 $cli->run($_SERVER['argv']);
