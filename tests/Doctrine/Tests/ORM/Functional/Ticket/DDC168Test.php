59c6449076241e052b1dc8e6b404a1ee829bcecc
beberlei <beberlei@625475ce-881a-0410-a577-b389adb331d8>
2009-12-07 21:20:04 +0000

    [2.0] DDC-168 - Add test-case that proves that the order of reflFields matters for inserting, which causes problems with caching.

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC168Test.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+require_once __DIR__ . '/../../../TestInit.php';
+
+class DDC168Test extends \Doctrine\Tests\OrmFunctionalTestCase
+{
+    protected function setUp() {
+        $this->useModelSet('company');
+        parent::setUp();
+    }
+
+    public function testJoinedSubclassPersisterRequiresSpecificOrderOfMetadataReflFieldsArray()
+    {
+        $metadata = $this->_em->getClassMetadata('Doctrine\Tests\Models\Company\CompanyEmployee');
+        ksort($metadata->reflFields);
+
+        $spouse = new \Doctrine\Tests\Models\Company\CompanyEmployee();
+        $spouse->setName("Blub");
+
+        $employee = new \Doctrine\Tests\Models\Company\CompanyEmployee();
+        $employee->setName("Foo");
+        $employee->setDepartment("bar");
+        $employee->setSalary(1000);
+        $employee->setSpouse($spouse);
+
+        $this->_em->persist($spouse);
+        $this->_em->persist($employee);
+
+        $this->_em->flush();
+        $this->_em->clear();
+
+        $q = $this->_em->createQuery("SELECT e FROM Doctrine\Tests\Models\Company\CompanyEmployee e WHERE e.name = ?1");
+        $q->setParameter(1, "Foo");
+        $theEmployee = $q->getSingleResult();
+
+        $this->assertEquals("bar", $theEmployee->getDepartment());
+    }
+}
