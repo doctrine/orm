cfb42d2eb9cb148fd539ba5a5fb2bbc5b7abcfe4
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2010-01-27 19:20:47 +0000

    [2.0] Fix for CLI Configuration and PEAR installer

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -5,7 +5,25 @@ require_once 'Doctrine/Common/ClassLoader.php';
 $classLoader = new \Doctrine\Common\ClassLoader('Doctrine');
 $classLoader->register();
 
+$configFile = getcwd() . DIRECTORY_SEPARATOR . 'cli-config.php';
 $configuration = new \Doctrine\Common\Cli\Configuration();
+
+if (file_exists($configFile)) {
+    if ( ! is_readable($configFile)) {
+        trigger_error(
+            'Configuration file [' . $configFile . '] does not have read permission.', E_ERROR
+        );
+    }
+
+    require $configFile;
+    
+    foreach ($GLOBALS as $configCandidate) {
+        if ($configCandidate instanceof \Doctrine\Common\Cli\Configuration) {
+            $configuration = $configCandidate;
+            break;
+        }
+    }
+}
 
 $cli = new \Doctrine\Common\Cli\CliController($configuration);
 $cli->run($_SERVER['argv']);
