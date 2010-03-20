46e7724988f412306f21f151fa72be3b743f87cf
beberlei <beberlei@625475ce-881a-0410-a577-b389adb331d8>
2010-03-20 17:37:18 +0000

    [2.0] Added missing test-case to Doctrine\Tests\ORM\Functional\AllTests

--- tests/Doctrine/Tests/ORM/Functional/AllTests.php
+++ tests/Doctrine/Tests/ORM/Functional/AllTests.php
@@ -48,6 +48,7 @@ class AllTests
         $suite->addTestSuite('Doctrine\Tests\ORM\Functional\ReferenceProxyTest');
         $suite->addTestSuite('Doctrine\Tests\ORM\Functional\LifecycleCallbackTest');
         $suite->addTestSuite('Doctrine\Tests\ORM\Functional\StandardEntityPersisterTest');
+        $suite->addTestSuite('Doctrine\Tests\ORM\Functional\CustomTreeWalkersTest');
         $suite->addTestSuite('Doctrine\Tests\ORM\Functional\MappedSuperclassTest');
         $suite->addTestSuite('Doctrine\Tests\ORM\Functional\EntityRepositoryTest');
         $suite->addTestSuite('Doctrine\Tests\ORM\Functional\IdentityMapTest');
