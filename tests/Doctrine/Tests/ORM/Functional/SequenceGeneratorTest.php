1e7ca2bd7a8308e79a77337c76e3b522e61cf397
beberlei <beberlei@625475ce-881a-0410-a577-b389adb331d8>
2010-03-20 17:04:46 +0000

    [2.0] DDC-390 Fix SequenceGeneratorTest which wasnt testing anything before :-) Now showing that Sequence Generator works with allocation sizes larger than 1

--- tests/Doctrine/Tests/ORM/Functional/SequenceGeneratorTest.php
+++ tests/Doctrine/Tests/ORM/Functional/SequenceGeneratorTest.php
@@ -11,24 +11,43 @@ require_once __DIR__ . '/../../TestInit.php';
  */
 class SequenceGeneratorTest extends \Doctrine\Tests\OrmFunctionalTestCase
 {
-    public function testFoo()
+    public function setUp()
     {
-        $this->assertEquals(1, 1);
+        parent::setUp();
+
+        if (!$this->_em->getConnection()->getDatabasePlatform()->supportsSequences()) {
+            $this->markTestSkipped('Only working for Databases that support sequences.');
+        }
+
+        try {
+            $this->_schemaTool->createSchema(array(
+                $this->_em->getClassMetadata(__NAMESPACE__ . '\SequenceEntity'),
+            ));
+        } catch(\Exception $e) {
+
+        }
+    }
+
+    public function testHighAllocationSizeSequence()
+    {
+        for ($i = 0; $i < 11; $i++) {
+            $e = new SequenceEntity();
+            $this->_em->persist($e);
+        }
+        $this->_em->flush();
     }
 }
 
 /**
  * @Entity
  */
-class SeqUser {
+class SequenceEntity
+{
     /**
      * @Id
-     * @IdGenerator("sequence")
+     * @column(type="integer")
+     * @GeneratedValue(strategy="SEQUENCE")
+     * @SequenceGenerator(allocationSize=5,sequenceName="person_id_seq")
      */
-    private $id;
-
-    public function getId() {
-        return $this->id;
-    }
+    public $id;
 }
-
