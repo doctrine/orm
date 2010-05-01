302409dba49ebc59a47ddc87b325f56cf184e4cd
Benjamin Eberlei <kontakt@beberlei.de>
2010-05-01 13:51:29 +0200

    Fixed Error due to merging tests/Doctrine/Tests/TestInit.php file

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -13,6 +13,9 @@ require_once __DIR__ . '/../../../lib/Doctrine/Common/ClassLoader.php';
 $classLoader = new \Doctrine\Common\ClassLoader('Doctrine');
 $classLoader->register();
 
+$classLoader = new \Doctrine\Common\ClassLoader('Symfony', __DIR__ . "/../../../lib/vendor");
+$classLoader->register();
+
 if (!file_exists(__DIR__."/Proxies")) {
     if (!mkdir(__DIR__."/Proxies")) {
         throw new Exception("Could not create " . __DIR__."/Proxies Folder.");
