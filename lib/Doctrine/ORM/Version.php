c8e20aa2179cca7606d9a24ec68ce94a3fedfa27
Roman S. Borschel <roman.borschel@googlemail.com>
2010-06-13 19:56:43 +0200

    Bumping dev version.

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.0.0BETA2-DEV';
+    const VERSION = '2.0.0BETA3-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
