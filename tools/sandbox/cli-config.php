bead9b8b4acf6002e8ff618276dbcea25422cade
Jonathan H. Wage <jonwage@gmail.com>
2010-06-17 14:32:26 -0400

    Fixing sandbox for source control code and removing it from the build.xml

--- tools/sandbox/cli-config.php
+++ tools/sandbox/cli-config.php
@@ -1,10 +1,17 @@
 <?php
 
-require_once __DIR__ . '/../../lib/Doctrine/Common/ClassLoader.php';
+require_once '../../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
 
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\\ORM', realpath(__DIR__ . '/../../lib'));
+$classLoader->register();
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\\DBAL', realpath(__DIR__ . '/../../lib/vendor/doctrine-dbal/lib'));
+$classLoader->register();
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\\Common', realpath(__DIR__ . '/../../lib/vendor/doctrine-common/lib'));
+$classLoader->register();
+$classLoader = new \Doctrine\Common\ClassLoader('Symfony', realpath(__DIR__ . '/../../lib/vendor'));
+$classLoader->register();
 $classLoader = new \Doctrine\Common\ClassLoader('Entities', __DIR__);
 $classLoader->register();
-
 $classLoader = new \Doctrine\Common\ClassLoader('Proxies', __DIR__);
 $classLoader->register();
 
