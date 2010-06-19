51e6681934a7cf4448b85c5670c04045f66c6056
Benjamin Eberlei <kontakt@beberlei.de>
2010-06-20 00:28:20 +0200

    DDC-646 - Bugfix with missing inclusion of Namespace, added test for ConvertDoctrine1SchemaCommand

---
+++ tests/Doctrine/Tests/ORM/Tools/Console/Command/ConvertDoctrine1SchemaCommandTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Tools\Console\Command;
+
+use Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand;
+
+class ConvertDoctrine1SchemaCommandTest extends \Doctrine\Tests\OrmTestCase
+{
+    public function testExecution()
+    {
+        $entityGenerator = $this->getMock('Doctrine\ORM\Tools\EntityGenerator');
+        $metadataExporter = $this->getMock('Doctrine\ORM\Tools\Export\ClassMetadataExporter');
+        $command = new ConvertDoctrine1SchemaCommand();
+        $command->setEntityGenerator($entityGenerator);
+
+        $output = $this->getMock('Symfony\Components\Console\Output\OutputInterface');
+        $output->expects($this->once())
+               ->method('write')
+               ->with($this->equalTo('No Metadata Classes to process.' . PHP_EOL));
+
+        $command->convertDoctrine1Schema($this->_getTestEntityManager(), array(), sys_get_temp_dir(), 'annotation', 4, null, $output);
+    }
+}
