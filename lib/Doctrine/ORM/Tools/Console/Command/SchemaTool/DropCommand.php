7ff9976b3cbb3b95ff4c83368be5ca8a40bce0f8
Benjamin Eberlei <kontakt@beberlei.de>
2010-08-31 23:42:27 +0200

    Made using schema-tool:drop and schema-tool:update more secure by requiring the user to confirm the operation with another flag --force.

--- lib/Doctrine/ORM/Tools/Console/Command/SchemaTool/DropCommand.php
+++ lib/Doctrine/ORM/Tools/Console/Command/SchemaTool/DropCommand.php
@@ -49,13 +49,17 @@ class DropCommand extends AbstractCommand
         $this
         ->setName('orm:schema-tool:drop')
         ->setDescription(
-            'Processes the schema and either drop the database schema of EntityManager Storage Connection or generate the SQL output.'
+            'Drop the complete database schema of EntityManager Storage Connection or generate the corresponding SQL output.'
         )
         ->setDefinition(array(
             new InputOption(
                 'dump-sql', null, InputOption::PARAMETER_NONE,
                 'Instead of try to apply generated SQLs into EntityManager Storage Connection, output them.'
-            )
+            ),
+            new InputOption(
+                'force', null, InputOption::PARAMETER_NONE,
+                "Don't ask for the deletion of the database, but force the operation to run."
+            ),
         ))
         ->setHelp(<<<EOT
 Processes the schema and either drop the database schema of EntityManager Storage Connection or generate the SQL output.
@@ -69,10 +73,19 @@ EOT
         if ($input->getOption('dump-sql') === true) {
             $sqls = $schemaTool->getDropSchemaSql($metadatas);
             $output->write(implode(';' . PHP_EOL, $sqls) . PHP_EOL);
-        } else {
+        } else if ($input->getOption('force') === true) {
             $output->write('Dropping database schema...' . PHP_EOL);
             $schemaTool->dropSchema($metadatas);
             $output->write('Database schema dropped successfully!' . PHP_EOL);
+        } else {
+            $sqls = $schemaTool->getDropSchemaSql($metadatas);
+
+            if (count($sqls)) {
+                $output->write('Schema-Tool would execute ' . count($sqls) . ' queries to drop the database.' . PHP_EOL);
+                $output->write('Please run the operation with --force to execute these queries or use --dump-sql to see them.' . PHP_EOL);
+            } else {
+                $output->write('Nothing to drop. The database is empty!' . PHP_EOL);
+            }
         }
     }
 }
