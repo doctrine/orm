2709f7730a6fcc3bae1f5ad5fa3b3158f10f8f1c
Timo A. Hummel <felicitus@felicitus.de>
2010-11-10 21:59:29 +0100

    Mistyped class name...notice to me: run AllTests and not only the single test.

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DDC873Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC873Test.php
@@ -6,7 +6,7 @@ use	Doctrine\ORM\Mapping\ClassMetadata;
 
 require_once __DIR__ . '/../../../TestInit.php';
 
-class DDC873 extends \Doctrine\Tests\OrmFunctionalTestCase
+class DDC873Test extends \Doctrine\Tests\OrmFunctionalTestCase
 {
     protected function setUp() {
         parent::setUp();
