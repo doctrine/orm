1f50dee8a84e8493f58fbb30052709e082455cce
Michael Ridgway <mcridgway@gmail.com>
2011-03-21 23:17:08 -0400

    DDC-696: Added onClear event

---
+++ tests/Doctrine/Tests/ORM/Functional/ClearEventTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Functional;
+
+use Doctrine\Tests\Models\CMS\CmsUser;
+use Doctrine\Tests\Models\CMS\CmsPhonenumber;
+use Doctrine\ORM\Event\OnClearEventArgs;
+use Doctrine\ORM\Events;
+
+require_once __DIR__ . '/../../TestInit.php';
+
+/**
+ * ClearEventTest
+ *
+ * @author Michael Ridgway
+ */
+class ClearEventTest extends \Doctrine\Tests\OrmFunctionalTestCase
+{
+    protected function setUp() {
+        parent::setUp();
+    }
+
+    public function testEventIsCalledOnClear()
+    {
+        $listener = new OnClearListener;
+        $this->_em->getEventManager()->addEventListener(Events::onClear, $listener);
+
+        $this->_em->clear();
+
+        $this->assertTrue($listener->called);
+    }
+}
+
+class OnClearListener
+{
+    public $called = false;
+
+    public function onClear(OnClearEventArgs $args)
+    {
+        $this->called = true;
+    }
+}
+
+
