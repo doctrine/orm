6ed0ff0a12f383ee192c1aca77931ad4bee90adf
Benjamin Eberlei <kontakt@beberlei.de>
2011-03-27 14:04:53 +0200

    [DDC-1079] Bugfix for shortcut for ArithmeticExpressions in SimpleSelectExpression that lead to literals not being valid. Problem was that ScalarExpression() did not handle AggregateExpressions() at all, which is now fixed.

--- lib/Doctrine/ORM/Query/Parser.php
+++ lib/Doctrine/ORM/Query/Parser.php
@@ -1644,7 +1644,7 @@ class Parser
             return $this->StateFieldPathExpression();
         } else if ($lookahead == Lexer::T_INTEGER || $lookahead == Lexer::T_FLOAT) {
             return $this->SimpleArithmeticExpression();
-        } else if ($this->_isFunction()) {
+        } else if ($this->_isFunction() || $this->_isAggregateFunction($this->_lexer->lookahead['type'])) {
             // We may be in an ArithmeticExpression (find the matching ")" and inspect for Math operator)
             $this->_lexer->peek(); // "("
             $peek = $this->_peekBeyondClosingParenthesis();
@@ -1652,8 +1652,12 @@ class Parser
             if ($this->_isMathOperator($peek)) {
                 return $this->SimpleArithmeticExpression();
             }
-            
-            return $this->FunctionDeclaration();
+
+            if ($this->_isAggregateFunction($this->_lexer->lookahead['type'])) {
+                return $this->AggregateExpression();
+            } else {
+                return $this->FunctionDeclaration();
+            }
         } else if ($lookahead == Lexer::T_STRING) {
             return $this->StringPrimary();
         } else if ($lookahead == Lexer::T_INPUT_PARAMETER) {
@@ -1797,15 +1801,8 @@ class Parser
         }
 
         $this->_lexer->peek();
-        $beyond = $this->_peekBeyondClosingParenthesis();
 
-        if ($this->_isMathOperator($beyond)) {
-            $expression = $this->ScalarExpression();
-        } else if ($this->_isAggregateFunction($this->_lexer->lookahead['type'])) {
-            $expression = $this->AggregateExpression();
-        } else {
-            $expression = $this->FunctionDeclaration();
-        }
+        $expression = $this->ScalarExpression();
 
         $expr = new AST\SimpleSelectExpression($expression);
 
