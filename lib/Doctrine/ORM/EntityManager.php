d135e301d993221ddd020e2c32b95cd3bd68bf64
Fabien Potencier <fabien.potencier@gmail.com>
2011-05-02 13:50:06 +0200

    Fix EntityManager::create() method to allow inheritance

--- lib/Doctrine/ORM/EntityManager.php
+++ lib/Doctrine/ORM/EntityManager.php
@@ -728,6 +728,6 @@ class EntityManager
             throw new \InvalidArgumentException("Invalid argument: " . $conn);
         }
 
-        return new EntityManager($conn, $config, $conn->getEventManager());
+        return new static($conn, $config, $conn->getEventManager());
     }
 }
