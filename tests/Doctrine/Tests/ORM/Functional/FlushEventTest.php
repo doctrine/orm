13a4ce0235e7e686a86e6222f99a83bf3c2b4211
Chris Hoult <chrishoult@timeout.com>
2011-06-21 13:25:30 +0100

    Added the postFlush event to the Event Manager for successful flushes; fixed a comment in OnFlushEventArgs

--- tests/Doctrine/Tests/ORM/Functional/FlushEventTest.php
+++ tests/Doctrine/Tests/ORM/Functional/FlushEventTest.php
@@ -5,6 +5,7 @@ namespace Doctrine\Tests\ORM\Functional;
 use Doctrine\Tests\Models\CMS\CmsUser;
 use Doctrine\Tests\Models\CMS\CmsPhonenumber;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Doctrine\ORM\Events;
 
 require_once __DIR__ . '/../../TestInit.php';
@@ -25,28 +26,47 @@ class FlushEventTest extends \Doctrine\Tests\OrmFunctionalTestCase
     {
         //$this->_em->getConnection()->getConfiguration()->setSQLLogger(new \Doctrine\DBAL\Logging\EchoSQLLogger);
         $this->_em->getEventManager()->addEventListener(Events::onFlush, new OnFlushListener);
-        
+
         $user = new CmsUser;
         $user->username = 'romanb';
         $user->name = 'Roman';
         $user->status = 'Dev';
-        
+
         $this->_em->persist($user);
-        
+
         $this->assertEquals(0, $user->phonenumbers->count());
-        
+
         $this->_em->flush();
-        
+
         $this->assertEquals(1, $user->phonenumbers->count());
         $this->assertTrue($this->_em->contains($user->phonenumbers->get(0)));
         $this->assertTrue($user->phonenumbers->get(0)->getUser() === $user);
-        
+
         $this->assertFalse($user->phonenumbers->isDirty());
-        
+
         // Can be used together with SQL Logging to check that a subsequent flush has
         // nothing to do. This proofs the correctness of the changes that happened in onFlush.
         //echo "SECOND FLUSH";
         //$this->_em->flush();
+    }
+
+    public function testPostFlush()
+    {
+        $postFlushListener = new PostFlushListener();
+        $this->_em->getEventManager()->addEventListener(Events::postFlush, $postFlushListener);
+
+        $user = new CmsUser;
+        $user->username = 'AnotherUser';
+        $user->name = 'AnotherName';
+        $user->status = 'AnotherStatus';
+
+        $this->_em->persist($user);
+
+        $this->assertFalse($postFlushListener->called);
+
+        $this->_em->flush();
+
+        $this->assertTrue($postFlushListener->called);
     }
 }
 
@@ -55,15 +75,15 @@ class OnFlushListener
     public function onFlush(OnFlushEventArgs $args)
     {
         //echo "---preFlush".PHP_EOL;
-        
+
         $em = $args->getEntityManager();
         $uow = $em->getUnitOfWork();
-        
+
         foreach ($uow->getScheduledEntityInsertions() as $entity) {
-            
+
             if ($entity instanceof CmsUser) {
                 // Adds a phonenumber to every newly persisted CmsUser ...
-                
+
                 $phone = new CmsPhonenumber;
                 $phone->phonenumber = 12345;
                 // Update object model
@@ -73,22 +93,30 @@ class OnFlushListener
                 // Explicitly calculate the changeset since onFlush is raised
                 // after changeset calculation!
                 $uow->computeChangeSet($em->getClassMetadata(get_class($phone)), $phone);
-                
+
                 // Take a snapshot because the UoW wont do this for us, because
                 // the UoW did not visit this collection.
                 // Alternatively we could provide an ->addVisitedCollection() method
                 // on the UoW.
                 $entity->getPhonenumbers()->takeSnapshot();
             }
-            
+
             /*foreach ($uow->getEntityChangeSet($entity) as $field => $change) {
                 list ($old, $new) = $change;
 
                 var_dump($old);
             }*/
-            
+
         }
     }
 }
 
+class PostFlushListener
+{
+    public $called = false;
 
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        $this->called = true;
+    }
+}
