b22f6924067800d4b462bde48bdb5aaff0d4c141
Alexander <iam.asm89@gmail.com>
2011-07-28 10:46:38 +0200

    [DDC-1301] Added tests for fetch="EXTRA_LAZY" count() on a "legacy" database

---
+++ tests/Doctrine/Tests/Models/Legacy/LegacyArticle.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Doctrine\Tests\Models\Legacy;
+
+/**
+ * @Entity
+ * @Table(name="legacy_articles")
+ */
+class LegacyArticle
+{
+    /**
+     * @Id
+     * @Column(name="iArticleId", type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+    /**
+     * @Column(name="sTopic", type="string", length=255)
+     */
+    public $topic;
+    /**
+     * @Column(name="sText", type="text")
+     */
+    public $text;
+    /**
+     * @ManyToOne(targetEntity="LegacyUser", inversedBy="articles")
+     * @JoinColumn(name="iUserId", referencedColumnName="iUserId")
+     */
+    public $user;
+    public function setAuthor(LegacyUser $author) {
+        $this->user = $author;
+    }
+}
