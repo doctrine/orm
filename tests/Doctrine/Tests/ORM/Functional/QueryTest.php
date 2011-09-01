3b3186ee98392802a44118cd421a3530119aa7ea
Guilherme Blanco <guilhermeblanco@gmail.com>
2011-09-01 19:11:57 -0300

    Added support to user provide an array of Entities as a DQL parameter. Fixes DDC-1356.

--- tests/Doctrine/Tests/ORM/Functional/QueryTest.php
+++ tests/Doctrine/Tests/ORM/Functional/QueryTest.php
@@ -501,4 +501,36 @@ class QueryTest extends \Doctrine\Tests\OrmFunctionalTestCase
         
         $this->assertEquals(0, count($users));
     }
+    
+    public function testQueryWithArrayOfEntitiesAsParameter()
+    {
+        $userA = new CmsUser;
+        $userA->name = 'Benjamin';
+        $userA->username = 'beberlei';
+        $userA->status = 'developer';
+        $this->_em->persist($userA);
+        
+        $userB = new CmsUser;
+        $userB->name = 'Roman';
+        $userB->username = 'romanb';
+        $userB->status = 'developer';
+        $this->_em->persist($userB);
+        
+        $userC = new CmsUser;
+        $userC->name = 'Jonathan';
+        $userC->username = 'jwage';
+        $userC->status = 'developer';
+        $this->_em->persist($userC);
+        
+        $this->_em->flush();
+        $this->_em->clear();
+        
+        $query = $this->_em->createQuery("SELECT u FROM Doctrine\Tests\Models\CMS\CmsUser u WHERE u IN (?0) OR u.username = ?1");
+        $query->setParameter(0, array($userA, $userC));
+        $query->setParameter(1, 'beberlei');
+        
+        $users = $query->execute();
+        
+        $this->assertEquals(2, count($users));
+    }
 }
