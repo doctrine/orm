e3a5ffbb55cb713cb00ae4a0980f972cc961df5b
Benjamin Eberlei <kontakt@beberlei.de>
2011-11-13 17:16:43 +0100

    DDC-1477 - Adjust patch to really fix bug in Proxy generation

--- lib/Doctrine/ORM/Proxy/ProxyFactory.php
+++ lib/Doctrine/ORM/Proxy/ProxyFactory.php
@@ -165,11 +165,13 @@ class ProxyFactory
     {
         $methods = '';
 
+        $methodNames = array();
         foreach ($class->reflClass->getMethods() as $method) {
             /* @var $method ReflectionMethod */
-            if ($method->isConstructor() || in_array(strtolower($method->getName()), array("__sleep", "__clone"))) {
+            if ($method->isConstructor() || in_array(strtolower($method->getName()), array("__sleep", "__clone")) || isset($methodNames[$method->getName()])) {
                 continue;
             }
+            $methodNames[$method->getName()] = true;
 
             if ($method->isPublic() && ! $method->isFinal() && ! $method->isStatic()) {
                 $methods .= "\n" . '    public function ';
@@ -219,6 +221,27 @@ class ProxyFactory
     }
 
     /**
+<<<<<<< HEAD
+=======
+     * @param ReflectionMethod $method
+     * @param ClassMetadata $class
+     * @return bool
+     */
+    private function isShortIdentifierGetter($method, $class)
+    {
+        $identifier = lcfirst(substr($method->getName(), 3));
+        return (
+            $method->getNumberOfParameters() == 0 &&
+            substr($method->getName(), 0, 3) == "get" &&
+            in_array($identifier, $class->identifier, true) &&
+            $class->hasField($identifier) &&
+            (($method->getEndLine() - $method->getStartLine()) <= 4)
+            && in_array($class->fieldMappings[$identifier]['type'], array('integer', 'bigint', 'smallint', 'string'))
+        );
+    }
+
+    /**
+>>>>>>> DDC-1477 - Adjust patch to really fix bug in Proxy generation
      * Generates the code for the __sleep method for a proxy class.
      *
      * @param $class
