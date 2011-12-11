d9b39f6fced1a3727893e1a38878cdf508f23498
Benjamin Eberlei <kontakt@beberlei.de>
2011-12-11 16:01:36 +0100

    DDC-1515 - Bugfix in hydration of dependant entities without additional fields.

--- lib/Doctrine/ORM/Internal/Hydration/AbstractHydrator.php
+++ lib/Doctrine/ORM/Internal/Hydration/AbstractHydrator.php
@@ -217,6 +217,7 @@ abstract class AbstractHydrator
 
             if (isset($cache[$key]['isMetaColumn'])) {
                 if (!isset($rowData[$dqlAlias][$cache[$key]['fieldName']]) || $value !== null) {
+                    $nonemptyComponents[$dqlAlias] = true;
                     $rowData[$dqlAlias][$cache[$key]['fieldName']] = $value;
                 }
                 continue;
