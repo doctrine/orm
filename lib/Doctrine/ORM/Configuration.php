8bdb7130738df5b7a9c5d8f73e3cbfb72149c909
Fabio B. Silva <fabio.bat.silva@gmail.com>
2011-12-23 14:41:03 -0200

    add support for NamingStrategy

--- lib/Doctrine/ORM/Configuration.php
+++ lib/Doctrine/ORM/Configuration.php
@@ -548,4 +548,29 @@ class Configuration extends \Doctrine\DBAL\Configuration
         return isset($this->_attributes['defaultRepositoryClassName']) ?
                 $this->_attributes['defaultRepositoryClassName'] : 'Doctrine\ORM\EntityRepository';
     }
+
+    /**
+     * Set naming strategy.
+     *
+     * @since 2.3
+     * @param NamingStrategy $namingStrategy
+     */
+    public function setNamingStrategy(NamingStrategy $namingStrategy)
+    {
+        $this->_attributes['namingStrategy'] = $namingStrategy;
+    }
+
+    /**
+     * Get naming strategy..
+     *
+     * @since 2.3
+     * @return NamingStrategy
+     */
+    public function getNamingStrategy()
+    {
+        if (!isset($this->_attributes['namingStrategy'])) {
+            $this->_attributes['namingStrategy'] = new DefaultNamingStrategy();
+        }
+        return $this->_attributes['namingStrategy'];
+    }
 }
