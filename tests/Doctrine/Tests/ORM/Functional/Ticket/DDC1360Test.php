a6deb51a0547efec163d9b57a1571af54accb2f1
Benjamin Eberlei <kontakt@beberlei.de>
2011-12-28 20:28:17 +0100

    DDC-1360 - Bugfix in quoting mechanism inside ClassMetadataInfo

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC1360Test.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+use Doctrine\Tests\OrmFunctionalTestCase;
+
+/**
+ * @group DDC-1360
+ */
+class DDC1360Test extends OrmFunctionalTestCase
+{
+    public function testSchemaDoubleQuotedCreate()
+    {
+        if ($this->_em->getConnection()->getDatabasePlatform()->getName() != "postgresql") {
+            $this->markTestSkipped("PostgreSQL only test.");
+        }
+
+        $sql = $this->_schemaTool->getCreateSchemaSQL(array(
+            $this->_em->getClassMetadata(__NAMESPACE__ . '\DDC1360DoubleQuote')
+        ));
+
+        $this->assertEquals(array(
+            'CREATE TABLE "user"."user" (id INT NOT NULL, PRIMARY KEY(id))',
+            'CREATE SEQUENCE "user".user_id_seq INCREMENT BY 1 MINVALUE 1 START 1',
+        ), $sql);
+    }
+}
+
+/**
+ * @Entity @Table(name="`user`.`user`")
+ */
+class DDC1360DoubleQuote
+{
+    /** @Id @GeneratedValue @Column(type="integer") */
+    public $id;
+}
+
