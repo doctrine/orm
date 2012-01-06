1d99fc4934be6bf64dfd45a537b4171b89c0b9e7
Fabio B. Silva <fabio.bat.silva@gmail.com>
2012-01-06 13:57:50 -0200

    added some tests

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DDC657Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC657Test.php
@@ -24,7 +24,7 @@ class DDC657Test extends \Doctrine\Tests\OrmFunctionalTestCase
     public function testEntitySingleResult()
     {
         $query      = $this->_em->createQuery('SELECT d FROM ' . self::NS . '\DateTimeModel d');
-        $datetime   = $query->getSingleResult();
+        $datetime   = $query->setMaxResults(1)->getSingleResult();
 
         $this->assertTrue($datetime instanceof DateTimeModel);
 
@@ -49,7 +49,20 @@ class DDC657Test extends \Doctrine\Tests\OrmFunctionalTestCase
     public function testTicketSingleResult()
     {
         $query      = $this->_em->createQuery('SELECT d.id, d.time, d.date, d.datetime FROM ' . self::NS . '\DateTimeModel d');
-        $datetime   = $query->getSingleResult();
+        $datetime   = $query->setMaxResults(1)->getSingleResult();
+
+        $this->assertTrue(is_array($datetime));
+
+        $this->assertTrue($datetime['datetime'] instanceof \DateTime);
+        $this->assertTrue($datetime['time'] instanceof \DateTime);
+        $this->assertTrue($datetime['date'] instanceof \DateTime);
+    }
+
+    public function testTicketResult()
+    {
+        $query      = $this->_em->createQuery('SELECT d.id, d.time, d.date, d.datetime FROM ' . self::NS . '\DateTimeModel d');
+        $result     = $query->getResult();
+        $datetime   = $result[0];
 
         $this->assertTrue(is_array($datetime));
 
@@ -61,15 +74,21 @@ class DDC657Test extends \Doctrine\Tests\OrmFunctionalTestCase
     public function loadFixtures()
     {
         $timezone           = new \DateTimeZone('America/Sao_Paulo');
-        $dateTime           = new DateTimeModel();
+
+        $dateTime1          = new DateTimeModel();
+        $dateTime2          = new DateTimeModel();
         
-        $dateTime->date     = new \DateTime('2012-01-06', $timezone);
-        $dateTime->time     = new \DateTime('2012-01-06 10:11:12', $timezone);
-        $dateTime->datetime = new \DateTime('2012-01-06 09:10:11', $timezone);
+        $dateTime1->date    = new \DateTime('2011-01-06', $timezone);
+        $dateTime1->time    = new \DateTime('2011-01-06 10:11:12', $timezone);
+        $dateTime1->datetime= new \DateTime('2011-01-06 09:10:11', $timezone);
+
+        $dateTime2->date    = new \DateTime('2012-01-06', $timezone);
+        $dateTime2->time    = new \DateTime('2012-01-06 10:11:12', $timezone);
+        $dateTime2->datetime= new \DateTime('2012-01-06 09:10:11', $timezone);
         
-        $this->_em->persist($dateTime);
+        $this->_em->persist($dateTime1);
+        $this->_em->persist($dateTime2);
 
         $this->_em->flush();
     }
-
 }
