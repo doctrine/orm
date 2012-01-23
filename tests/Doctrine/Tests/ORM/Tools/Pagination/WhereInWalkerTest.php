5dc0081f563f0b05ac3fbae4d23ee7bca5925cfd
Sander Marechal <sander@prezent.nl>
2012-01-23 15:42:41 +0100

    Add support for paginating WHERE NOT ... queries
    The Pagination tool throws an exception on a DQL query like:
    
    SELECT u FROM User u WHERE NOT (u INSTANCE OF Person)
    
    This is because Paginate does not know about the
    Doctrine\ORM\Query\AST\ConditionalFactor which implements the NOT
    operator. This patch adds support for that.

--- tests/Doctrine/Tests/ORM/Tools/Pagination/WhereInWalkerTest.php
+++ tests/Doctrine/Tests/ORM/Tools/Pagination/WhereInWalkerTest.php
@@ -107,5 +107,19 @@ class WhereInWalkerTest extends PaginationTestCase
                 "SELECT u0_.id AS id0, g1_.id AS id1 FROM User u0_ INNER JOIN user_group u2_ ON u0_.id = u2_.user_id INNER JOIN groups g1_ ON g1_.id = u2_.group_id WHERE (1 = 1 AND 2 = 2 OR 3 = 3) AND u0_.id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $whereInQuery->getSql()
         );
     }
+
+    public function testWhereInQuery_WhereNot()
+    {
+        $query = $this->entityManager->createQuery(
+                        'SELECT u, g FROM Doctrine\Tests\ORM\Tools\Pagination\User u JOIN u.groups g WHERE NOT 1 = 2'
+        );
+        $whereInQuery = clone $query;
+        $whereInQuery->setHint(Query::HINT_CUSTOM_TREE_WALKERS, array('Doctrine\ORM\Tools\Pagination\WhereInWalker'));
+        $whereInQuery->setHint(WhereInWalker::HINT_PAGINATOR_ID_COUNT, 10);
+
+        $this->assertEquals(
+                "SELECT u0_.id AS id0, g1_.id AS id1 FROM User u0_ INNER JOIN user_group u2_ ON u0_.id = u2_.user_id INNER JOIN groups g1_ ON g1_.id = u2_.group_id WHERE (NOT 1 = 2) AND u0_.id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $whereInQuery->getSql()
+        );
+    }
 }
 
