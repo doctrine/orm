7bca37e827085b535e35bc537cec4a92b976b338
Benjamin Eberlei <kontakt@beberlei.de>
2012-01-28 12:25:01 +0100

    [DDC-1526] Collections are not marked as initialized when they are fetch joined but dont contain any results. This only occurs when using LEFT JOINs on the assocations and causes another query to be fired when the empty collection is accessed again.

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC1526Test.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+/**
+ * @group DDC-1526
+ */
+class DDC1526Test extends \Doctrine\Tests\OrmFunctionalTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_schemaTool->createSchema(array(
+            $this->_em->getClassMetadata(__NAMESPACE__ . '\\DDC1526Menu'),
+        ));
+    }
+
+    public function testIssue()
+    {
+        $parents = array();
+        for ($i = 0; $i < 9; $i++) {
+            $entity = new DDC1526Menu;
+
+            if (isset ($parents[($i % 3)])) {
+                $entity->parent = $parents[($i%3)];
+            }
+
+            $this->_em->persist($entity);
+            $parents[$i] = $entity;
+        }
+        $this->_em->flush();
+        $this->_em->clear();
+
+
+        $dql = "SELECT m, c
+            FROM " . __NAMESPACE__ . "\DDC1526Menu m
+            LEFT JOIN m.children c";
+        $menus = $this->_em->createQuery($dql)->getResult();
+
+        // All Children collection now have to be initiailzed
+        foreach ($menus as $menu) {
+            $this->assertTrue($menu->children->isInitialized());
+        }
+    }
+}
+
+/**
+ * @Entity
+ */
+class DDC1526Menu
+{
+    /**
+     * @Column(type="integer")
+     * @Id
+     * @GeneratedValue
+     */
+    public $id;
+    /**
+     * @ManyToOne(targetEntity="DDC1526Menu", inversedBy="children")
+     */
+    public $parent;
+
+    /**
+     * @OneToMany(targetEntity="DDC1526Menu", mappedBy="parent")
+     */
+    public $children;
+}
