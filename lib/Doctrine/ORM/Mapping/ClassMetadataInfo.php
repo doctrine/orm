8c8deb9c9bcf61fb8dea27f85513446d42ad2259
Rivaros <rivantsiv@gmail.com>
2012-02-13 15:05:28 +0200

    Convention fixes

--- lib/Doctrine/ORM/Mapping/ClassMetadataInfo.php
+++ lib/Doctrine/ORM/Mapping/ClassMetadataInfo.php
@@ -1209,8 +1209,9 @@ class ClassMetadataInfo implements ClassMetadata
             foreach ($mapping['joinColumns'] as $key => &$joinColumn) {
                 if ($mapping['type'] === self::ONE_TO_ONE && ! $this->isInheritanceTypeSingleTable()) {
                     if (count($mapping['joinColumns']) == 1) {
-                        if(! isset($mapping['id']) || ! $mapping['id'])
+                        if (! isset($mapping['id']) || ! $mapping['id']) {
                             $joinColumn['unique'] = true;
+                        }
                     } else {
                         $uniqueContraintColumns[] = $joinColumn['name'];
                     }
