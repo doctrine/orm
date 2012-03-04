1a192b6530396a5e235b7777e655f006eb92560e
Guilherme Blanco <guilhermeblanco@gmail.com>
2012-03-03 23:10:56 -0500

    Moved SQRT function to Platform.

--- lib/Doctrine/ORM/Query/AST/Functions/SqrtFunction.php
+++ lib/Doctrine/ORM/Query/AST/Functions/SqrtFunction.php
@@ -41,8 +41,9 @@ class SqrtFunction extends FunctionNode
      */
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
-        //TODO: Use platform to get SQL
-        return 'SQRT(' . $sqlWalker->walkSimpleArithmeticExpression($this->simpleArithmeticExpression) . ')';
+        return $sqlWalker->getConnection()->getDatabasePlatform()->getSqrtExpression(
+            $sqlWalker->walkSimpleArithmeticExpression($this->simpleArithmeticExpression)
+        );
     }
 
     /**
