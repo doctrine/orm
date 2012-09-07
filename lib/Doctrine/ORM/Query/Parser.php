68269358a5a5d89e06b37d9b4758e79708461b65
Stefan Klug <klug.stefan@gmx.de>
2012-09-07 11:46:01 +0200

    fix for failing tests

--- lib/Doctrine/ORM/Query/Parser.php
+++ lib/Doctrine/ORM/Query/Parser.php
@@ -489,7 +489,7 @@ class Parser
         $this->_lexer->resetPeek();
 
         // We deny the COUNT(SELECT * FROM User u) here. COUNT won't be considered a function
-        return ($lookaheadType === Lexer::T_IDENTIFIER && $peek['type'] === Lexer::T_OPEN_PARENTHESIS && $nextpeek['type'] !== Lexer::T_SELECT);
+        return ($lookaheadType >= Lexer::T_IDENTIFIER && $peek['type'] === Lexer::T_OPEN_PARENTHESIS && $nextpeek['type'] !== Lexer::T_SELECT);
     }
 
     /**
