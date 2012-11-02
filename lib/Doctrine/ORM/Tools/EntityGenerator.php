95a85d67eae60a9cafd375433b6440d48616f463
Markus Lanthaler <mark_lanthaler@gmx.net>
2012-11-02 17:15:44 +0100

    Improve DocBlock annotations of generated entities
    Currently, the DocBlock annotations for member variables contain the variable name as description which is redundant and should be removed. Furthermore the class is annotated with the FQN instead of just the name. This makes automatically generated documentation quite ugly.

--- lib/Doctrine/ORM/Tools/EntityGenerator.php
+++ lib/Doctrine/ORM/Tools/EntityGenerator.php
@@ -672,7 +672,7 @@ public function __construct()
     {
         $lines = array();
         $lines[] = '/**';
-        $lines[] = ' * '.$metadata->name;
+        $lines[] = ' * ' . $this->getClassName($metadata);
 
         if ($this->generateAnnotations) {
             $lines[] = ' *';
@@ -1121,7 +1121,7 @@ public function __construct()
     {
         $lines = array();
         $lines[] = $this->spaces . '/**';
-        $lines[] = $this->spaces . ' * @var ' . $this->getType($fieldMapping['type']) . ' $' . $fieldMapping['fieldName'];
+        $lines[] = $this->spaces . ' * @var ' . $this->getType($fieldMapping['type']);
 
         if ($this->generateAnnotations) {
             $lines[] = $this->spaces . ' *';
