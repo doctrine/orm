a813dd5643a2cc6394c724b85de8dc49a4f8b19a
catalin <catalin@run.ro>
2012-11-06 16:05:47 +0200

    - Applied Phpstorm PSR format - Changed RegExp format

--- lib/Doctrine/ORM/Tools/Console/MetadataFilter.php
+++ lib/Doctrine/ORM/Tools/Console/MetadataFilter.php
@@ -51,7 +51,7 @@ class MetadataFilter extends \FilterIterator implements \Countable
 
     public function __construct(\ArrayIterator $metadata, $filter)
     {
-        $this->filter = (array) $filter;
+        $this->filter = (array)$filter;
 
         parent::__construct($metadata);
     }
@@ -66,7 +66,9 @@ class MetadataFilter extends \FilterIterator implements \Countable
         $metadata = $it->current();
 
         foreach ($this->filter as $filter) {
-            if(preg_match("#$filter#",$metadata->name,$m)) return true;
+            if (preg_match("/$filter/", $metadata->name, $m)) {
+                return true;
+            }
         }
 
         return false;
