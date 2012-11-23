a40e89305c13e2991885e0a82581738839f4b3a8
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2012-11-23 10:33:46 +0100

    No need for ArrayCollection

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DCC2161Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DCC2161Test.php
@@ -2,8 +2,6 @@
 
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
 require_once __DIR__ . '/../../../TestInit.php';
 
 class DDC2161Test extends \Doctrine\Tests\OrmFunctionalTestCase
