470e41a5f7484c998848f6144c845541ea97c963
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2012-11-23 10:18:47 +0100

    DCC-2161 Added failing test that moves the foreign key reference

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DCC2161Test.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+require_once __DIR__ . '/../../../TestInit.php';
+
+class DDC2161Test extends \Doctrine\Tests\OrmFunctionalTestCase
+{
+
+    /**
+     * @group DDC-2161
+     */
+    public function testSchemaUpdateOnForeignKeyMove()
+    {
+        $classes = array(
+          $this->_em->getClassMetadata(__NAMESPACE__ . '\\DDC2161_1')
+        );
+        $this->_schemaTool->createSchema($classes);
+
+        $classes = array(
+          $this->_em->getClassMetadata(__NAMESPACE__ . '\\DDC2161_2'),
+          $this->_em->getClassMetadata(__NAMESPACE__ . '\\DDC2161_3')
+        );
+        $this->_schemaTool->updateSchema($classes,true);
+
+        $sm = $this->_em->getConnection()->getSchemaManager();
+        $schema = $sm->createSchema();
+
+        $fk = $schema->getTable("DDC2161_1")->getForeignKeys();
+        $fk = array_shift($fk);
+
+        $this->assertEquals("DDC2161_3", $fk->getForeignTableName(), "Foreign key table should be DDC2161_3.");
+        ;
+    }
+}
+
+/**
+ * @Entity
+ */
+class DDC2161_1
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @OneToMany(targetEntity="DDC2161_1", mappedBy="parent")
+     */
+    private $children;
+
+    /**
+     * @ManyToOne(targetEntity="DDC2161_1", inversedBy="children")
+     */
+    private $parent;
+
+    public function __construct()
+    {
+        $this->children = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+}
+
+/**
+ * @Table("DDC2161_1")
+ * @Entity
+ */
+class DDC2161_2
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ManyToOne(targetEntity="DDC2161_3", inversedBy="children")
+     */
+    private $parent;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getParent()
+    {
+        return $this->parent;
+    }
+}
+
+/**
+ * @Entity
+ */
+class DDC2161_3
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @OneToMany(targetEntity="DDC2161_2", mappedBy="parent")
+     */
+    private $children;
+
+    public function __construct()
+    {
+        $this->children = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+}
