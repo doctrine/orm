76f2ba50eb802cf62a1246d4731a195655abbed1
Benjamin Morel <benjamin.morel@strictcoding.co.uk>
2012-12-14 18:55:16 +0000

    Fixed documentation for Doctrine\Tests\DbalTypes

--- tests/Doctrine/Tests/DbalTypes/NegativeToPositiveType.php
+++ tests/Doctrine/Tests/DbalTypes/NegativeToPositiveType.php
@@ -7,26 +7,41 @@ use Doctrine\DBAL\Platforms\AbstractPlatform;
 
 class NegativeToPositiveType extends Type
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'negative_to_positive';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
     {
         return $platform->getIntegerTypeDeclarationSQL($fieldDeclaration);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function canRequireSQLConversion()
     {
         return true;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
     {
         return 'ABS(' . $sqlExpr . ')';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function convertToPHPValueSQL($sqlExpr, $platform)
     {
         return '-(' . $sqlExpr . ')';
