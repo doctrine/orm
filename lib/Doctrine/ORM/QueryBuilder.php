63bcbbd7212d89bfdf606d18b705152462c9cdb3
Dries De Peuter <dries.depeuter@gmail.com>
2013-01-23 11:39:24 +0100

    Fix adding multiple joins.
    When more joins are added, they must be added completely.

--- lib/Doctrine/ORM/QueryBuilder.php
+++ lib/Doctrine/ORM/QueryBuilder.php
@@ -551,6 +551,11 @@ class QueryBuilder
 
                 $newDqlPart[$k] = $v;
             }
+            
+            // When a join contains more items, this must be added completely.
+            if (!$append) {
+                $isMultiple = false;
+            }
 
             $dqlPart = $newDqlPart;
         }
