d1ffced31f565a6cfe0e4f8a8051b94dd39487ac
Matthieu Napoli <matthieu@mnapoli.fr>
2013-02-08 11:04:10 +0100

    Code formatting

--- lib/Doctrine/ORM/Query/SqlWalker.php
+++ lib/Doctrine/ORM/Query/SqlWalker.php
@@ -2073,6 +2073,7 @@ class SqlWalker implements TreeWalker
         $this->parserResult->addParameterMapping($inputParam->name, $this->sqlParamIndex++);
 
         $parameter = $this->query->getParameter($inputParam->name);
+
         if ($parameter && Type::hasType($parameter->getType())) {
             $parameterType = Type::getType($parameter->getType());
             return $parameterType->convertToDatabaseValueSQL('?', $this->platform);
