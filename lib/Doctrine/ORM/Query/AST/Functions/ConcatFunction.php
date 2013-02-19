4841a068be7a26f9d9f6366563eead19a7fffd40
Rajesh Sharma <broncha@rajesharma.com>
2013-02-19 20:31:52 +0545

    [DDC-2304] accept more than 2 parameters in CONCAT function
    - Tested and parser validates at least 2 parameters given
    - test added for CONCAT function and indentation fixed
    - calling getConcatExpression using call_user_func_array as number of arguments is not known removing dependency to patch DBAL
    - maintaining backward compatibility

--- lib/Doctrine/ORM/Query/AST/Functions/ConcatFunction.php
+++ lib/Doctrine/ORM/Query/AST/Functions/ConcatFunction.php
@@ -22,7 +22,7 @@ namespace Doctrine\ORM\Query\AST\Functions;
 use Doctrine\ORM\Query\Lexer;
 
 /**
- * "CONCAT" "(" StringPrimary "," StringPrimary ")"
+ * "CONCAT" "(" StringPrimary "," StringPrimary {"," StringPrimary }* ")"
  *
  *
  * @link    www.doctrine-project.org
@@ -35,18 +35,25 @@ use Doctrine\ORM\Query\Lexer;
 class ConcatFunction extends FunctionNode
 {
     public $firstStringPrimary;
+    
     public $secondStringPrimary;
-
+    
+    public $concatExpressions = array();
+    
     /**
      * @override
      */
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
         $platform = $sqlWalker->getConnection()->getDatabasePlatform();
-        return $platform->getConcatExpression(
-            $sqlWalker->walkStringPrimary($this->firstStringPrimary),
-            $sqlWalker->walkStringPrimary($this->secondStringPrimary)
-        );
+        
+        $args = array();
+        
+        foreach ($this->concatExpressions as $expression) {
+            $args[] = $sqlWalker->walkStringPrimary($expression);
+        }
+        
+        return call_user_func_array(array($platform,'getConcatExpression'), $args);
     }
 
     /**
@@ -56,10 +63,19 @@ class ConcatFunction extends FunctionNode
     {
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
-
+        
         $this->firstStringPrimary = $parser->StringPrimary();
+        $this->concatExpressions[] = $this->firstStringPrimary;
+        
         $parser->match(Lexer::T_COMMA);
+        
         $this->secondStringPrimary = $parser->StringPrimary();
+        $this->concatExpressions[] = $this->secondStringPrimary;
+        
+        while ($parser->getLexer()->isNextToken(Lexer::T_COMMA)) {
+ 		    $parser->match(Lexer::T_COMMA);
+	        $this->concatExpressions[] = $parser->StringPrimary();
+        }
 
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
