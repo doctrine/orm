618dcce260ed8a326f30fa65ac172a53dde670f1
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-03-14 17:42:52 +0100

    ObjectHydrator: support entity aliasing.

--- lib/Doctrine/ORM/Internal/Hydration/ObjectHydrator.php
+++ lib/Doctrine/ORM/Internal/Hydration/ObjectHydrator.php
@@ -101,9 +101,10 @@ class ObjectHydrator extends AbstractHydrator
         foreach ($this->_rsm->aliasMap as $dqlAlias => $className) {
             $this->identifierMap[$dqlAlias] = array();
             $this->idTemplate[$dqlAlias]    = '';
+            $classMetadata = $this->_em->getClassMetadata($className);
 
-            if ( ! isset($this->ce[$className])) {
-                $this->ce[$className] = $this->_em->getClassMetadata($className);
+            if ( ! isset($this->ce[$classMetadata->name])) {
+                $this->ce[$classMetadata->name] = $this->_em->getClassMetadata($className);
             }
 
             // Remember which associations are "fetch joined", so that we know where to inject
@@ -135,7 +136,7 @@ class ObjectHydrator extends AbstractHydrator
 
             // handle fetch-joined owning side bi-directional one-to-one associations
             if ($assoc['inversedBy']) {
-                $class        = $this->ce[$className];
+                $class        = $this->ce[$classMetadata->name];
                 $inverseAssoc = $class->associationMappings[$assoc['inversedBy']];
 
                 if ( ! ($inverseAssoc['type'] & ClassMetadata::TO_ONE)) {
@@ -247,7 +248,9 @@ class ObjectHydrator extends AbstractHydrator
      */
     private function getEntity(array $data, $dqlAlias)
     {
-        $className = $this->_rsm->aliasMap[$dqlAlias];
+        $classAliasMap = $this->_rsm->aliasMap[$dqlAlias];
+        $classMetadata = $this->_em->getClassMetadata($classAliasMap);
+        $className = $classMetadata->name;
 
         if (isset($this->_rsm->discriminatorColumns[$dqlAlias])) {
 
