66de7ce91586e458f97359754f10cb1d5d09922f
Mike Meier <mike.meier@ibrows.ch>
2013-03-18 11:10:14 +0100

    add SqlWalkerTest for getSQLTableAlias method

---
+++ tests/Doctrine/Tests/ORM/Query/SqlWalkerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Query;
+
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query;
+use Doctrine\Tests\OrmTestCase;
+use Doctrine\ORM\Query\ParserResult;
+
+class SqlWalkerTest extends OrmTestCase
+{
+    public function testGetSQLTableAlias()
+    {
+        $query = new Query($this->_getTestEntityManager());
+        $sqlWalker = new SqlWalker($query, new ParserResult(), array());
+
+        $this->assertSame('t0_', $sqlWalker->getSQLTableAlias('table'));
+        $this->assertSame('t1_', $sqlWalker->getSQLTableAlias('çtable'));
+    }
+}
