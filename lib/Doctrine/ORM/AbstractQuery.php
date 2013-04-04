64b2ecfefc8a6db94a0ec931dfc1e8012a5eb8a2
Benjamin Eberlei <kontakt@beberlei.de>
2013-04-04 20:20:23 +0200

    [DDC-2224] Rewrite instanceof feature with parameter needle ClassMetadata breaks caching of queries.

--- lib/Doctrine/ORM/AbstractQuery.php
+++ lib/Doctrine/ORM/AbstractQuery.php
@@ -284,6 +284,10 @@ abstract class AbstractQuery
             }
         }
 
+        if ($value instanceof Mapping\ClassMetadata) {
+            return $value->name;
+        }
+
         return $value;
     }
 
