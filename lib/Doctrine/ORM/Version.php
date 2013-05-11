425f375899b9b054867ea3a3cb752dabdd874651
Benjamin Eberlei <kontakt@beberlei.de>
2013-05-11 09:51:12 +0200

    Bump dev version to 2.3.5

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.3.4';
+    const VERSION = '2.3.5-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
