9771f05119d8d13b82cae5df299d42967e6e1786
fabios <fabios@nationalfibre.net>
2013-07-11 12:41:39 -0400

    Test collection lock

--- tests/Doctrine/Tests/ORM/Functional/SecondLevelCacheConcurrentTest.php
+++ tests/Doctrine/Tests/ORM/Functional/SecondLevelCacheConcurrentTest.php
@@ -12,7 +12,10 @@ use Doctrine\ORM\Cache\DefaultQueryCache;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Tests\Models\Cache\Country;
 use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Cache\CollectionCacheKey;
 use Doctrine\ORM\Cache\EntityCacheKey;
+use Doctrine\Tests\Models\Cache\State;
+use Doctrine\Tests\Models\Cache\City;
 use Doctrine\ORM\Cache\CacheFactory;
 use Doctrine\Common\Cache\Cache;
 use Doctrine\ORM\Cache\Lock;
@@ -37,7 +40,7 @@ class SecondLevelCacheConcurrentTest extends SecondLevelCacheAbstractTest
         $this->_em->getConfiguration()->setSecondLevelCacheFactory($this->cacheFactory);
     }
 
-    public function testBasicConcurrentReadLock()
+    public function testBasicConcurrentEntityReadLock()
     {
         $this->loadFixturesCountries();
         $this->_em->clear();
@@ -61,7 +64,7 @@ class SecondLevelCacheConcurrentTest extends SecondLevelCacheAbstractTest
         $this->assertFalse($this->cache->containsEntity(Country::CLASSNAME, $countryId));
     }
 
-    public function testBasicConcurrentWriteLock()
+    public function testBasicConcurrentEntityWriteLock()
     {
         $this->loadFixturesCountries();
         $this->_em->clear();
@@ -111,6 +114,128 @@ class SecondLevelCacheConcurrentTest extends SecondLevelCacheAbstractTest
         $this->assertEquals($queryCount + 1, $this->getCurrentQueryCount());
         $this->assertEquals('Foo 1', $country->getName());
         $this->assertTrue($this->cache->containsEntity(Country::CLASSNAME, $countryId));
+    }
+
+    public function testBasicConcurrentCollectionReadLock()
+    {
+        $this->loadFixturesCountries();
+        $this->loadFixturesStates();
+        $this->loadFixturesCities();
+
+        $this->_em->clear();
+        $this->evictRegions();
+
+        $stateId    = $this->states[0]->getId();
+        $state      = $this->_em->find(State::CLASSNAME, $stateId);
+
+        $this->assertInstanceOf(State::CLASSNAME, $state);
+        $this->assertCount(2, $state->getCities());
+
+        $this->_em->clear();
+        $this->secondLevelCacheLogger->clearStats();
+
+        $stateId   = $this->states[0]->getId();
+        $cacheId   = new CollectionCacheKey(State::CLASSNAME, 'cities', array('id'=>$stateId));
+        $region    = $this->_em->getCache()->getCollectionCacheRegionAccess(State::CLASSNAME, 'cities')->getRegion();
+
+        $this->assertTrue($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+
+        /* @var $region \Doctrine\Tests\Mocks\ConcurrentRegionMock */
+        $region->setLock($cacheId, Lock::createLockRead()); // another proc lock the entity cache
+
+        $this->assertFalse($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+
+        $queryCount = $this->getCurrentQueryCount();
+        $state      = $this->_em->find(State::CLASSNAME, $stateId);
+
+        $this->assertEquals(0, $this->secondLevelCacheLogger->getMissCount());
+        $this->assertEquals(1, $this->secondLevelCacheLogger->getHitCount());
+
+        $this->assertEquals(0, $this->secondLevelCacheLogger->getRegionMissCount($this->getEntityRegion(State::CLASSNAME)));
+        $this->assertEquals(1, $this->secondLevelCacheLogger->getRegionHitCount($this->getEntityRegion(State::CLASSNAME)));
+
+        $this->assertInstanceOf(State::CLASSNAME, $state);
+        $this->assertCount(2, $state->getCities());
+
+        $this->assertEquals(1, $this->secondLevelCacheLogger->getMissCount());
+        $this->assertEquals(1, $this->secondLevelCacheLogger->getHitCount());
+        $this->assertEquals(1, $this->secondLevelCacheLogger->getRegionMissCount($this->getCollectionRegion(State::CLASSNAME, 'cities')));
+
+        $this->assertEquals($queryCount + 1, $this->getCurrentQueryCount());
+        $this->assertFalse($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+    }
+
+    public function testBasicConcurrentCollectionWriteLock()
+    {
+        $this->loadFixturesCountries();
+        $this->loadFixturesStates();
+        $this->loadFixturesCities();
+
+        $this->_em->clear();
+        $this->evictRegions();
+
+        $lock    = Lock::createLockWrite();
+        $stateId = $this->states[0]->getId();
+        $state   = $this->_em->find(State::CLASSNAME, $stateId);
+
+        $this->assertInstanceOf(State::CLASSNAME, $state);
+        $this->assertCount(2, $state->getCities());
+
+        $this->_em->clear();
+        $this->secondLevelCacheLogger->clearStats();
+
+        $stateId   = $this->states[0]->getId();
+        $cacheKey  = new CollectionCacheKey(State::CLASSNAME, 'cities', array('id'=>$stateId));
+        $region    = $this->_em->getCache()->getCollectionCacheRegionAccess(State::CLASSNAME, 'cities')->getRegion();
+
+        $this->assertTrue($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+
+        /* @var $region \Doctrine\Tests\Mocks\ConcurrentRegionMock */
+        $region->setLock($cacheKey, $lock); // another proc lock entity cache
+
+        $queryCount = $this->getCurrentQueryCount();
+        $state      = $this->_em->find(State::CLASSNAME, $stateId);
+
+        $this->assertInstanceOf(State::CLASSNAME, $state);
+        $this->assertCount(2, $state->getCities()); // Cache locked, goes straight to database
+
+        $this->assertEquals($queryCount + 1, $this->getCurrentQueryCount());
+        $this->assertFalse($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+
+        $city0 = $state->getCities()->get(0);
+        $city1 = $state->getCities()->get(1);
+        
+        $state->getCities()->remove(0);
+
+        $this->_em->remove($city1);
+        $this->_em->persist($state);
+        $this->_em->flush();
+        $this->_em->clear();
+
+        $queryCount = $this->getCurrentQueryCount();
+        $state      = $this->_em->find(State::CLASSNAME, $stateId);
+
+        $this->assertInstanceOf(State::CLASSNAME, $state);
+        $this->assertCount(1, $state->getCities()); // Cache locked, goes straight to database
+
+        $this->assertEquals($queryCount + 1, $this->getCurrentQueryCount());
+        $this->assertFalse($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+
+        /* @var \Doctrine\Tests\Mocks\ConcurrentRegionMock */
+        $region->writeUnlock($cacheKey, $lock); // another proc unlock
+        $region->evict($cacheKey); // and clear the cache.
+
+        $this->assertFalse($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
+
+        $this->_em->clear();
+
+        $queryCount = $this->getCurrentQueryCount();
+        $state      = $this->_em->find(State::CLASSNAME, $stateId);
+
+        $this->assertInstanceOf(State::CLASSNAME, $state);
+        $this->assertCount(1, $state->getCities()); //No Cache, goes to database
+        $this->assertEquals($queryCount + 1, $this->getCurrentQueryCount());
+        $this->assertTrue($this->cache->containsCollection(State::CLASSNAME, 'cities', $stateId));
     }
 }
 
