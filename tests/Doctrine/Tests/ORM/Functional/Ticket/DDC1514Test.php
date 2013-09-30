9d2255b1d55d82515c23717b23a1afd4a80299a6
flip111 <flip101@gmail.com>
2013-09-30 14:34:23 +0200

    Changed code AGAIN (facepalm). Just went with a simple foreach.

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DDC1514Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC1514Test.php
@@ -57,9 +57,12 @@ class DDC1514Test extends \Doctrine\Tests\OrmFunctionalTestCase
         $dql = "SELECT a, b, ba, c FROM " . __NAMESPACE__ . "\DDC1514EntityA AS a LEFT JOIN a.entitiesB AS b LEFT JOIN b.entityATo AS ba LEFT JOIN a.entityC AS c";
         $results = $this->_em->createQuery($dql)->getResult();
 
-        $a2FromDB = array_shift(array_filter($results, function($entity) use ($a2) {
-            return $entity->id === $a2->id;
-        }));
+        foreach ($results as $entity) {
+            if ($entity->id === $a2->id) {
+                $a2FromDB = $entity;
+                break;
+						}
+        }
 
         $this->assertEquals($c->title, $a2FromDB->entityC->title);
     }
