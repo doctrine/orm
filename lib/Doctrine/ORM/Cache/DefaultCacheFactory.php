FabioBatSilva/doctrine2

Extract cache config

lib/Doctrine/ORM/Cache/DefaultCacheFactory.php
@@ -22,7 +22,7 @@ namespace Doctrine\ORM\Cache;
 
 use Doctrine\ORM\Cache;
 use Doctrine\ORM\Cache\Region;
-use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Cache\RegionsConfig;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Cache\Region\DefaultRegion;
@@ -50,9 +50,9 @@ class DefaultCacheFactory implements CacheFactory
     private $cache;
 
     /**
-     * @var \Doctrine\ORM\Configuration
+     * @var \Doctrine\ORM\Cache\RegionsConfig
      */
-    private $configuration;
+    private $regionsConfig;
 
     /**
      * @var array
@@ -65,13 +65,13 @@ class DefaultCacheFactory implements CacheFactory
     private $fileLockRegionDirectory;
 
     /**
-     * @param \Doctrine\ORM\Configuration  $configuration
-     * @param \Doctrine\Common\Cache\Cache $cache
+     * @param \Doctrine\ORM\Cache\RegionsConfig     $cacheConfig
+     * @param \Doctrine\Common\Cache\Cache          $cache
      */
-    public function __construct(Configuration $configuration, CacheDriver $cache)
+    public function __construct(RegionsConfig $cacheConfig, CacheDriver $cache)
     {
         $this->cache         = $cache;
-        $this->configuration = $configuration;
+        $this->regionsConfig = $cacheConfig;
     }
 
     /**
@@ -181,7 +181,7 @@ class DefaultCacheFactory implements CacheFactory
         }
 
         $region = new DefaultRegion($cache['region'], clone $this->cache, array(
-            'lifetime' => $this->configuration->getSecondLevelCacheRegionLifetime($cache['region'])
+            'lifetime' => $this->regionsConfig->getLifetime($cache['region'])
         ));
 
         if ($cache['usage'] === ClassMetadata::CACHE_USAGE_READ_WRITE) {
@@ -194,7 +194,7 @@ class DefaultCacheFactory implements CacheFactory
             }
 
             $directory = $this->fileLockRegionDirectory . DIRECTORY_SEPARATOR . $cache['region'];
-            $region    = new FileLockRegion($region, $directory, $this->configuration->getSecondLevelCacheLockLifetime());
+            $region    = new FileLockRegion($region, $directory, $this->regionsConfig->getLockLifetime($cache['region']));
         }
 
         return $this->regions[$cache['region']] = $region;

lib/Doctrine/ORM/EntityManager.php
@@ -169,7 +169,7 @@ use Doctrine\Common\Util\ClassUtils;
         );
 
         if ($config->isSecondLevelCacheEnabled()) {
-            $cacheClass  = $config->getSecondLevelCacheClassName();
+            $cacheClass  = $config->getSecondLevelCacheConfig()->getCacheClassName();
             $this->cache = new $cacheClass($this);
         }
     }

tests/Doctrine/Tests/ORM/Cache/CacheConfigTest.php
@@ -1,0 +1,82 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Cache;
+
+use Doctrine\Tests\DoctrineTestCase;
+use Doctrine\ORM\Cache\CacheConfig;
+
+/**
+ * @group DDC-2183
+ */
+class CacheConfigTest extends DoctrineTestCase
+{
+    /**
+     * @var \Doctrine\ORM\Cache\CacheConfig
+     */
+    private $config;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->config = new CacheConfig();
+    }
+
+    public function testSetGetCacheClassName()
+    {
+        $mockClass = get_class($this->getMock('Doctrine\ORM\Cache'));
+
+        $this->assertEquals('Doctrine\ORM\Cache\DefaultCache', $this->config->getCacheClassName());
+        $this->config->setCacheClassName($mockClass);
+        $this->assertEquals($mockClass, $this->config->getCacheClassName());
+
+        $this->setExpectedException('Doctrine\ORM\ORMException');
+        $this->config->setCacheClassName(__CLASS__);
+    }
+
+    public function testSetGetRegionLifetime()
+    {
+        $config = $this->config->getRegionsConfig();
+
+        $config->setDefaultLifetime(111);
+
+        $this->assertEquals($config->getDefaultLifetime(), $config->getLifetime('foo_region'));
+
+        $config->setLifetime('foo_region', 222);
+
+        $this->assertEquals(222, $config->getLifetime('foo_region'));
+    }
+
+    public function testSetGetCacheLogger()
+    {
+        $logger = $this->getMock('Doctrine\ORM\Cache\Logging\CacheLogger');
+
+        $this->assertNull($this->config->getCacheLogger());
+
+        $this->config->setCacheLogger($logger);
+
+        $this->assertEquals($logger, $this->config->getCacheLogger());
+    }
+
+    public function testSetGetCacheFactory()
+    {
+        $factory = $this->getMock('Doctrine\ORM\Cache\CacheFactory');
+
+        $this->assertNull($this->config->getCacheFactory());
+
+        $this->config->setCacheFactory($factory);
+
+        $this->assertEquals($factory, $this->config->getCacheFactory());
+    }
+
+    public function testSetGetQueryValidator()
+    {
+        $validator = $this->getMock('Doctrine\ORM\Cache\QueryCacheValidator');
+
+        $this->assertInstanceOf('Doctrine\ORM\Cache\TimestampQueryCacheValidator', $this->config->getQueryValidator());
+
+        $this->config->setQueryValidator($validator);
+
+        $this->assertEquals($validator, $this->config->getQueryValidator());
+    }
+}

tests/Doctrine/Tests/ORM/Cache/DefaultCacheFactoryTest.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Cache\Region\DefaultRegion;
 use Doctrine\Tests\Mocks\ConcurrentRegionMock;
 use Doctrine\ORM\Persisters\BasicEntityPersister;
 use Doctrine\ORM\Persisters\OneToManyPersister;
+use Doctrine\ORM\Cache\RegionsConfig;
 
 /**
  * @group DDC-2183
@@ -25,16 +26,20 @@ class DefaultCacheFactoryTest extends OrmTestCase
      */
     private $em;
 
+    /**
+     * @var \Doctrine\ORM\Cache\RegionsConfig
+     */
+    private $regionsConfig;
+
     protected function setUp()
     {
         $this->enableSecondLevelCache();
         parent::setUp();
 
-        $this->em = $this->_getTestEntityManager();
-
-
-        $arguments     = array($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
-        $this->factory = $this->getMock('\Doctrine\ORM\Cache\DefaultCacheFactory', array(
+        $this->em               = $this->_getTestEntityManager();
+        $this->regionsConfig    = new RegionsConfig;
+        $arguments              = array($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
+        $this->factory          = $this->getMock('\Doctrine\ORM\Cache\DefaultCacheFactory', array(
             'getRegion'
         ), $arguments);
     }
@@ -182,7 +187,7 @@ class DefaultCacheFactoryTest extends OrmTestCase
         $metadata2  = clone $em->getClassMetadata('Doctrine\Tests\Models\Cache\AttractionLocationInfo');
         $persister1 = new BasicEntityPersister($em, $metadata1);
         $persister2 = new BasicEntityPersister($em, $metadata2);
-        $factory    = new DefaultCacheFactory($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
+        $factory    = new DefaultCacheFactory($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
 
         $cachedPersister1 = $factory->buildCachedEntityPersister($em, $persister1, $metadata1);
         $cachedPersister2 = $factory->buildCachedEntityPersister($em, $persister2, $metadata2);
@@ -201,7 +206,7 @@ class DefaultCacheFactoryTest extends OrmTestCase
         $metadata2  = clone $em->getClassMetadata('Doctrine\Tests\Models\Cache\City');
         $persister1 = new BasicEntityPersister($em, $metadata1);
         $persister2 = new BasicEntityPersister($em, $metadata2);
-        $factory    = new DefaultCacheFactory($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
+        $factory    = new DefaultCacheFactory($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
 
         $cachedPersister1 = $factory->buildCachedEntityPersister($em, $persister1, $metadata1);
         $cachedPersister2 = $factory->buildCachedEntityPersister($em, $persister2, $metadata2);
@@ -252,7 +257,7 @@ class DefaultCacheFactoryTest extends OrmTestCase
      */
     public function testInvalidFileLockRegionDirectoryException()
     {
-        $factory = new \Doctrine\ORM\Cache\DefaultCacheFactory($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
+        $factory = new \Doctrine\ORM\Cache\DefaultCacheFactory($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
 
         $factory->getRegion(array(
             'usage'   => ClassMetadata::CACHE_USAGE_READ_WRITE,

tests/Doctrine/Tests/OrmFunctionalTestCase.php
@@ -448,18 +448,20 @@ abstract class OrmFunctionalTestCase extends OrmTestCase
 
         if ($this->isSecondLevelCacheEnabled || $enableSecondLevelCache) {
 
-            $cache   = $this->getSharedSecondLevelCacheDriverImpl();
-            $factory = new DefaultCacheFactory($config, $cache);
+            $cacheConfig    = new \Doctrine\ORM\Cache\CacheConfig();
+            $cache          = $this->getSharedSecondLevelCacheDriverImpl();
+            $factory        = new DefaultCacheFactory($cacheConfig->getRegionsConfig(), $cache);
 
             $this->secondLevelCacheFactory = $factory;
 
             if ($this->isSecondLevelCacheLogEnabled) {
-                $this->secondLevelCacheLogger  = new StatisticsCacheLogger();
-                $config->setSecondLevelCacheLogger($this->secondLevelCacheLogger);
+                $this->secondLevelCacheLogger = new StatisticsCacheLogger();
+                $cacheConfig->setCacheLogger($this->secondLevelCacheLogger);
             }
-            
-            $config->setSecondLevelCacheEnabled();
-            $config->setSecondLevelCacheFactory($factory);
+
+            $cacheConfig->setCacheFactory($factory);
+            $config->setSecondLevelCacheEnabled(true);
+            $config->setSecondLevelCacheConfig($cacheConfig);
 
             $this->isSecondLevelCacheEnabled = true;
         }

tests/Doctrine/Tests/OrmTestCase.php
@@ -127,13 +127,16 @@ abstract class OrmTestCase extends DoctrineTestCase
         ), true));
 
         if ($this->isSecondLevelCacheEnabled) {
-            $cache   = $this->getSharedSecondLevelCacheDriverImpl();
-            $factory = new DefaultCacheFactory($config, $cache);
+
+            $cacheConfig    = new \Doctrine\ORM\Cache\CacheConfig();
+            $cache          = $this->getSharedSecondLevelCacheDriverImpl();
+            $factory        = new DefaultCacheFactory($cacheConfig->getRegionsConfig(), $cache);
 
             $this->secondLevelCacheFactory = $factory;
 
-            $config->setSecondLevelCacheEnabled();
-            $config->setSecondLevelCacheFactory($factory);
+            $cacheConfig->setCacheFactory($factory);
+            $config->setSecondLevelCacheEnabled(true);
+            $config->setSecondLevelCacheConfig($cacheConfig);
         }
 
         if ($conn === null) {

doctrine/orm

lib/Doctrine/ORM/Cache/DefaultCacheFactory.php
@@ -22,7 +22,7 @@ namespace Doctrine\ORM\Cache;
 
 use Doctrine\ORM\Cache;
 use Doctrine\ORM\Cache\Region;
-use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Cache\RegionsConfiguration;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Cache\Region\DefaultRegion;
@@ -50,9 +50,9 @@ class DefaultCacheFactory implements CacheFactory
     private $cache;
 
     /**
-     * @var \Doctrine\ORM\Configuration
+     * @var \Doctrine\ORM\Cache\RegionsConfiguration
      */
-    private $configuration;
+    private $regionsConfig;
 
     /**
      * @var array
@@ -65,13 +65,13 @@ class DefaultCacheFactory implements CacheFactory
     private $fileLockRegionDirectory;
 
     /**
-     * @param \Doctrine\ORM\Configuration  $configuration
-     * @param \Doctrine\Common\Cache\Cache $cache
+     * @param \Doctrine\ORM\Cache\RegionsConfiguration  $cacheConfig
+     * @param \Doctrine\Common\Cache\Cache              $cache
      */
-    public function __construct(Configuration $configuration, CacheDriver $cache)
+    public function __construct(RegionsConfiguration $cacheConfig, CacheDriver $cache)
     {
         $this->cache         = $cache;
-        $this->configuration = $configuration;
+        $this->regionsConfig = $cacheConfig;
     }
 
     /**
@@ -181,7 +181,7 @@ class DefaultCacheFactory implements CacheFactory
         }
 
         $region = new DefaultRegion($cache['region'], clone $this->cache, array(
-            'lifetime' => $this->configuration->getSecondLevelCacheRegionLifetime($cache['region'])
+            'lifetime' => $this->regionsConfig->getLifetime($cache['region'])
         ));
 
         if ($cache['usage'] === ClassMetadata::CACHE_USAGE_READ_WRITE) {
@@ -194,7 +194,7 @@ class DefaultCacheFactory implements CacheFactory
             }
 
             $directory = $this->fileLockRegionDirectory . DIRECTORY_SEPARATOR . $cache['region'];
-            $region    = new FileLockRegion($region, $directory, $this->configuration->getSecondLevelCacheLockLifetime());
+            $region    = new FileLockRegion($region, $directory, $this->regionsConfig->getLockLifetime($cache['region']));
         }
 
         return $this->regions[$cache['region']] = $region;

lib/Doctrine/ORM/EntityManager.php
@@ -166,7 +166,7 @@ use Doctrine\Common\Util\ClassUtils;
         );
 
         if ($config->isSecondLevelCacheEnabled()) {
-            $cacheClass  = $config->getSecondLevelCacheClassName();
+            $cacheClass  = $config->getSecondLevelCacheConfiguration()->getCacheClassName();
             $this->cache = new $cacheClass($this);
         }
     }

tests/Doctrine/Tests/ORM/Cache/CacheConfigTest.php
@@ -1,0 +1,82 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Cache;
+
+use Doctrine\Tests\DoctrineTestCase;
+use Doctrine\ORM\Cache\CacheConfiguration;
+
+/**
+ * @group DDC-2183
+ */
+class CacheConfigTest extends DoctrineTestCase
+{
+    /**
+     * @var \Doctrine\ORM\Cache\CacheConfiguration
+     */
+    private $config;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->config = new CacheConfiguration();
+    }
+
+    public function testSetGetCacheClassName()
+    {
+        $mockClass = get_class($this->getMock('Doctrine\ORM\Cache'));
+
+        $this->assertEquals('Doctrine\ORM\Cache\DefaultCache', $this->config->getCacheClassName());
+        $this->config->setCacheClassName($mockClass);
+        $this->assertEquals($mockClass, $this->config->getCacheClassName());
+
+        $this->setExpectedException('Doctrine\ORM\ORMException');
+        $this->config->setCacheClassName(__CLASS__);
+    }
+
+    public function testSetGetRegionLifetime()
+    {
+        $config = $this->config->getRegionsConfiguration();
+
+        $config->setDefaultLifetime(111);
+
+        $this->assertEquals($config->getDefaultLifetime(), $config->getLifetime('foo_region'));
+
+        $config->setLifetime('foo_region', 222);
+
+        $this->assertEquals(222, $config->getLifetime('foo_region'));
+    }
+
+    public function testSetGetCacheLogger()
+    {
+        $logger = $this->getMock('Doctrine\ORM\Cache\Logging\CacheLogger');
+
+        $this->assertNull($this->config->getCacheLogger());
+
+        $this->config->setCacheLogger($logger);
+
+        $this->assertEquals($logger, $this->config->getCacheLogger());
+    }
+
+    public function testSetGetCacheFactory()
+    {
+        $factory = $this->getMock('Doctrine\ORM\Cache\CacheFactory');
+
+        $this->assertNull($this->config->getCacheFactory());
+
+        $this->config->setCacheFactory($factory);
+
+        $this->assertEquals($factory, $this->config->getCacheFactory());
+    }
+
+    public function testSetGetQueryValidator()
+    {
+        $validator = $this->getMock('Doctrine\ORM\Cache\QueryCacheValidator');
+
+        $this->assertInstanceOf('Doctrine\ORM\Cache\TimestampQueryCacheValidator', $this->config->getQueryValidator());
+
+        $this->config->setQueryValidator($validator);
+
+        $this->assertEquals($validator, $this->config->getQueryValidator());
+    }
+}

tests/Doctrine/Tests/ORM/Cache/DefaultCacheFactoryTest.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Cache\Region\DefaultRegion;
 use Doctrine\Tests\Mocks\ConcurrentRegionMock;
 use Doctrine\ORM\Persisters\BasicEntityPersister;
 use Doctrine\ORM\Persisters\OneToManyPersister;
+use Doctrine\ORM\Cache\RegionsConfiguration;
 
 /**
  * @group DDC-2183
@@ -25,16 +26,20 @@ class DefaultCacheFactoryTest extends OrmTestCase
      */
     private $em;
 
+    /**
+     * @var \Doctrine\ORM\Cache\RegionsConfiguration
+     */
+    private $regionsConfig;
+
     protected function setUp()
     {
         $this->enableSecondLevelCache();
         parent::setUp();
 
-        $this->em = $this->_getTestEntityManager();
-
-
-        $arguments     = array($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
-        $this->factory = $this->getMock('\Doctrine\ORM\Cache\DefaultCacheFactory', array(
+        $this->em               = $this->_getTestEntityManager();
+        $this->regionsConfig    = new RegionsConfiguration;
+        $arguments              = array($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
+        $this->factory          = $this->getMock('\Doctrine\ORM\Cache\DefaultCacheFactory', array(
             'getRegion'
         ), $arguments);
     }
@@ -182,7 +187,7 @@ class DefaultCacheFactoryTest extends OrmTestCase
         $metadata2  = clone $em->getClassMetadata('Doctrine\Tests\Models\Cache\AttractionLocationInfo');
         $persister1 = new BasicEntityPersister($em, $metadata1);
         $persister2 = new BasicEntityPersister($em, $metadata2);
-        $factory    = new DefaultCacheFactory($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
+        $factory    = new DefaultCacheFactory($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
 
         $cachedPersister1 = $factory->buildCachedEntityPersister($em, $persister1, $metadata1);
         $cachedPersister2 = $factory->buildCachedEntityPersister($em, $persister2, $metadata2);
@@ -201,7 +206,7 @@ class DefaultCacheFactoryTest extends OrmTestCase
         $metadata2  = clone $em->getClassMetadata('Doctrine\Tests\Models\Cache\City');
         $persister1 = new BasicEntityPersister($em, $metadata1);
         $persister2 = new BasicEntityPersister($em, $metadata2);
-        $factory    = new DefaultCacheFactory($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
+        $factory    = new DefaultCacheFactory($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
 
         $cachedPersister1 = $factory->buildCachedEntityPersister($em, $persister1, $metadata1);
         $cachedPersister2 = $factory->buildCachedEntityPersister($em, $persister2, $metadata2);
@@ -252,7 +257,7 @@ class DefaultCacheFactoryTest extends OrmTestCase
      */
     public function testInvalidFileLockRegionDirectoryException()
     {
-        $factory = new \Doctrine\ORM\Cache\DefaultCacheFactory($this->em->getConfiguration(), $this->getSharedSecondLevelCacheDriverImpl());
+        $factory = new \Doctrine\ORM\Cache\DefaultCacheFactory($this->regionsConfig, $this->getSharedSecondLevelCacheDriverImpl());
 
         $factory->getRegion(array(
             'usage'   => ClassMetadata::CACHE_USAGE_READ_WRITE,

tests/Doctrine/Tests/OrmFunctionalTestCase.php
@@ -448,18 +448,20 @@ abstract class OrmFunctionalTestCase extends OrmTestCase
 
         if ($this->isSecondLevelCacheEnabled || $enableSecondLevelCache) {
 
-            $cache   = $this->getSharedSecondLevelCacheDriverImpl();
-            $factory = new DefaultCacheFactory($config, $cache);
+            $cacheConfig    = new \Doctrine\ORM\Cache\CacheConfiguration();
+            $cache          = $this->getSharedSecondLevelCacheDriverImpl();
+            $factory        = new DefaultCacheFactory($cacheConfig->getRegionsConfiguration(), $cache);
 
             $this->secondLevelCacheFactory = $factory;
 
             if ($this->isSecondLevelCacheLogEnabled) {
-                $this->secondLevelCacheLogger  = new StatisticsCacheLogger();
-                $config->setSecondLevelCacheLogger($this->secondLevelCacheLogger);
+                $this->secondLevelCacheLogger = new StatisticsCacheLogger();
+                $cacheConfig->setCacheLogger($this->secondLevelCacheLogger);
             }
-            
-            $config->setSecondLevelCacheEnabled();
-            $config->setSecondLevelCacheFactory($factory);
+
+            $cacheConfig->setCacheFactory($factory);
+            $config->setSecondLevelCacheEnabled(true);
+            $config->setSecondLevelCacheConfiguration($cacheConfig);
 
             $this->isSecondLevelCacheEnabled = true;
         }

tests/Doctrine/Tests/OrmTestCase.php
@@ -127,13 +127,16 @@ abstract class OrmTestCase extends DoctrineTestCase
         ), true));
 
         if ($this->isSecondLevelCacheEnabled) {
-            $cache   = $this->getSharedSecondLevelCacheDriverImpl();
-            $factory = new DefaultCacheFactory($config, $cache);
+
+            $cacheConfig    = new \Doctrine\ORM\Cache\CacheConfiguration();
+            $cache          = $this->getSharedSecondLevelCacheDriverImpl();
+            $factory        = new DefaultCacheFactory($cacheConfig->getRegionsConfiguration(), $cache);
 
             $this->secondLevelCacheFactory = $factory;
 
-            $config->setSecondLevelCacheEnabled();
-            $config->setSecondLevelCacheFactory($factory);
+            $cacheConfig->setCacheFactory($factory);
+            $config->setSecondLevelCacheEnabled(true);
+            $config->setSecondLevelCacheConfiguration($cacheConfig);
         }
 
         if ($conn === null) {