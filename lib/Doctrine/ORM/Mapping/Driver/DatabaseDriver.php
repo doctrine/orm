63816e26ccc95b0ec308dbc5d124aaccc959628d
José Gabriel González Pérez <jgabrielsinner10@gmail.com>
2014-01-04 10:39:17 -0430

    Fix for no dot on Class Names
    If you work with PostgreSQL Schemas, You should filter the names of table to generate Correct name for PHP Classes. This way allow not write dots (.) as part of the Class Name.
    
    Additionally, there is a variable ($schema) that must be contained in the class "Doctrine\DBAL\Schema\Table" on an $schema possible property but this is not available.

--- lib/Doctrine/ORM/Mapping/Driver/DatabaseDriver.php
+++ lib/Doctrine/ORM/Mapping/Driver/DatabaseDriver.php
@@ -299,9 +299,19 @@ class DatabaseDriver implements MappingDriver
             if ($pkColumns == $allForeignKeyColumns && count($foreignKeys) == 2) {
                 $this->manyToManyTables[$tableName] = $table;
             } else {
+                
+                // If you work with PostgreSQL Schemas you need tu filter the names of table to generate
+                // Correct name for PHP Classes
+                if (strpos($tableName, ".") !== false) {
+                    list($schema, $table_shortname) = explode(".", $tableName);
+                }else{
+                    $schema = 'public';
+                    $table_shortname = $tableName;
+                }
+                
                 // lower-casing is necessary because of Oracle Uppercase Tablenames,
                 // assumption is lower-case + underscore separated.
-                $className = $this->getClassNameForTable($tableName);
+                $className = $this->getClassNameForTable($table_shortname);
 
                 $this->tables[$tableName] = $table;
                 $this->classToTableNames[$className] = $tableName;
