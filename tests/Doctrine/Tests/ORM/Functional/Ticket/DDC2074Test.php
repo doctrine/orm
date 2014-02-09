5c06121d9432680028a02142405cd7a353ddb487
Benjamin Eberlei <kontakt@beberlei.de>
2014-02-09 14:27:23 +0100

    [DDC-2624] Fix bug when persistent collection is cloned and used in a new entity.

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DDC2074Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC2074Test.php
@@ -12,6 +12,12 @@ use Doctrine\Tests\Models\ECommerce\ECommerceProduct;
  */
 class DDC2074Test extends \Doctrine\Tests\OrmFunctionalTestCase
 {
+    public function setUp()
+    {
+        $this->useModelSet('ecommerce');
+        parent::setUp();
+    }
+
     public function testShouldNotScheduleDeletionOnClonedInstances()
     {
         $class = $this->_em->getClassMetadata('Doctrine\Tests\Models\ECommerce\ECommerceProduct');
@@ -25,5 +31,31 @@ class DDC2074Test extends \Doctrine\Tests\OrmFunctionalTestCase
         $clonedCollection->clear();
 
         $this->assertEquals(0, count($uow->getScheduledCollectionDeletions()));
+    }
+
+    public function testSavingClonedPersistentCollection()
+    {
+        $product = new ECommerceProduct();
+        $category = new ECommerceCategory();
+        $category->setName('foo');
+        $product->addCategory($category);
+
+        $this->_em->persist($product);
+        $this->_em->persist($category);
+        $this->_em->flush();
+
+        $newProduct = clone $product;
+
+        $this->_em->persist($newProduct);
+        $this->_em->flush();
+        $this->_em->clear();
+
+        $product1 = $this->_em->find('Doctrine\Tests\Models\ECommerce\ECommerceProduct', $product->getId());
+        $product2 = $this->_em->find('Doctrine\Tests\Models\ECommerce\ECommerceProduct', $newProduct->getId());
+
+        $this->assertCount(1, $product1->getCategories());
+        $this->assertCount(1, $product2->getCategories());
+
+        $this->assertSame($product1->getCategories()->get(0), $product2->getCategories()->get(0));
     }
 }
