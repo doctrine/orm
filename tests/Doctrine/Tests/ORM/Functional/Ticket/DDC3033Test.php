doctrine/orm

commit 53a5a48aed7d87aa1533c0bcbd72e41b686527d8

[DDC-2624] Fix bug when persistent collection is cloned and used in a new entity.

lib/Doctrine/ORM/UnitOfWork.php
@@ -560,7 +560,15 @@ class UnitOfWork implements PropertyChangedListener
         foreach ($class->reflFields as $name => $refProp) {
             $value = $refProp->getValue($entity);
 
-            if ($class->isCollectionValuedAssociation($name) && $value !== null && ! ($value instanceof PersistentCollection)) {
+            if ($class->isCollectionValuedAssociation($name) && $value !== null) {
+                if ($value instanceof PersistentCollection) {
+                    if ($value->getOwner() === $entity) {
+                        continue;
+                    }
+
+                    $value = new ArrayCollection($value->getValues());
+                }
+
                 // If $value is not a Collection then use an ArrayCollection.
                 if ( ! $value instanceof Collection) {
                     $value = new ArrayCollection($value);

tests/Doctrine/Tests/Models/ECommerce/ECommerceProduct.php
@@ -137,6 +137,11 @@ class ECommerceProduct
         }
     }
 
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+    }
+
     public function getCategories()
     {
         return $this->categories;
@@ -166,6 +171,9 @@ class ECommerceProduct
     public function __clone()
     {
         $this->isCloned = true;
+        if ($this->categories) {
+            $this->categories = clone $this->categories;
+        }
     }
 
     /**

tests/Doctrine/Tests/ORM/Functional/Ticket/DDC2074Test.php
@@ -12,6 +12,12 @@ use Doctrine\Tests\Models\ECommerce\ECommerceProduct;
  */
 class DDC2074Test extends \Doctrine\Tests\OrmFunctionalTestCase
 {
+    public function setUp()
+    {
+        $this->useModelSet('ecommerce');
+        parent::setUp();
+    }
+
     public function testShouldNotScheduleDeletionOnClonedInstances()
     {
         $class = $this->_em->getClassMetadata('Doctrine\Tests\Models\ECommerce\ECommerceProduct');
@@ -25,5 +31,31 @@ class DDC2074Test extends \Doctrine\Tests\OrmFunctionalTestCase
         $clonedCollection->clear();
 
         $this->assertEquals(0, count($uow->getScheduledCollectionDeletions()));
+    }
+
+    public function testSavingClonedPersistentCollection()
+    {
+        $product = new ECommerceProduct();
+        $category = new ECommerceCategory();
+        $category->setName('foo');
+        $product->addCategory($category);
+
+        $this->_em->persist($product);
+        $this->_em->persist($category);
+        $this->_em->flush();
+
+        $newProduct = clone $product;
+
+        $this->_em->persist($newProduct);
+        $this->_em->flush();
+        $this->_em->clear();
+
+        $product1 = $this->_em->find('Doctrine\Tests\Models\ECommerce\ECommerceProduct', $product->getId());
+        $product2 = $this->_em->find('Doctrine\Tests\Models\ECommerce\ECommerceProduct', $newProduct->getId());
+
+        $this->assertCount(1, $product1->getCategories());
+        $this->assertCount(1, $product2->getCategories());
+
+        $this->assertSame($product1->getCategories()->get(0), $product2->getCategories()->get(0));
     }
 }

commit 263822fd191d647a0a2e997d07fc0449553b826b

Failing Test (since commit 53a5a48aed7d87aa1533c0bcbd72e41b686527d8)
Hi,

It seems to be a regression since the commit https://github.com/doctrine/doctrine2/commit/53a5a48aed7d87aa1533c0bcbd72e41b686527d8

Doctrine\ORM\PersistentCollection can be populated in $changeSet if you set a PreUpdate and PostUpdate event.

Original issue: http://www.doctrine-project.org/jira/browse/DDC-3033

tests/Doctrine/Tests/ORM/Functional/Ticket/DDC3033Test.php
@@ -1,0 +1,219 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+require_once __DIR__ . '/../../../TestInit.php';
+
+/**
+ * @group DDC-3033
+ */
+class DDC3033Test extends \Doctrine\Tests\OrmFunctionalTestCase
+{
+    public function testIssue()
+    {
+        $this->_schemaTool->createSchema(array(
+            $this->_em->getClassMetadata(__NAMESPACE__ . '\\DDC3033User'),
+            $this->_em->getClassMetadata(__NAMESPACE__ . '\\DDC3033Product'),
+        ));
+
+        $user = new DDC3033User();
+        $user->setTitle("Test User");
+        $this->_em->persist($user);
+
+        $user2 = new DDC3033User();
+        $user2->setTitle("Test User 2");
+        $this->_em->persist($user2);
+
+        $product = new DDC3033Product();
+        $product->setTitle("Test product");
+        $product->addBuyer($user);
+
+	      $this->_em->persist($product);
+        $this->_em->flush();
+
+	      $product->setTitle("Test Change title");
+	      $product->addBuyer($user2);
+
+	      $this->_em->persist($product);
+	      $this->_em->flush();
+
+	      $expect = array(
+            'title' => array(
+		            0 => 'Test product',
+		            1 => 'Test Change title',
+	          ),
+	      );
+
+	      $this->assertEquals(print_r($expect, true), print_r($product->changeSet, true));
+    }
+}
+
+/**
+ * @Table
+ * @Entity @HasLifecycleCallbacks
+ */
+class DDC3033Product
+{
+    public $changeSet = array();
+
+    /**
+     * @var integer $id
+     *
+     * @Column(name="id", type="integer")
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     *
+     * @Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ManyToMany(targetEntity="DDC3033User")
+     * @JoinTable(
+     *   name="user_purchases",
+     *   joinColumns={@JoinColumn(name="product_id", referencedColumnName="id")},
+     *   inverseJoinColumns={@JoinColumn(name="user_id", referencedColumnName="id")}
+     * )
+     */
+    private $buyers;
+
+    /**
+     * Default constructor
+     */
+    public function __construct()
+    {
+        $this->buyers = new ArrayCollection();
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $buyers
+     */
+    public function setBuyers($buyers)
+    {
+        $this->buyers = $buyers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBuyers()
+    {
+        return $this->buyers;
+    }
+
+    /**
+     * @param DDC3033User $buyer
+     */
+    public function addBuyer(DDC3033User $buyer)
+    {
+        $this->buyers[] = $buyer;
+    }
+
+    /**
+     * @PreUpdate
+     */
+    public function preUpdate(LifecycleEventArgs $eventArgs)
+    {
+    }
+
+    /**
+     * @PostUpdate
+     */
+    public function postUpdate(LifecycleEventArgs $eventArgs)
+    {
+        $em            = $eventArgs->getEntityManager();
+        $uow           = $em->getUnitOfWork();
+        $entity        = $eventArgs->getEntity();
+        $classMetadata = $em->getClassMetadata(get_class($entity));
+
+	      $uow->computeChangeSet($classMetadata, $entity);
+        $this->changeSet = $uow->getEntityChangeSet($entity);
+    }
+}
+
+/**
+ * @Table
+ * @Entity @HasLifecycleCallbacks
+ */
+class DDC3033User
+{
+    /**
+     * @var integer
+     *
+     * @Column(name="id", type="integer")
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}