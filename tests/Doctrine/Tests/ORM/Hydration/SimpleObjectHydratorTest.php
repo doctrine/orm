a054b595509b6a01fdd74da086148849f022314b
Frank Liepert <trainmaster@gmx.net>
2014-04-09 10:19:48 +0200

    [DDC-3076] Add test

--- tests/Doctrine/Tests/ORM/Hydration/SimpleObjectHydratorTest.php
+++ tests/Doctrine/Tests/ORM/Hydration/SimpleObjectHydratorTest.php
@@ -56,4 +56,31 @@ class SimpleObjectHydratorTest extends HydrationTestCase
         $result = $hydrator->hydrateAll($stmt, $rsm);
         $this->assertEquals($result[0], $expectedEntity);
     }
+
+    /**
+     * @group DDC-3076
+     *
+     * @expectedException \Doctrine\ORM\Internal\Hydration\HydrationException
+     * @expectedExceptionMessage The discriminator value "subworker" is invalid. It must be one of "person", "manager", "employee".
+     */
+    public function testInvalidDiscriminatorValueException()
+    {
+        $rsm = new ResultSetMapping;
+        $rsm->addEntityResult('Doctrine\Tests\Models\Company\CompanyPerson', 'p');
+        $rsm->addFieldResult('p', 'p__id', 'id');
+        $rsm->addFieldResult('p', 'p__name', 'name');
+        $rsm->addMetaResult('p ', 'discr', 'discr');
+        $rsm->setDiscriminatorColumn('p', 'discr');
+        $resultSet = array(
+              array(
+                  'u__id'   => '1',
+                  'u__name' => 'Fabio B. Silva',
+                  'discr'   => 'subworker'
+              ),
+         );
+
+        $stmt       = new HydratorMockStatement($resultSet);
+        $hydrator   = new \Doctrine\ORM\Internal\Hydration\SimpleObjectHydrator($this->_em);
+        $hydrator->hydrateAll($stmt, $rsm);
+    }
 }
