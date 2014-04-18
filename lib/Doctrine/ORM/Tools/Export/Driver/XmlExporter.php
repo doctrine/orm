e68ec80432c978c2460b113688210f1a27edbdec
Fabio B. Silva <fabio.bat.silva@gmail.com>
2014-04-17 22:45:19 -0400

    Remove SimpleXmlElement hack

--- lib/Doctrine/ORM/Tools/Export/Driver/XmlExporter.php
+++ lib/Doctrine/ORM/Tools/Export/Driver/XmlExporter.php
@@ -398,13 +398,15 @@ class XmlExporter extends AbstractExporter
     private function exportTableOptions(\SimpleXMLElement $parentXml, array $options)
     {        
         foreach ($options as $name => $option) {
-            $optionXml = $parentXml->addChild('option');
+            $isArray   = is_array($option);
+            $optionXml = $isArray
+                ? $parentXml->addChild('option')
+                : $parentXml->addChild('option', (string) $option);
+
             $optionXml->addAttribute('name', (string) $name);
-            
-            if (is_array($option)) {
+
+            if ($isArray) {
                 $this->exportTableOptions($optionXml, $option);
-            } else {
-                $optionXml[0] = (string) $option;
             }
         }
     }
