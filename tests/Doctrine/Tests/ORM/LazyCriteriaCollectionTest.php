7cb40ed9153961742e6f26144da36ceef547c936
Marco Pivetta <ocramius@gmail.com>
2014-05-17 18:45:33 +0200

    Mocking up tests for the lazy-criteria-collection

---
+++ tests/Doctrine/Tests/ORM/LazyCriteriaCollectionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Doctrine\Tests\ORM;
+
+use Doctrine\Tests\Mocks\ConnectionMock;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @author Marco Pivetta <ocramius@gmail.com>
+ *
+ * @covers \Doctrine\ORM\LazyCriteriaCollection
+ */
+class LazyCriteriaCollectionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+
+    }
+}
