ba089e2bd69a7c9a6d1203b7fff8d71d3b47ce43
Gusakov Nikita <dev@nkt.me>
2014-06-07 10:12:23 +0400

    Fixed new instance creating for php 5.4+

--- lib/Doctrine/ORM/Mapping/ClassMetadataInfo.php
+++ lib/Doctrine/ORM/Mapping/ClassMetadataInfo.php
@@ -908,7 +908,7 @@ class ClassMetadataInfo implements ClassMetadata
     public function newInstance()
     {
         if ($this->_prototype === null) {
-            if (PHP_VERSION_ID === 50429 || PHP_VERSION_ID === 50513) {
+            if (PHP_VERSION_ID >= 50400) {
                 $this->_prototype = $this->reflClass->newInstanceWithoutConstructor();
             } else {
                 $this->_prototype = unserialize(sprintf('O:%d:"%s":0:{}', strlen($this->name), $this->name));
