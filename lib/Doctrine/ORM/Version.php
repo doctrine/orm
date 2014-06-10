84f8ef5ca46f02f3001af47ba25fc917c776a247
Benjamin Eberlei <kontakt@beberlei.de>
2014-06-10 13:49:09 +0200

    Bump version to 2.4.4

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.4.3';
+    const VERSION = '2.4.4-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
