8a13376d42b5ea467727ffe730aa0e14ca3c5e29
Benjamin Eberlei <kontakt@beberlei.de>
2014-06-10 13:49:08 +0200

    Release 2.4.3

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.4.3-DEV';
+    const VERSION = '2.4.3';
 
     /**
      * Compares a Doctrine version with the current one.
