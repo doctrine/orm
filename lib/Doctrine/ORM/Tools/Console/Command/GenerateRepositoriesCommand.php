c0ee57ae55157d2258549ae5c0d13e4bcd3668a6
encoder64 <beta.screech@gmail.com>
2014-07-27 14:44:31 +0300

    Default/Custom Entity Repository for Entity Repository Generator

--- lib/Doctrine/ORM/Tools/Console/Command/GenerateRepositoriesCommand.php
+++ lib/Doctrine/ORM/Tools/Console/Command/GenerateRepositoriesCommand.php
@@ -73,6 +73,8 @@ EOT
         $metadatas = $em->getMetadataFactory()->getAllMetadata();
         $metadatas = MetadataFilter::filter($metadatas, $input->getOption('filter'));
 
+        $repositoryName = $em->getConfiguration()->getDefaultRepositoryClassName();
+
         // Process destination directory
         $destPath = realpath($input->getArgument('dest-path'));
 
@@ -91,7 +93,9 @@ EOT
         if (count($metadatas)) {
             $numRepositories = 0;
             $generator = new EntityRepositoryGenerator();
-
+            
+            $generator->setDefaultRepositoryName($repositoryName);
+            
             foreach ($metadatas as $metadata) {
                 if ($metadata->customRepositoryClassName) {
                     $output->writeln(
