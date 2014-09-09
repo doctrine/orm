7a73d27600c178f75a3dd64d67c3bcc867c7aa34
mmoreram <marc.morera@befactory.es>
2014-09-09 17:13:34 +0200

    Added resolve entities support in discrim. map

---
+++ lib/Doctrine/ORM/Tools/ResolveDiscriminatorMapListener.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ORM\Tools;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+/**
+ * ResolveDiscriminatorMapListener
+ *
+ * Mechanism to overwrite interfaces or classes specified in discrimination map
+ *
+ * @author Benjamin Eberlei <kontakt@beberlei.de>
+ * @since 2.2
+ */
+class ResolveDiscriminatorMapListener
+{
+    /**
+     * @var array
+     */
+    private $resolveTargetEntities = array();
+
+    /**
+     * Adds a target-entity class name to resolve to a new class name.
+     *
+     * @param string $originalEntity
+     * @param string $newEntity
+     * @param array  $mapping
+     *
+     * @return void
+     */
+    public function addResolveTargetEntity($originalEntity, $newEntity, array $mapping)
+    {
+        $mapping['targetEntity'] = ltrim($newEntity, "\\");
+        $this->resolveTargetEntities[ltrim($originalEntity, "\\")] = $mapping;
+    }
+
+    /**
+     * Processes event and resolves new target entity names.
+     *
+     * @param LoadClassMetadataEventArgs $args
+     *
+     * @return void
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $args)
+    {
+        $classMetadata = $args->getClassMetadata();
+
+        if (!empty($classMetadata->discriminatorMap)) {
+
+            $this->remapDiscriminatorMap($classMetadata);
+        }
+    }
+
+    /**
+     * Replaces all Interfaces in discriminator map
+     *
+     * @param \Doctrine\ORM\Mapping\ClassMetadataInfo $classMetadata
+     */
+    private function remapDiscriminatorMap(ClassMetadataInfo $classMetadata)
+    {
+        foreach ($classMetadata->discriminatorMap as $name => $interface) {
+
+            if (isset($this->resolveTargetEntities[$interface])) {
+
+                $classMetadata->discriminatorMap[$name] = $this->resolveTargetEntities[$interface]['targetEntity'];
+            }
+        }
+    }
+}
