935694543829373185b0c54a91b9596d68d5b540
zhil <starsfm@gmail.com>
2014-09-26 21:39:20 +0300

    Update Composite.php

--- lib/Doctrine/ORM/Query/Expr/Composite.php
+++ lib/Doctrine/ORM/Query/Expr/Composite.php
@@ -35,19 +35,20 @@ class Composite extends Base
      */
     public function __toString()
     {
-        if ($this->count() === 1) {
+        if ($this->count() === 1 && $this->parts[0] !== $this) {
             return (string) $this->parts[0];
         }
 
         $components = array();
 
         foreach ($this->parts as $part) {
-            $components[] = $this->processQueryPart($part);
+            if($part !== $this) {
+                $components[] = $this->processQueryPart($part);
+            }
         }
 
         return implode($this->separator, $components);
     }
-
     /**
      * @param string $part
      *
