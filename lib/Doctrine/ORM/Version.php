8bb1d5448baf25e6ad0ed0627b7bd099e1738399
Marco Pivetta <ocramius@gmail.com>
2014-10-06 15:23:31 +0200

    Bump version to 2.4.7

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.4.6';
+    const VERSION = '2.4.7-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
