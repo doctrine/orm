28b4a72963578b1f8dd83fecc0e9c1a73163d1c3
Pieter Vogelaar <p.vogelaar@moxx-mobility.com>
2014-10-08 10:16:21 +0200

    Added proxy identifier values to the EntityNotFoundException

--- lib/Doctrine/ORM/Proxy/ProxyFactory.php
+++ lib/Doctrine/ORM/Proxy/ProxyFactory.php
@@ -142,7 +142,11 @@ class ProxyFactory extends AbstractProxyFactory
                     $proxy->__setCloner($cloner);
                     $proxy->__setInitialized(false);
 
-                    throw new EntityNotFoundException($classMetadata->getName());
+                    $json = json_encode($classMetadata->getIdentifierValues($proxy));
+                    $message = "Entity of type '{$classMetadata->getName()}'"
+                        . " was not found with identifier(s) '{$json}'";
+
+                    throw new EntityNotFoundException($message);
                 }
             };
         }
@@ -173,7 +177,11 @@ class ProxyFactory extends AbstractProxyFactory
                 $proxy->__setCloner($cloner);
                 $proxy->__setInitialized(false);
 
-                throw new EntityNotFoundException($classMetadata->getName());
+                $json = json_encode($classMetadata->getIdentifierValues($proxy));
+                $message = "Entity of type '{$classMetadata->getName()}'"
+                    . " was not found with identifier(s) '{$json}'";
+
+                throw new EntityNotFoundException($message);
             }
         };
     }
@@ -197,12 +205,16 @@ class ProxyFactory extends AbstractProxyFactory
 
             $proxy->__setInitialized(true);
             $proxy->__setInitializer(null);
- 
+
             $class    = $entityPersister->getClassMetadata();
             $original = $entityPersister->loadById($classMetadata->getIdentifierValues($proxy));
 
             if (null === $original) {
-                throw new EntityNotFoundException($classMetadata->getName());
+                $json = json_encode($classMetadata->getIdentifierValues($proxy));
+                $message = "Entity of type '{$classMetadata->getName()}'"
+                    . " was not found with identifier(s) '{$json}'";
+
+                throw new EntityNotFoundException($message);
             }
 
             foreach ($class->getReflectionClass()->getProperties() as $property) {
