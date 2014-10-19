d557a0e01bd3a0ad273eab9f193f29547172f78f
Marco Pivetta <ocramius@gmail.com>
2014-10-19 17:09:27 +0200

    #615 - assignment alignment

--- tests/Doctrine/Tests/ORM/Query/SqlWalkerTest.php
+++ tests/Doctrine/Tests/ORM/Query/SqlWalkerTest.php
@@ -11,7 +11,7 @@ class SqlWalkerTest extends OrmTestCase
 {
     public function testGetSQLTableAlias()
     {
-        $query = new Query($this->_getTestEntityManager());
+        $query     = new Query($this->_getTestEntityManager());
         $sqlWalker = new SqlWalker($query, new ParserResult(), array());
 
         $this->assertSame('t0_', $sqlWalker->getSQLTableAlias('table'));
