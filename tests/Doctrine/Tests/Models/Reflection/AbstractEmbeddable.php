d75d6ffb1137cf89b18e56a851e8922caf448721
Marco Pivetta <ocramius@gmail.com>
2014-12-08 01:17:57 +0100

    #1213 - DDC-3437 - adding relevant test assets

---
+++ tests/Doctrine/Tests/Models/Reflection/AbstractEmbeddable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Doctrine\Tests\Models\Reflection;
+
+/**
+ * A test asset used to check that embeddables support properties defined in abstract classes
+ */
+abstract class AbstractEmbeddable
+{
+    private $propertyInAbstractClass;
+}
