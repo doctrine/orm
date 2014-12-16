2bc4ff3cab2ae297bcd05f2e619d42e6a7ca9e68
Marco Pivetta <ocramius@gmail.com>
2014-12-16 14:45:01 +0100

    Release v2.4.7

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.4.7-DEV';
+    const VERSION = '2.4.7';
 
     /**
      * Compares a Doctrine version with the current one.
