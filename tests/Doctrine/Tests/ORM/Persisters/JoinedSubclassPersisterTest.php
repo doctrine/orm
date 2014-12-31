e2b34ff5d2688a8ee5a72bad617dbfb5384e8c6e
Marco Pivetta <ocramius@gmail.com>
2014-12-31 09:44:39 +0100

    #1235 DDC-3470 - joined subclass persister empty `executeInserts()` method return type test

---
+++ tests/Doctrine/Tests/ORM/Persisters/JoinedSubclassPersisterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Persisters;
+
+use Doctrine\ORM\Persisters\JoinedSubclassPersister;
+use Doctrine\Tests\OrmTestCase;
+
+class JoinedSubClassPersisterTest extends OrmTestCase
+{
+    /**
+     * @var JoinedSubclassPersister
+     */
+    protected $persister;
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->em = $this->_getTestEntityManager();
+
+        $this->persister = new JoinedSubclassPersister(
+            $this->em,
+            $this->em->getClassMetadata('Doctrine\Tests\Models\JoinedInheritanceType\RootClass')
+        );
+    }
+
+    public function testExecuteInsertsWillReturnEmptySetWithNoQueuedInserts()
+    {
+        $this->assertSame(array(), $this->persister->executeInserts());
+    }
+}
