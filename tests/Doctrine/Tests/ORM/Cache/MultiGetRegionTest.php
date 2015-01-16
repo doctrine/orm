77487162d554e99d944699e88e2c4d445d2d0d06
Asmir Mustafic <goetas@gmail.com>
2015-01-17 00:13:47 +0100

    Added test case for MultiGetRegion

---
+++ tests/Doctrine/Tests/ORM/Cache/MultiGetRegionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Doctrine\Tests\ORM\Cache;
+
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Tests\Mocks\CacheEntryMock;
+use Doctrine\Tests\Mocks\CacheKeyMock;
+use Doctrine\ORM\Cache\Region\DefaultMultiGetRegion;
+
+/**
+ * @author  Asmir Mustafic <goetas@gmail.com>
+ */
+class MultiGetRegionTest extends AbstractRegionTest
+{
+    protected function createRegion()
+    {
+        return new DefaultMultiGetRegion('default.region.test', $this->cache);
+    }
+
+    public function testGetMulti()
+    {
+        $key1 = new CacheKeyMock('key.1');
+        $value1 = new CacheEntryMock(array('id'=>1, 'name' => 'bar'));
+        
+        $key2 = new CacheKeyMock('key.2');
+        $value2 = new CacheEntryMock(array('id'=>2, 'name' => 'bar'));
+
+        $this->assertFalse($this->region->contains($key1));
+        $this->assertFalse($this->region->contains($key2));
+
+        $this->region->put($key1, $value1);
+        $this->region->put($key2, $value2);
+
+        $actual = $this->region->getMulti(array($key1, $key2));
+
+        $this->assertEquals($value1, $actual[0]);
+        $this->assertEquals($value2, $actual[1]);
+    }
+}
