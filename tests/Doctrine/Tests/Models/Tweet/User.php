7292920b15d8a4ea7cf4d3a4b31bb2b46c850cd1
Marco Pivetta <ocramius@gmail.com>
2015-01-24 11:54:03 +0100

    #1169 DDC-3343 - refactoring test to use pre-existing test models

---
+++ tests/Doctrine/Tests/Models/Tweet/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Doctrine\Tests\Models\Tweet;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity
+ * @Table(name="tweet_user")
+ */
+class User
+{
+    const CLASSNAME = __CLASS__;
+
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    public $id;
+
+    /**
+     * @Column(type="string")
+     */
+    public $name;
+
+    /**
+     * @OneToMany(targetEntity="Tweet", mappedBy="author", cascade={"persist"}, fetch="EXTRA_LAZY")
+     */
+    public $tweets;
+
+    public function __construct()
+    {
+        $this->tweets = new ArrayCollection();
+    }
+
+    public function addTweet(Tweet $tweet)
+    {
+        $tweet->setAuthor($this);
+        $this->tweets->add($tweet);
+    }
+}
