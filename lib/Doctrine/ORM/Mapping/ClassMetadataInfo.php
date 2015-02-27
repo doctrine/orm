07f4556a1b34f68093b2064c1c582828fe5da49a
Ed Epstein <ed@rosterbot.com>
2015-02-27 01:05:59 -0800

    Fix whitespace.

--- lib/Doctrine/ORM/Mapping/ClassMetadataInfo.php
+++ lib/Doctrine/ORM/Mapping/ClassMetadataInfo.php
@@ -1566,13 +1566,13 @@ class ClassMetadataInfo implements ClassMetadata
             $uniqueConstraintColumns = array();
             foreach ($mapping['joinColumns'] as &$joinColumn) {
                 if ($mapping['type'] === self::ONE_TO_ONE && ! $this->isInheritanceTypeSingleTable()) {
-                        if (count($mapping['joinColumns']) == 1) {
-                            if ( (! isset($mapping['id']) || ! $mapping['id'])) {
-                                $joinColumn['unique'] = isset($joinColumn['unique']) ? $joinColumn['unique'] : true ;
-                            }
-                        } else {
-                            $uniqueContraintColumns[] = $joinColumn['name'];
+                    if (count($mapping['joinColumns']) == 1) {
+                        if ( (! isset($mapping['id']) || ! $mapping['id'])) {
+                            $joinColumn['unique'] = isset($joinColumn['unique']) ? $joinColumn['unique'] : true ;
                         }
+                    } else {
+                        $uniqueContraintColumns[] = $joinColumn['name'];
+                    }
                 }
 
                 if (empty($joinColumn['name'])) {
