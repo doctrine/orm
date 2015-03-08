0a144d1c91d0ecdaa90c9ffac7917e4d3cf4a6bc
Fábio Carneiro <fhcs@live.com>
2015-03-08 20:48:30 -0300

    load all many to many join columns

--- lib/Doctrine/ORM/Mapping/Driver/YamlDriver.php
+++ lib/Doctrine/ORM/Mapping/Driver/YamlDriver.php
@@ -514,9 +514,9 @@ class YamlDriver extends FileDriver
                             if ( ! isset($joinColumnElement['name'])) {
                                 $joinColumnElement['name'] = $joinColumnName;
                             }
+                            
+                            $joinTable['joinColumns'][] = $this->joinColumnToArray($joinColumnElement);
                         }
-
-                        $joinTable['joinColumns'][] = $this->joinColumnToArray($joinColumnElement);
                     }
 
                     if (isset($joinTableElement['inverseJoinColumns'])) {
@@ -524,9 +524,9 @@ class YamlDriver extends FileDriver
                             if ( ! isset($joinColumnElement['name'])) {
                                 $joinColumnElement['name'] = $joinColumnName;
                             }
+                            
+                            $joinTable['inverseJoinColumns'][] = $this->joinColumnToArray($joinColumnElement);
                         }
-
-                        $joinTable['inverseJoinColumns'][] = $this->joinColumnToArray($joinColumnElement);
                     }
 
                     $mapping['joinTable'] = $joinTable;
