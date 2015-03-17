0c0d3a1a7c1d4d54dca8b80951aa948ec7784126
Marco Pivetta <ocramius@gmail.com>
2015-03-17 23:14:29 +0000

    #1316 - removing unused API from stub class

--- tests/Doctrine/Tests/Models/NonPublicSchemaJoins/User.php
+++ tests/Doctrine/Tests/Models/NonPublicSchemaJoins/User.php
@@ -10,70 +10,31 @@ namespace Doctrine\Tests\Models\NonPublicSchemaJoins;
  */
 class User
 {
+    const CLASSNAME = __CLASS__;
+
     /**
      * @Column(type="integer")
      * @Id
      */
-    private $id;
+    public $id;
 
     /**
      * @ManyToMany(targetEntity="Doctrine\Tests\Models\NonPublicSchemaJoins\User", inversedBy="authors")
-     * @JoinTable(name="author_reader", schema="readers",
+     * @JoinTable(
+     *      name="author_reader",
+     *      schema="readers",
      *      joinColumns={@JoinColumn(name="author_id", referencedColumnName="id")},
      *      inverseJoinColumns={@JoinColumn(name="reader_id", referencedColumnName="id")}
      * )
      *
      * @var User[]
      */
-    private $readers;
+    public $readers;
 
     /**
      * @ManyToMany(targetEntity="Doctrine\Tests\Models\NonPublicSchemaJoins\User", mappedBy="readers")
      *
      * @var User[]
      */
-    private $authors;
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return User[]
-     */
-    public function getReaders()
-    {
-        return $this->readers;
-    }
-
-    /**
-     * @param User[] $readers
-     * @return User
-     */
-    public function setReaders($readers)
-    {
-        $this->readers = $readers;
-
-        return $this;
-    }
-
-    /**
-     * @return User[]
-     */
-    public function getAuthors()
-    {
-        return $this->authors;
-    }
-
-    /**
-     * @param User[] $authors
-     * @return User
-     */
-    public function setAuthors($authors)
-    {
-        $this->authors = $authors;
-
-        return $this;
-    }
+    public $authors;
 }
