1e38d7d07edd0ccf6a510328270662566df9c093
Marco Pivetta <ocramius@gmail.com>
2015-03-17 22:30:06 +0000

    #1338 DDC-3619 - moved test to unit of work tests

--- tests/Doctrine/Tests/ORM/UnitOfWorkTest.php
+++ tests/Doctrine/Tests/ORM/UnitOfWorkTest.php
@@ -229,6 +229,21 @@ class UnitOfWorkTest extends \Doctrine\Tests\OrmTestCase
         $this->setExpectedException('InvalidArgumentException');
         $this->_unitOfWork->lock(null, null, null);
     }
+
+    public function testRemovedAndRePersistedEntitiesAreInTheIdentityMapAndAreNotGarbageCollected()
+    {
+        $entity     = new ForumUser();
+        $entity->id = 123;
+
+        $this->_unitOfWork->registerManaged($entity, array('id' => 123), array());
+        $this->assertTrue($this->_unitOfWork->isInIdentityMap($entity));
+
+        $this->_unitOfWork->remove($entity);
+        $this->assertFalse($this->_unitOfWork->isInIdentityMap($entity));
+
+        $this->_unitOfWork->persist($entity);
+        $this->assertTrue($this->_unitOfWork->isInIdentityMap($entity));
+    }
 }
 
 /**
