98081232630e4e59bc2f25d9e3693f0f609e7cbe
napsi <napsi@napsi.dk>
2015-04-22 16:29:50 +0200

    indentation

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -436,21 +436,21 @@ class UnitOfWork implements PropertyChangedListener
         $this->visitedCollections =
         $this->orphanRemovals = array();
 
-		if ($entity === null) {
-        	$this->entityChangeSets =
-        	$this->scheduledForSynchronization = array();
+        if ($entity === null) {
+            $this->entityChangeSets =
+            $this->scheduledForSynchronization = array();
         } elseif (is_object($entity)) {
-        	$oid			= spl_object_hash($entity);
-        	$rootClassName	= $this->em->getClassMetadata(get_class($entity))->rootEntityName;
-        	unset($this->entityChangeSets[$oid]);
-        	unset($this->scheduledForSynchronization[$rootClassName][$oid]);
+            $oid           = spl_object_hash($entity);
+            $rootClassName = $this->em->getClassMetadata(get_class($entity))->rootEntityName;
+            unset($this->entityChangeSets[$oid]);
+            unset($this->scheduledForSynchronization[$rootClassName][$oid]);
         } elseif (is_array($entity)) {
-        	foreach ($entity as $object) {
-        		$oid			= spl_object_hash($object);
-        		$rootClassName	= $this->em->getClassMetadata(get_class($object))->rootEntityName;
-        		unset($this->entityChangeSets[$oid]);
-        		unset($this->scheduledForSynchronization[$rootClassName][$oid]);
-        	}
+            foreach ($entity as $object) {
+                $oid           = spl_object_hash($object);
+                $rootClassName = $this->em->getClassMetadata(get_class($object))->rootEntityName;
+                unset($this->entityChangeSets[$oid]);
+                unset($this->scheduledForSynchronization[$rootClassName][$oid]);
+            }
         }
     }
 
