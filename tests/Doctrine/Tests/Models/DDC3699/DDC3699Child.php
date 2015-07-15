86abbb0e786c63a5b9ed997c983f963aa26362be
Marco Pivetta <ocramius@gmail.com>
2015-07-15 21:46:23 +0100

    DDC-3699 - #1387 - simpifying tests, clarifying on test method names

--- tests/Doctrine/Tests/Models/DDC3699/DDC3699Child.php
+++ tests/Doctrine/Tests/Models/DDC3699/DDC3699Child.php
@@ -2,90 +2,20 @@
 
 namespace Doctrine\Tests\Models\DDC3699;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
-/**
- * @Entity
- * @Table(name="ddc3699_child")
- */
+/** @Entity @Table(name="ddc3699_child") */
 class DDC3699Child extends DDC3699Parent
 {
     const CLASSNAME = __CLASS__;
 
-    /**
-     * @Id
-     * @Column(type="integer")
-     */
-    protected $id;
+    /** @Id @Column(type="integer") */
+    public $id;
 
-    /**
-     * @Column(type="string")
-     */
-    protected $childField;
+    /** @Column(type="string") */
+    public $childField;
 
-    /**
-     * @OneToOne(targetEntity="DDC3699RelationOne", inversedBy="child")
-     */
-    protected $oneRelation;
+    /** @OneToOne(targetEntity="DDC3699RelationOne", inversedBy="child") */
+    public $oneRelation;
 
-    /**
-     * @OneToMany(targetEntity="DDC3699RelationMany", mappedBy="child")
-     */
-    protected $relations;
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function getChildField()
-    {
-        return $this->childField;
-    }
-
-    public function setChildField($childField)
-    {
-        $this->childField = $childField;
-    }
-
-    public function getOneRelation()
-    {
-        return $this->oneRelation;
-    }
-
-    public function setOneRelation($oneRelation)
-    {
-        $this->oneRelation = $oneRelation;
-    }
-
-    public function hasRelation($relation)
-    {
-        return $this->relations && $this->relations->contains($relation);
-    }
-
-    public function addRelation($relation)
-    {
-        if (!$this->hasRelation($relation)) {
-            $this->relations[] = $relation;
-        }
-
-        return $this;
-    }
-
-    public function removeRelation($relation)
-    {
-        $this->relations->removeElement($relation);
-
-        return $this;
-    }
-
-    public function getRelations()
-    {
-        return $this->relations;
-    }
+    /** @OneToMany(targetEntity="DDC3699RelationMany", mappedBy="child") */
+    public $relations;
 }
