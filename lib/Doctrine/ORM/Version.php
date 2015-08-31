e6a83bedbe67579cb0bfb688e982e617943a2945
Benjamin Eberlei <kontakt@beberlei.de>
2015-08-31 14:59:39 +0200

    Release 2.5.1

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.5.1-DEV';
+    const VERSION = '2.5.1';
 
     /**
      * Compares a Doctrine version with the current one.
