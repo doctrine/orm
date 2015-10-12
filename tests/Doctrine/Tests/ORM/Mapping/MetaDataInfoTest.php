7c0a98f41cf2d4ecdeb4bc4236b61a538b0a6cf7
cybermo <dropins@live.com>
2015-10-12 16:44:30 +0200

    Create MetaDataInfoTest.php

---
+++ tests/Doctrine/Tests/ORM/Mapping/MetaDataInfoTest.php
@@ -0,0 +1,53 @@
+namespace Doctrine\Tests\ORM\Mapping;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+/**
+ * @group DDC-3944
+ */
+class ClassMetadataBuilderTest extends \Doctrine\Tests\OrmTestCase
+{
+    private $product;
+
+    public function setUp()
+    {
+        $this->product = new Product();
+    }
+    
+    /**
+     * @group DDC-3944
+     */
+    public function testRemoveLifecycleCallbackNotFound()
+    {
+        $productMd = new ClassMetadata('Product');
+        $productMd->initializeReflection(new RuntimeReflectionService());
+        
+        $callBacks = $productMd->getLifecycleCallbacks('PrePersist');
+        
+        $this->assertTrue( in_array('setUpdatedAtValue', $callBacks) );
+        $product->removeLifecycleCallback('setUpdatedAtValue', 'PrePersist');
+        $this->assertFalse( in_array('setUpdatedAtValue', $callBacks) );
+    }
+    
+    
+    /**
+ * @ORM\Entity()
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Product
+{
+    protected $id;
+    protected $name;
+    protacted $updatedAt;
+    
+  /**
+   * @ORM\PrePersist
+   */
+  public function setUpdatedAtValue()
+  {
+      $this->updatedAt = new \DateTime();
+  }
+}
+
+
+    
+    
