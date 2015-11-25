8ea62b95b8e4a8b3de000a59f018ec250b41d714
Guilherme Blanco <guilhermeblanco@gmail.com>
2015-11-25 04:20:35 +0000

    Tests around reported cases over DDC-2524

--- tests/Doctrine/Tests/ORM/CommitOrderCalculatorTest.php
+++ tests/Doctrine/Tests/ORM/CommitOrderCalculatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Doctrine\Tests\ORM;
 
+use Doctrine\ORM\Internal\CommitOrderCalculator;
 use Doctrine\ORM\Mapping\ClassMetadata;
 
 /**
@@ -17,7 +18,7 @@ class CommitOrderCalculatorTest extends \Doctrine\Tests\OrmTestCase
 
     protected function setUp()
     {
-        $this->_calc = new \Doctrine\ORM\Internal\CommitOrderCalculator();
+        $this->_calc = new CommitOrderCalculator();
     }
 
     public function testCommitOrdering1()
@@ -28,21 +29,41 @@ class CommitOrderCalculatorTest extends \Doctrine\Tests\OrmTestCase
         $class4 = new ClassMetadata(__NAMESPACE__ . '\NodeClass4');
         $class5 = new ClassMetadata(__NAMESPACE__ . '\NodeClass5');
 
-        $this->_calc->addClass($class1);
-        $this->_calc->addClass($class2);
-        $this->_calc->addClass($class3);
-        $this->_calc->addClass($class4);
-        $this->_calc->addClass($class5);
+        $this->_calc->addNode($class1->name, $class1);
+        $this->_calc->addNode($class2->name, $class2);
+        $this->_calc->addNode($class3->name, $class3);
+        $this->_calc->addNode($class4->name, $class4);
+        $this->_calc->addNode($class5->name, $class5);
 
-        $this->_calc->addDependency($class1, $class2);
-        $this->_calc->addDependency($class2, $class3);
-        $this->_calc->addDependency($class3, $class4);
-        $this->_calc->addDependency($class5, $class1);
+        $this->_calc->addDependency($class1->name, $class2->name, 1);
+        $this->_calc->addDependency($class2->name, $class3->name, 1);
+        $this->_calc->addDependency($class3->name, $class4->name, 1);
+        $this->_calc->addDependency($class5->name, $class1->name, 1);
 
-        $sorted = $this->_calc->getCommitOrder();
+        $sorted = $this->_calc->sort();
 
         // There is only 1 valid ordering for this constellation
         $correctOrder = array($class5, $class1, $class2, $class3, $class4);
+
+        $this->assertSame($correctOrder, $sorted);
+    }
+
+    public function testCommitOrdering2()
+    {
+        $class1 = new ClassMetadata(__NAMESPACE__ . '\NodeClass1');
+        $class2 = new ClassMetadata(__NAMESPACE__ . '\NodeClass2');
+
+        $this->_calc->addNode($class1->name, $class1);
+        $this->_calc->addNode($class2->name, $class2);
+
+        $this->_calc->addDependency($class1->name, $class2->name, 0);
+        $this->_calc->addDependency($class2->name, $class1->name, 1);
+
+        $sorted = $this->_calc->sort();
+
+        // There is only 1 valid ordering for this constellation
+        $correctOrder = array($class2, $class1);
+
         $this->assertSame($correctOrder, $sorted);
     }
 }
