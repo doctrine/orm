8ef52077d2b7d2854b6dff44346d5216ff273e85
bilouwan <moroine.bentefrit@adactive.com>
2015-11-27 17:16:17 +0100

    Fix Spl_object_hash conflict on merge (reverted from commit 54bebabb4066f3c54f5504c3b951d04eeadc4014) (reverted from commit 0408ffd793471863de6c3f8c88f3a1cbbb0750db)

--- tests/Doctrine/Tests/ORM/Functional/MergeSplHashConflictTest.php
+++ tests/Doctrine/Tests/ORM/Functional/MergeSplHashConflictTest.php
@@ -2,14 +2,8 @@
 
 namespace Doctrine\Tests\ORM\Functional;
 
-use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\ORM\Query;
 use Doctrine\Tests\Models\CMS\CmsUser;
-use Doctrine\Tests\Models\CMS\CmsPhonenumber;
-use Doctrine\Tests\Models\CMS\CmsAddress;
-use Doctrine\Tests\Models\CMS\CmsGroup;
-use Doctrine\Tests\Models\CMS\CmsArticle;
-use Doctrine\Tests\Models\CMS\CmsComment;
 
 class MergeSplHashConflict extends \Doctrine\Tests\OrmFunctionalTestCase
 {
@@ -19,6 +13,9 @@ class MergeSplHashConflict extends \Doctrine\Tests\OrmFunctionalTestCase
         parent::setUp();
     }
 
+    /**
+     * @test
+     */
     public function testMergeSplHashConflict()
     {
         // Create
@@ -28,7 +25,9 @@ class MergeSplHashConflict extends \Doctrine\Tests\OrmFunctionalTestCase
         $mergedUser = $this->_em->merge($user);
         unset($user);
 
+        $this->assertTrue(false);
+
         // As $user is unset, $hash could be reused by spl_object_hash
-        $this->assertEquals([], $this->_em->getUnitOfWork()->getOriginalEntityDataByOid($hash));
+        $this->assertEquals(["gdhs"], $this->_em->getUnitOfWork()->getOriginalEntityDataByOid($hash));
     }
 }
