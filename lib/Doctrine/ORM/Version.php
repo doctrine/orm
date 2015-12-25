1c6524db5566cf9e4c5d2c85693890a1517b1e3c
Marco Pivetta <ocramius@gmail.com>
2015-12-25 16:50:31 +0100

    Bumping to development version 2.5.4-DEV

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.5.3';
+    const VERSION = '2.5.4-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
