06b3ae2cb6d092e66c99d599d9780a3acf99ee7d
bilouwan <moroine.bentefrit@adactive.com>
2015-12-29 12:26:53 +0100

    Improve code coverage

--- tests/Doctrine/Tests/ORM/Functional/MergeSplHashConflictTest.php
+++ tests/Doctrine/Tests/ORM/Functional/MergeSplHashConflictTest.php
@@ -14,8 +14,48 @@ class MergeSplHashConflictTest extends OrmFunctionalTestCase
         parent::setUp();
     }
 
+
     /**
      * @test
+     * @covers Doctrine\ORM\UnitOfWork::forgetEntity
+     */
+    public function testForgetEntity()
+    {
+        $user = new CmsUser;
+        $user->username = 'bilouwan';
+        $user->name = 'moroine';
+        $hash = spl_object_hash($user);
+        $this->_em->persist($user);
+        $this->_em->flush();
+
+        $uow = $this->_em->getUnitOfWork();
+        $reflexion = new \ReflectionClass(get_class($uow));
+        $method = $reflexion->getMethod('forgetEntity');
+        $method->setAccessible(true);
+        $method->invoke($uow, $user);
+
+        $propertyNames = [
+            'entityInsertions',
+            'entityUpdates',
+            'entityDeletions',
+            'entityIdentifiers',
+            'entityStates',
+            'originalEntityData',
+        ];
+
+        foreach ($propertyNames as $propertyName) {
+            $property = $reflexion->getProperty($propertyName);
+            $property->setAccessible(true);
+            $originalEntityData = $property->getValue($uow);
+
+            // As $user is unset, $hash could be reused by spl_object_hash
+            $this->assertArrayNotHasKey($hash, $originalEntityData);
+        }
+    }
+
+    /**
+     * @test
+     * @covers Doctrine\ORM\UnitOfWork::merge
      */
     public function testMergeSplHashConflict()
     {
