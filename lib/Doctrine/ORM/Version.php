bc4ddbfb0114cb33438cc811c9a740d8aa304aab
Marco Pivetta <ocramius@gmail.com>
2016-01-05 22:34:58 +0100

    Release 2.5.4

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.5.4-DEV';
+    const VERSION = '2.5.4';
 
     /**
      * Compares a Doctrine version with the current one.
