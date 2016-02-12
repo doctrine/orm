c970e1f290860e6dc3605b7a27ab760ecc3bef75
Steffen Abel <abel@redhotmagma.de>
2016-02-12 16:13:25 +0100

    Fixed Filter Condition for inverse side

--- lib/Doctrine/ORM/Persisters/Entity/BasicEntityPersister.php
+++ lib/Doctrine/ORM/Persisters/Entity/BasicEntityPersister.php
@@ -1282,11 +1282,19 @@ class BasicEntityPersister implements EntityPersister
                 $this->currentPersisterContext->selectJoinSql .= ' LEFT JOIN';
 
                 foreach ($association['joinColumns'] as $joinColumn) {
+
+                    $tableAlias = $this->getSQLTableAlias($association['sourceEntity'], $assocAlias);
+
                     $sourceCol       = $this->quoteStrategy->getJoinColumnName($joinColumn, $this->class, $this->platform);
                     $targetCol       = $this->quoteStrategy->getReferencedJoinColumnName($joinColumn, $this->class, $this->platform);
 
-                    $joinCondition[] = $this->getSQLTableAlias($association['sourceEntity'], $assocAlias) . '.' . $sourceCol . ' = '
+                    $joinCondition[] = $tableAlias . '.' . $sourceCol . ' = '
                         . $this->getSQLTableAlias($association['targetEntity']) . '.' . $targetCol;
+
+                    // Add filter SQL
+                    if ($filterSql = $this->generateFilterConditionSQL($eagerEntity, $tableAlias)) {
+                        $joinCondition[] = $filterSql;
+                    }
                 }
             }
 
