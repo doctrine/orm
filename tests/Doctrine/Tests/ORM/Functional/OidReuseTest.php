a3d93afc4fb4b8c984f79a7b4a88d689c9b41305
Mathieu De Zutter <mathieu@dezutter.org>
2016-03-01 19:51:38 +0100

    Additional assertion to check that unreferenced objects are not in UOW.

--- tests/Doctrine/Tests/ORM/Functional/OidReuseTest.php
+++ tests/Doctrine/Tests/ORM/Functional/OidReuseTest.php
@@ -22,10 +22,18 @@ class OidReuseTest extends OrmFunctionalTestCase
 
     public function testOidReuse()
     {
+        $uow = $this->_em->getUnitOfWork();
+        $reflexion = new \ReflectionClass(get_class($uow));
+        $originalEntityDataProperty = $reflexion->getProperty('originalEntityData');
+        $originalEntityDataProperty->setAccessible(true);
+
         $user = new CmsUser();
+        $oid = spl_object_hash($user);
         $this->_em->merge($user);
 
         $user = null;
+
+        $this->assertArrayNotHasKey($oid, $originalEntityDataProperty->getValue($uow));
 
         $user = new CmsUser();
         $this->_em->persist($user);
