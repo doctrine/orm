4448bef01eb5e84e95c74d118d5dcf37f9b13b7c
Fernando Andre <netriver@gmail.com>
2016-03-22 18:08:58 -0300

    fix syntax error

--- tests/Doctrine/Tests/ORM/UnknownEntityRepositoryMethodTest.php
+++ tests/Doctrine/Tests/ORM/UnknownEntityRepositoryMethodTest.php
@@ -14,7 +14,7 @@ class UnknownEntityRepositoryMethodTest extends PHPUnit_Framework_TestCase
 {
     public function testFromClassNameAndIdentifier()
     {
-        $exception = new UnknownEntityRepositoryMethod('foo', 'bar')
+        $exception = new UnknownEntityRepositoryMethod('foo', 'bar');
 
         $this->assertInstanceOf('Doctrine\ORM\UnknownEntityRepositoryMethod', $exception);
         $this->assertSame('Entity of type \'foo\' has no method bar() in the Repository class.Use findBy or findOneBy or implement the method.', $exception->getMessage());
