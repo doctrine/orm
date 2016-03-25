d2be74e00a28e4b4f9001ba38f66e1f3694a2d08
Steven <Steven>
2016-03-25 19:26:16 +1100

    Added orderBy parameter to findAll method

--- tests/Doctrine/Tests/ORM/Functional/EntityRepositoryTest.php
+++ tests/Doctrine/Tests/ORM/Functional/EntityRepositoryTest.php
@@ -253,6 +253,20 @@ class EntityRepositoryTest extends \Doctrine\Tests\OrmFunctionalTestCase
         $this->assertEquals(4, count($users));
     }
 
+    public function testFindAllOrderBy()
+    {
+        $this->loadFixture();
+
+        $repos = $this->_em->getRepository('Doctrine\Tests\Models\CMS\CmsUser');
+        $usersAsc = $repos->findAll(array("username" => "ASC"));
+        $usersDesc = $repos->findAll(array("username" => "DESC"));
+
+        $this->assertEquals(4, count($usersAsc), "Pre-condition: only four users in fixture");
+        $this->assertEquals(4, count($usersDesc), "Pre-condition: only four users in fixture");
+        $this->assertSame($usersAsc[0], $usersDesc[3]);
+        $this->assertSame($usersAsc[3], $usersDesc[0]);
+    }
+
     public function testFindByAlias()
     {
         $user1Id = $this->loadFixture();
