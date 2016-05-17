b836de866ae148226f016056d26e16dd08152440
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-05-17 22:25:37 +0000

    Initial code for FieldMetadata

--- tests/Doctrine/Tests/ORM/Mapping/AbstractMappingDriverTest.php
+++ tests/Doctrine/Tests/ORM/Mapping/AbstractMappingDriverTest.php
@@ -67,11 +67,12 @@ abstract class AbstractMappingDriverTest extends OrmTestCase
     }
 
     /**
-     * @depends testLoadMapping
      * @param ClassMetadata $class
      */
-    public function testEntityTableNameAndInheritance($class)
+    public function testEntityTableNameAndInheritance()
     {
+        $class = $this->createClassMetadata(User::class);
+
         self::assertEquals('cms_users', $class->getTableName());
         self::assertEquals(ClassMetadata::INHERITANCE_TYPE_NONE, $class->inheritanceType);
 
@@ -276,11 +277,13 @@ abstract class AbstractMappingDriverTest extends OrmTestCase
     {
         $class = $this->createClassMetadata(User::class);
 
-        $this->assertInternalType('bool', $class->fieldMappings['id']['options']['unsigned']);
-        $this->assertFalse($class->fieldMappings['id']['options']['unsigned']);
+        $idOptions = $class->getProperty('id')->getOptions();
+        $nameOptions = $class->getProperty('name')->getOptions();
 
-        $this->assertInternalType('bool', $class->fieldMappings['name']['options']['fixed']);
-        $this->assertFalse($class->fieldMappings['name']['options']['fixed']);
+        self::assertInternalType('bool', $idOptions['unsigned']);
+        self::assertFalse($idOptions['unsigned']);
+        self::assertInternalType('bool', $nameOptions['fixed']);
+        self::assertFalse($nameOptions['fixed']);
 
         return $class;
     }
