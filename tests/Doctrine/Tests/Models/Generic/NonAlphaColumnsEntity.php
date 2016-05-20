0d2bb69d655fae75c18457a996e9f576222ef5e3
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-05-20 23:35:00 +0000

    Removed quoting support and make sure we always quote data. Still missing checks for discriminator column and version column.

--- tests/Doctrine/Tests/Models/Generic/NonAlphaColumnsEntity.php
+++ tests/Doctrine/Tests/Models/Generic/NonAlphaColumnsEntity.php
@@ -4,19 +4,19 @@ namespace Doctrine\Tests\Models\Generic;
 
 /**
  * @Entity
- * @Table(name="`not-a-simple-entity`")
+ * @Table(name="not-a-simple-entity")
  */
 class NonAlphaColumnsEntity
 {
     /**
      * @Id
-     * @Column(type="integer", name="`simple-entity-id`")
+     * @Column(type="integer", name="simple-entity-id")
      * @GeneratedValue(strategy="AUTO")
      */
     public $id;
 
     /**
-     * @Column(type="string", name="`simple-entity-value`")
+     * @Column(type="string", name="simple-entity-value")
      */
     public $value;
 
