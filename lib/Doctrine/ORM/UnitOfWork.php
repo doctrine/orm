beb26414926757a63e65a7b860fd0f1b77121dcf
Rico Humme <rico@redant.nl>
2016-06-03 16:31:35 +0200

    Correct naming convention of function. Was confusing otherwise

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2391,7 +2391,7 @@ class UnitOfWork implements PropertyChangedListener
             $this->orphanRemovals = array();
         } else {
             $this->clearIdentityMap($entityName);
-            $this->clearIdentityInsertions($entityName);
+            $this->clearEntityInsertions($entityName);
         }
 
         if ($this->evm->hasListeners(Events::onClear)) {
@@ -3476,7 +3476,7 @@ class UnitOfWork implements PropertyChangedListener
     /**
      * @param $entityName
      */
-    private function clearIdentityInsertions($entityName)
+    private function clearEntityInsertions($entityName)
     {
         foreach ($this->entityInsertions as $hash => $entity) {
             if (get_class($entity) === $entityName) {
