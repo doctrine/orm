8adee19b1ed425428a316feefdd99b279808e670
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-07-11 01:28:37 +0000

    Fixes and implemented incomplete tests

--- tests/Doctrine/Tests/ORM/Mapping/StaticPHPMappingDriverTest.php
+++ tests/Doctrine/Tests/ORM/Mapping/StaticPHPMappingDriverTest.php
@@ -11,7 +11,6 @@ class StaticPHPMappingDriverTest extends AbstractMappingDriverTest
         return new StaticPHPDriver(__DIR__ . DIRECTORY_SEPARATOR . 'php');
     }
 
-
     /**
      * All class with static::loadMetadata are entities for php driver
      *
@@ -20,23 +19,5 @@ class StaticPHPMappingDriverTest extends AbstractMappingDriverTest
     public function testinvalidEntityOrMappedSuperClassShouldMentionParentClasses()
     {
         $this->createClassMetadata('Doctrine\Tests\Models\DDC889\DDC889Class');
-    }
-
-    /**
-     * @group DDC-2825
-     * @group 881
-     */
-    public function testSchemaDefinitionViaExplicitTableSchemaAnnotationProperty()
-    {
-        $this->markTestIncomplete();
-    }
-
-    /**
-     * @group DDC-2825
-     * @group 881
-     */
-    public function testSchemaDefinitionViaSchemaDefinedInTableNameInTableAnnotationProperty()
-    {
-        $this->markTestIncomplete();
     }
 }
