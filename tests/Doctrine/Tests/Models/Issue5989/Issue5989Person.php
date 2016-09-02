12b5e79ff2d99ea6dfe8b23a77ae515c741c324a
Carl Vuorinen <carl.vuorinen@gmail.com>
2016-09-02 10:31:26 +0300

    Create a failing test for issue #5989
    Field with type=simple_array in a joined inheritance gets overridden by empty array in the hydrator

---
+++ tests/Doctrine/Tests/Models/Issue5989/Issue5989Person.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Doctrine\Tests\Models\Issue5989;
+
+/**
+ * @Entity
+ * @Table(name="issue5989_persons")
+ * @InheritanceType("JOINED")
+ * @DiscriminatorColumn(name="discr", type="string")
+ * @DiscriminatorMap({
+ *      "person"    = "Issue5989Person",
+ *      "manager"   = "Issue5989Manager",
+ *      "employee"  = "Issue5989Employee"
+ * })
+ */
+class Issue5989Person
+{
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $id;
+
+    public function getId()
+    {
+        return  $this->id;
+    }
+}
