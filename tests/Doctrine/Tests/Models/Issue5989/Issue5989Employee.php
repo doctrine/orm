c973e4447a7c088b718bc39e1aa6b1870e9e1a80
Carl Vuorinen <carl.vuorinen@gmail.com>
2016-09-08 11:48:34 +0300

    PR fixes (public properties & correct letter case in annotations)

--- tests/Doctrine/Tests/Models/Issue5989/Issue5989Employee.php
+++ tests/Doctrine/Tests/Models/Issue5989/Issue5989Employee.php
@@ -9,19 +9,9 @@ namespace Doctrine\Tests\Models\Issue5989;
 class Issue5989Employee extends Issue5989Person
 {
     /**
-     * @column(type="simple_array", nullable=true)
+     * @Column(type="simple_array", nullable=true)
      *
      * @var array
      */
-    private $tags;
-
-    public function getTags()
-    {
-        return $this->tags;
-    }
-
-    public function setTags(array $tags)
-    {
-        $this->tags = $tags;
-    }
+    public $tags;
 }
