fc01791b22070741e77a9ee0aceb61d61f8e455e
Javier Spagnoletti <phansys@gmail.com>
2016-11-18 12:35:51 -0300

    Removed surplus semicolon at `EntityGenerator::generateFieldMappingPropertyDocBlock()`

--- lib/Doctrine/ORM/Tools/EntityGenerator.php
+++ lib/Doctrine/ORM/Tools/EntityGenerator.php
@@ -1627,7 +1627,7 @@ public function __construct(<params>)
     {
         $lines = array();
         $lines[] = $this->spaces . '/**';
-        $lines[] = $this->spaces . ' * @var ' . $this->getType($fieldMapping['type']) . ($this->getNullableField($fieldMapping) ? '|' . $this->getNullableField($fieldMapping) : '');;
+        $lines[] = $this->spaces . ' * @var ' . $this->getType($fieldMapping['type']) . ($this->getNullableField($fieldMapping) ? '|' . $this->getNullableField($fieldMapping) : '');
 
         if ($this->generateAnnotations) {
             $lines[] = $this->spaces . ' *';
