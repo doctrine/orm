de4b40b29f72dc000359c63546ead6e4e6e66006
Michał Kurzeja <mkurzeja@accesto.pl>
2016-12-12 21:03:25 +0200

    #6167 Code review updates, better readability
    (cherry picked from commit 571115c)

--- tests/Doctrine/Tests/ORM/Id/SequenceGeneratorTest.php
+++ tests/Doctrine/Tests/ORM/Id/SequenceGeneratorTest.php
@@ -25,12 +25,12 @@ class SequenceGeneratorTest extends OrmTestCase
     public function testGeneration()
     {
         $this->_em->getConnection()->setFetchOneException(
-            new \Exception('Fetch* method used. Query method should be used instead, as NEXTVAL should be run on a master server in master-slave setup.')
+            new \RuntimeException('Fetch* method used. Query method should be used instead, as NEXTVAL should be run on a master server in master-slave setup.')
         );
 
         for ($i=0; $i < 42; ++$i) {
             if ($i % 10 == 0) {
-                $nextId = array(array((int)($i / 10) * 10));
+                $nextId = [[(int)($i / 10) * 10]];
                 $this->_em->getConnection()->setQueryResult(new StatementArrayMock($nextId));
             }
             $id = $this->_seqGen->generate($this->_em, null);
