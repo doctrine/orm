9c2b54b748f9cfb857995d20ec90ddd690c712ba
Marco Pivetta <ocramius@gmail.com>
2017-06-21 07:27:41 +0200

    Adding classes required by the `SchemaToolTest` that exist in 'master', but not in '2.5'

--- tests/Doctrine/Tests/ORM/Tools/SchemaToolTest.php
+++ tests/Doctrine/Tests/ORM/Tools/SchemaToolTest.php
@@ -208,3 +208,45 @@ class UniqueConstraintAnnotationModel
      */
     private $hash;
 }
+
+/**
+ * @Entity
+ * @Table(name="first_entity")
+ */
+class FirstEntity
+{
+    /**
+     * @Id
+     * @Column(name="id")
+     */
+    public $id;
+
+    /**
+     * @OneToOne(targetEntity="SecondEntity")
+     * @JoinColumn(name="id", referencedColumnName="fist_entity_id")
+     */
+    public $secondEntity;
+
+    /**
+     * @Column(name="name")
+     */
+    public $name;
+}
+
+/**
+ * @Entity
+ * @Table(name="second_entity")
+ */
+class SecondEntity
+{
+    /**
+     * @Id
+     * @Column(name="fist_entity_id")
+     */
+    public $fist_entity_id;
+
+    /**
+     * @Column(name="name")
+     */
+    public $name;
+}
