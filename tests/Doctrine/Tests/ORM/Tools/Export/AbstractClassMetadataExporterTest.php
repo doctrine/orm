e5cb4e5455442c107398a52ed03094941da592d5
Marco Pivetta <ocramius@gmail.com>
2017-06-24 11:55:31 +0200

    #1577 removing `merge` and `detach` related mapping operations in cascades in tests

--- tests/Doctrine/Tests/ORM/Tools/Export/AbstractClassMetadataExporterTest.php
+++ tests/Doctrine/Tests/ORM/Tools/Export/AbstractClassMetadataExporterTest.php
@@ -245,8 +245,6 @@ abstract class AbstractClassMetadataExporterTest extends OrmTestCase
         self::assertContains('remove', $property->getCascade());
         self::assertContains('persist', $property->getCascade());
         self::assertNotContains('refresh', $property->getCascade());
-        self::assertNotContains('merge', $property->getCascade());
-        self::assertNotContains('detach', $property->getCascade());
         self::assertTrue($property->isOrphanRemoval());
         self::assertEquals(FetchMode::EAGER, $property->getFetchMode());
 
@@ -283,8 +281,6 @@ abstract class AbstractClassMetadataExporterTest extends OrmTestCase
         self::assertContains('remove', $property->getCascade());
         self::assertContains('persist', $property->getCascade());
         self::assertNotContains('refresh', $property->getCascade());
-        self::assertContains('merge', $property->getCascade());
-        self::assertNotContains('detach', $property->getCascade());
         self::assertTrue($property->isOrphanRemoval());
         self::assertEquals(FetchMode::LAZY, $property->getFetchMode());
 
@@ -321,8 +317,6 @@ abstract class AbstractClassMetadataExporterTest extends OrmTestCase
         self::assertContains('remove', $property->getCascade());
         self::assertContains('persist', $property->getCascade());
         self::assertContains('refresh', $property->getCascade());
-        self::assertContains('merge', $property->getCascade());
-        self::assertContains('detach', $property->getCascade());
 
         self::assertEquals(FetchMode::EXTRA_LAZY, $property->getFetchMode());
 
@@ -358,10 +352,8 @@ abstract class AbstractClassMetadataExporterTest extends OrmTestCase
         self::assertNotNull($property);
 
         self::assertContains('persist', $property->getCascade());
-        self::assertContains('merge', $property->getCascade());
         self::assertContains('remove', $property->getCascade());
         self::assertNotContains('refresh', $property->getCascade());
-        self::assertNotContains('detach', $property->getCascade());
 
         self::assertTrue($property->isOrphanRemoval());
 
