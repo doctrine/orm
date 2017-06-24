1a6451168b44d78a7955abf61a9d182a5203f3aa
Marco Pivetta <ocramius@gmail.com>
2017-06-24 11:22:57 +0200

    #1577 re-adding `merge()` and `detach()`, since a release of doctrine/common is required to completely remove those methods from the `ObjectManager` interface first

--- lib/Doctrine/ORM/EntityManager.php
+++ lib/Doctrine/ORM/EntityManager.php
@@ -338,6 +338,26 @@ final class EntityManager implements EntityManagerInterface
     }
 
     /**
+     * {@inheritDoc}
+     *
+     * @deprecated
+     */
+    public function merge($object)
+    {
+        throw new \BadMethodCallException('@TODO method disabled - will be removed in 3.0 with a release of doctrine/common');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @deprecated
+     */
+    public function detach($object)
+    {
+        throw new \BadMethodCallException('@TODO method disabled - will be removed in 3.0 with a release of doctrine/common');
+    }
+
+    /**
      * Flushes all changes to objects that have been queued up to now to the database.
      * This effectively synchronizes the in-memory state of managed objects with the
      * database.
