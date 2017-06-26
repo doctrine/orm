8cc29e84a0d68edbb8a5f09bf4bdc90b18190c6f
fullbl <daniele.pastori@libero.it>
2017-06-26 10:42:09 +0200

    Update AbstractHydrator.php
    revert strict comparison in hydration (fails on some dates)

--- lib/Doctrine/ORM/Internal/Hydration/AbstractHydrator.php
+++ lib/Doctrine/ORM/Internal/Hydration/AbstractHydrator.php
@@ -297,7 +297,7 @@ abstract class AbstractHydrator
                     if(
                         isset($cacheKeyInfo['discriminatorColumn']) && 
                         isset($data[$cacheKeyInfo['discriminatorColumn']]) &&
-                        $data[$cacheKeyInfo['discriminatorColumn']] !== $cacheKeyInfo['discriminatorValue']
+                        $data[$cacheKeyInfo['discriminatorColumn']] != $cacheKeyInfo['discriminatorValue']
                     ){
                         break;
                     }
