de9ce4242e0e20ee3309308b2250d0a0c8da052e
PyRowMan <vaillentin@gmail.com>
2017-06-28 09:52:43 +0200

    Partition code
    * Move the code into a dedicated function
    
    * Remove an unwanted double-quote

--- lib/Doctrine/ORM/Tools/EntityGenerator.php
+++ lib/Doctrine/ORM/Tools/EntityGenerator.php
@@ -1659,22 +1659,8 @@ public function __construct(<params>)
                 $column[] = 'nullable=' .  var_export($fieldMapping['nullable'], true);
             }
 
-            $options = [];
-
-            if (isset($fieldMapping['options']) && $fieldMapping['options']) {
-                foreach ($fieldMapping['options'] as $key => $value) {
-                    if ($value) {
-                        if ($key === 'unsigned') {
-                            $options[] = '"unsigned"=true';
-                        } else {
-                            $options[] = '"' . $key . '"="' . $value . '""';
-                        }
-                    }
-                }
-            }
-
-            if ($options) {
-                $column[] = 'options={'.implode(',', $options).'}';
+            if (isset($fieldMapping['options']) && !empty($fieldMapping['options'])) {
+                $column[] = $this->generateFormattedOptionsString($fieldMapping['options'])
             }
 
             if (isset($fieldMapping['columnDefinition'])) {
@@ -1721,6 +1707,28 @@ public function __construct(<params>)
         $lines[] = $this->spaces . ' */';
 
         return implode("\n", $lines);
+    }
+    
+    /**
+     * @param array $optionsArray
+     *
+     * @return string
+     */
+    protected function generateFormattedOptionsString(array $optionsArray) 
+    {
+        $options  = []
+        foreach ($optionsArray as $key => $value) {
+            if ($value) {
+                if ($key === 'unsigned') {
+                    $options[] = '"unsigned"=true';
+                } else {
+                    $options[] = '"' . $key . '"="' . $value . '"';
+                }
+            }
+        }
+        if ($options) {
+            return 'options={'.implode(',', $options).'}';
+        }
     }
 
     /**
