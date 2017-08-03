eacb4613fe9d3185564d443710879fa687ff60f2
Usman Zafar <usman@mrmerkle.com.au>
2017-08-03 12:44:29 +0000

    added the test and updated the entitymanager

--- tests/Doctrine/Tests/Mocks/EntityManagerMock.php
+++ tests/Doctrine/Tests/Mocks/EntityManagerMock.php
@@ -7,11 +7,21 @@ namespace Doctrine\Tests\Mocks;
 use Doctrine\Common\EventManager;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping;
+use Doctrine\ORM\NativeQuery;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\PessimisticLockException;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Utility\IdentifierFlattener;
 
 /**
  * Special EntityManager mock used for testing purposes.
  */
-class EntityManagerMock extends EntityManager
+class EntityManagerMock implements EntityManagerInterface
 {
     /**
      * @var \Doctrine\ORM\UnitOfWork|null
@@ -23,12 +33,25 @@ class EntityManagerMock extends EntityManager
      */
     private $proxyFactoryMock;
 
+    /** @var  EntityManagerInterface $em */
+    private $em;
+
+    /**
+     * EntityManagerMock constructor.
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getUnitOfWork()
     {
-        return isset($this->uowMock) ? $this->uowMock : parent::getUnitOfWork();
+        return isset($this->uowMock) ? $this->uowMock : $this->em->getUnitOfWork();
     }
 
     /* Mock API */
@@ -60,7 +83,7 @@ class EntityManagerMock extends EntityManager
      */
     public function getProxyFactory()
     {
-        return isset($this->proxyFactoryMock) ? $this->proxyFactoryMock : parent::getProxyFactory();
+        return isset($this->proxyFactoryMock) ? $this->proxyFactoryMock : $this->em->getProxyFactory();
     }
 
     /**
@@ -79,7 +102,502 @@ class EntityManagerMock extends EntityManager
         if (null === $eventManager) {
             $eventManager = new EventManager();
         }
+        return new EntityManagerMock(new EntityManager($conn, $config, $eventManager));
+    }
 
-        return new EntityManagerMock($conn, $config, $eventManager);
+    /**
+     * Returns the cache API for managing the second level cache regions or NULL if the cache is not enabled.
+     *
+     * @return \Doctrine\ORM\Cache|null
+     */
+    public function getCache()
+    {
+        return $this->em->getCache();
+    }
+
+    /**
+     * Gets the database connection object used by the EntityManager.
+     *
+     * @return \Doctrine\DBAL\Connection
+     */
+    public function getConnection()
+    {
+        return $this->em->getConnection();
+    }
+
+
+    /**
+     * @return Query\Expr
+     */
+    public function getExpressionBuilder()
+    {
+        return $this->em->getExpressionBuilder();
+    }
+
+    /**
+     * Gets an IdentifierFlattener used for converting Entities into an array of identifier values.
+     *
+     * @return IdentifierFlattener
+     */
+    public function getIdentifierFlattener()
+    {
+        return $this->em->getIdentifierFlattener();
+    }
+
+    /**
+     * Starts a transaction on the underlying database connection.
+     *
+     * @return void
+     */
+    public function beginTransaction()
+    {
+        $this->em->beginTransaction();
+    }
+
+    /**
+     * Executes a function in a transaction.
+     *
+     * The function gets passed this EntityManager instance as an (optional) parameter.
+     *
+     * {@link flush} is invoked prior to transaction commit.
+     *
+     * If an exception occurs during execution of the function or flushing or transaction commit,
+     * the transaction is rolled back, the EntityManager closed and the exception re-thrown.
+     *
+     * @param callable $func The function to execute transactionally.
+     *
+     * @return mixed The value returned from the closure.
+     *
+     * @throws \Throwable
+     */
+    public function transactional(callable $func)
+    {
+        return $this->em->transactional($func);
+    }
+
+    /**
+     * Commits a transaction on the underlying database connection.
+     *
+     * @return void
+     */
+    public function commit()
+    {
+        $this->em->commit();
+    }
+
+    /**
+     * Performs a rollback on the underlying database connection.
+     *
+     * @return void
+     */
+    public function rollback()
+    {
+        $this->em->rollback();
+    }
+
+    /**
+     * Creates a new Query object.
+     *
+     * @param string $dql The DQL string.
+     *
+     * @return Query
+     */
+    public function createQuery($dql = '')
+    {
+        return $this->em->createQuery($dql);
+    }
+
+    /**
+     * Creates a Query from a named query.
+     *
+     * @param string $name
+     *
+     * @return Query
+     */
+    public function createNamedQuery($name)
+    {
+        return $this->em->createNamedQuery($name);
+    }
+
+    /**
+     * Creates a native SQL query.
+     *
+     * @param string $sql
+     * @param ResultSetMapping $rsm The ResultSetMapping to use.
+     *
+     * @return NativeQuery
+     */
+    public function createNativeQuery($sql, ResultSetMapping $rsm)
+    {
+        return $this->em->createNativeQuery($sql, $rsm);
+    }
+
+    /**
+     * Creates a NativeQuery from a named native query.
+     *
+     * @param string $name
+     *
+     * @return NativeQuery
+     */
+    public function createNamedNativeQuery($name)
+    {
+        return $this->em->createNamedNativeQuery($name);
+    }
+
+    /**
+     * Create a QueryBuilder instance
+     *
+     * @return QueryBuilder
+     */
+    public function createQueryBuilder()
+    {
+        return $this->em->createQueryBuilder();
+    }
+
+    /**
+     * Gets a reference to the entity identified by the given type and identifier
+     * without actually loading it, if the entity is not yet loaded.
+     *
+     * @param string $entityName The name of the entity type.
+     * @param mixed $id          The entity identifier.
+     *
+     * @return object The entity reference.
+     *
+     * @throws ORMException
+     */
+    public function getReference($entityName, $id)
+    {
+        return $this->em->getReference($entityName, $id);
+    }
+
+    /**
+     * Gets a partial reference to the entity identified by the given type and identifier
+     * without actually loading it, if the entity is not yet loaded.
+     *
+     * The returned reference may be a partial object if the entity is not yet loaded/managed.
+     * If it is a partial object it will not initialize the rest of the entity state on access.
+     * Thus you can only ever safely access the identifier of an entity obtained through
+     * this method.
+     *
+     * The use-cases for partial references involve maintaining bidirectional associations
+     * without loading one side of the association or to update an entity without loading it.
+     * Note, however, that in the latter case the original (persistent) entity data will
+     * never be visible to the application (especially not event listeners) as it will
+     * never be loaded in the first place.
+     *
+     * @param string $entityName The name of the entity type.
+     * @param mixed $identifier  The entity identifier.
+     *
+     * @return object The (partial) entity reference.
+     */
+    public function getPartialReference($entityName, $identifier)
+    {
+        return $this->em->getPartialReference($entityName, $identifier);
+    }
+
+    /**
+     * Closes the EntityManager. All entities that are currently managed
+     * by this EntityManager become detached. The EntityManager may no longer
+     * be used after it is closed.
+     *
+     * @return void
+     */
+    public function close()
+    {
+        $this->em->close();
+    }
+
+    /**
+     * Creates a copy of the given entity. Can create a shallow or a deep copy.
+     *
+     * @param object $entity The entity to copy.
+     * @param boolean $deep  FALSE for a shallow copy, TRUE for a deep copy.
+     *
+     * @return object The new entity.
+     *
+     * @throws \BadMethodCallException
+     */
+    public function copy($entity, $deep = false)
+    {
+        return $this->em->copy($entity, $deep);
+    }
+
+    /**
+     * Acquire a lock on the given entity.
+     *
+     * @param object $entity
+     * @param int $lockMode
+     * @param int|null $lockVersion
+     *
+     * @return void
+     *
+     * @throws OptimisticLockException
+     * @throws PessimisticLockException
+     */
+    public function lock($entity, $lockMode, $lockVersion = null)
+    {
+        $this->em->lock($entity, $lockMode, $lockVersion);
+    }
+
+    /**
+     * Gets the EventManager used by the EntityManager.
+     *
+     * @return \Doctrine\Common\EventManager
+     */
+    public function getEventManager()
+    {
+        return $this->em->getEventManager();
+    }
+
+    /**
+     * Gets the Configuration used by the EntityManager.
+     *
+     * @return Configuration
+     */
+    public function getConfiguration()
+    {
+        return $this->em->getConfiguration();
+    }
+
+    /**
+     * Check if the Entity manager is open or closed.
+     *
+     * @return bool
+     */
+    public function isOpen()
+    {
+        return $this->em->isOpen();
+    }
+
+    /**
+     * Gets a hydrator for the given hydration mode.
+     *
+     * This method caches the hydrator instances which is used for all queries that don't
+     * selectively iterate over the result.
+     *
+     * @deprecated
+     *
+     * @param int $hydrationMode
+     *
+     * @return \Doctrine\ORM\Internal\Hydration\AbstractHydrator
+     */
+    public function getHydrator($hydrationMode)
+    {
+        return $this->em->getHydrator($hydrationMode);
+    }
+
+    /**
+     * Create a new instance for the given hydration mode.
+     *
+     * @param int $hydrationMode
+     *
+     * @return \Doctrine\ORM\Internal\Hydration\AbstractHydrator
+     *
+     * @throws ORMException
+     */
+    public function newHydrator($hydrationMode)
+    {
+        return $this->em->newHydrator($hydrationMode);
+    }
+
+    /**
+     * Gets the enabled filters.
+     *
+     * @return \Doctrine\ORM\Query\FilterCollection The active filter collection.
+     */
+    public function getFilters()
+    {
+        return $this->em->getFilters();
+    }
+
+    /**
+     * Checks whether the state of the filter collection is clean.
+     *
+     * @return boolean True, if the filter collection is clean.
+     */
+    public function isFiltersStateClean()
+    {
+        return $this->em->isFiltersStateClean();
+    }
+
+    /**
+     * Checks whether the Entity Manager has filters.
+     *
+     * @return boolean True, if the EM has a filter collection.
+     */
+    public function hasFilters()
+    {
+        return $this->em->hasFilters();
+    }
+
+    /**
+     * Finds an object by its identifier.
+     *
+     * This is just a convenient shortcut for getRepository($className)->find($id).
+     *
+     * @param string $className The class name of the object to find.
+     * @param mixed $id         The identity of the object to find.
+     *
+     * @return object The found object.
+     */
+    public function find($className, $id)
+    {
+        return $this->em->find($className, $id);
+    }
+
+    /**
+     * Tells the ObjectManager to make an instance managed and persistent.
+     *
+     * The object will be entered into the database as a result of the flush operation.
+     *
+     * NOTE: The persist operation always considers objects that are not yet known to
+     * this ObjectManager as NEW. Do not pass detached objects to the persist operation.
+     *
+     * @param object $object The instance to make managed and persistent.
+     *
+     * @return void
+     */
+    public function persist($object)
+    {
+        $this->em->persist($object);
+    }
+
+    /**
+     * Removes an object instance.
+     *
+     * A removed object will be removed from the database as a result of the flush operation.
+     *
+     * @param object $object The object instance to remove.
+     *
+     * @return void
+     */
+    public function remove($object)
+    {
+        $this->em->remove($object);
+    }
+
+    /**
+     * Merges the state of a detached object into the persistence context
+     * of this ObjectManager and returns the managed copy of the object.
+     * The object passed to merge will not become associated/managed with this ObjectManager.
+     *
+     * @param object $object
+     *
+     * @return object
+     */
+    public function merge($object)
+    {
+        return $this->em->merge($object);
+    }
+
+    /**
+     * Clears the ObjectManager. All objects that are currently managed
+     * by this ObjectManager become detached.
+     *
+     * @param string|null $objectName if given, only objects of this type will get detached.
+     *
+     * @return void
+     */
+    public function clear($objectName = null)
+    {
+        $this->em->clear($objectName);
+    }
+
+    /**
+     * Detaches an object from the ObjectManager, causing a managed object to
+     * become detached. Unflushed changes made to the object if any
+     * (including removal of the object), will not be synchronized to the database.
+     * Objects which previously referenced the detached object will continue to
+     * reference it.
+     *
+     * @param object $object The object to detach.
+     *
+     * @return void
+     */
+    public function detach($object)
+    {
+        $this->em->detach($object);
+    }
+
+    /**
+     * Refreshes the persistent state of an object from the database,
+     * overriding any local changes that have not yet been persisted.
+     *
+     * @param object $object The object to refresh.
+     *
+     * @return void
+     */
+    public function refresh($object)
+    {
+        $this->em->refresh($object);
+    }
+
+    /**
+     * Flushes all changes to objects that have been queued up to now to the database.
+     * This effectively synchronizes the in-memory state of managed objects with the
+     * database.
+     *
+     * @return void
+     */
+    public function flush()
+    {
+        $this->em->flush();
+    }
+
+    /**
+     * Gets the repository for a class.
+     *
+     * @param string $className
+     *
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    public function getRepository($className)
+    {
+        return $this->em->getRepository($className);
+    }
+
+    /**
+     * Gets the metadata factory used to gather the metadata of classes.
+     *
+     * @return \Doctrine\Common\Persistence\Mapping\ClassMetadataFactory
+     */
+    public function getMetadataFactory()
+    {
+        return $this->em->getMetadataFactory();
+    }
+
+    /**
+     * Helper method to initialize a lazy loading proxy or persistent collection.
+     *
+     * This method is a no-op for other objects.
+     *
+     * @param object $obj
+     *
+     * @return void
+     */
+    public function initializeObject($obj)
+    {
+        $this->em->initializeObject($obj);
+    }
+
+    /**
+     * Checks if the object is part of the current UnitOfWork and therefore managed.
+     *
+     * @param object $object
+     *
+     * @return bool
+     */
+    public function contains($object)
+    {
+        return $this->em->contains($object);
+    }
+
+    /**
+     * @param string $className
+     * @return Mapping\ClassMetadata
+     */
+    public function getClassMetadata($className) : Mapping\ClassMetadata
+    {
+        return $this->em->getClassMetadata($className);
     }
 }
