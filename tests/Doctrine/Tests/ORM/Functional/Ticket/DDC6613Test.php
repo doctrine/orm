693a0546d31f8a7e5876e2fa76cb998af9a87c1f
Marco Pivetta <ocramius@gmail.com>
2017-08-11 14:23:21 +0200

    #6613 #6614 CS - applying `@group` annotation to the test

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DDC6613Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC6613Test.php
@@ -8,23 +8,21 @@
 
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
-
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Embeddable;
 use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\PersistentCollection;
-use Doctrine\ORM\Proxy\Proxy;
-use Doctrine\Tests\Models\DDC6613\Phone;
-use Doctrine\Tests\Models\DDC6613\User;
+use Doctrine\Tests\OrmFunctionalTestCase;
 
-
-class DDC6613Test extends \Doctrine\Tests\OrmFunctionalTestCase
+/**
+ * @group #6613
+ * @group #6614
+ */
+class DDC6613Test extends OrmFunctionalTestCase
 {
-
     public function setUp()
     {
         parent::setUp();
@@ -33,7 +31,7 @@ class DDC6613Test extends \Doctrine\Tests\OrmFunctionalTestCase
             $this->setUpEntitySchema(
                 [
                     DDC6613Phone::class,
-                    DDC6613User::class
+                    DDC6613User::class,
                 ]
             );
         } catch (SchemaException $e) {
@@ -49,7 +47,7 @@ class DDC6613Test extends \Doctrine\Tests\OrmFunctionalTestCase
         $this->_em->clear();
 
         /** @var User $user */
-        $user = $this->_em->find(User::class, 1);
+        $user   = $this->_em->find(User::class, 1);
         $phone1 = new DDC6613Phone();
         $phones = $user->phones;
         $user->phones->add($phone1);
@@ -77,7 +75,6 @@ class DDC6613Test extends \Doctrine\Tests\OrmFunctionalTestCase
         self::assertTrue($phones->isInitialized());
         self::assertCount(2, $user->phones);
     }
-
 }
 
 /**
@@ -103,8 +100,6 @@ class DDC6613User
         $this->id     = uniqid('user', true);
         $this->phones = new ArrayCollection();
     }
-
-
 }
 
 /**
@@ -112,7 +107,6 @@ class DDC6613User
  */
 class DDC6613Phone
 {
-
     /**
      * @Id
      * @GeneratedValue(strategy="NONE")
