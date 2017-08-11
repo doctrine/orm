649c43f42468ca4707670c0f7cc58e8f6e9f8e6f
Marco Pivetta <ocramius@gmail.com>
2017-08-11 20:32:12 +0200

    Removing useless checking for never-used parameter, inlining merge operation

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -1056,16 +1056,10 @@ class UnitOfWork implements PropertyChangedListener
     /**
      * Gets the commit order.
      *
-     * @param array|null $entityChangeSet
-     *
      * @return array
      */
-    private function getCommitOrder(array $entityChangeSet = null)
+    private function getCommitOrder()
     {
-        if ($entityChangeSet === null) {
-            $entityChangeSet = array_merge($this->entityInsertions, $this->entityUpdates, $this->entityDeletions);
-        }
-
         $calc = new Internal\CommitOrderCalculator();
 
         // See if there are any new classes in the changeset, that are not in the
@@ -1075,7 +1069,7 @@ class UnitOfWork implements PropertyChangedListener
         // are not yet available.
         $newNodes = [];
 
-        foreach ($entityChangeSet as $entity) {
+        foreach (\array_merge($this->entityInsertions, $this->entityUpdates, $this->entityDeletions) as $entity) {
             $class = $this->em->getClassMetadata(get_class($entity));
 
             if ($calc->hasNode($class->getClassName())) {
