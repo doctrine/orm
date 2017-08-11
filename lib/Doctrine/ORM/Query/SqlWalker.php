4e2d1809ba174484f02d2df4a867b00ac88bac35
Marco Pivetta <ocramius@gmail.com>
2017-08-11 20:06:46 +0200

    Removed typical `if (count(expr)) {` issue from sql walker

--- lib/Doctrine/ORM/Query/SqlWalker.php
+++ lib/Doctrine/ORM/Query/SqlWalker.php
@@ -1853,7 +1853,7 @@ class SqlWalker implements TreeWalker
                 }
             }
 
-            if (count($filterClauses)) {
+            if ($filterClauses) {
                 if ($condSql) {
                     $condSql = '(' . $condSql . ') AND ';
                 }
