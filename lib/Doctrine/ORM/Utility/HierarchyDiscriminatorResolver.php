5224a89549453a8a021fbfd47d93829efecc0f5b
Alessandro Lai <alessandro.lai85@gmail.com>
2017-08-18 11:02:28 +0200

    Apply various and CS fixes as per review

--- lib/Doctrine/ORM/Utility/HierarchyDiscriminatorResolver.php
+++ lib/Doctrine/ORM/Utility/HierarchyDiscriminatorResolver.php
@@ -6,20 +6,27 @@ use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\ORM\EntityManagerInterface;
 
 /**
- * Class HierarchyDiscriminatorResolver
- * @package Doctrine\ORM\Utility
  * @internal This class exists only to avoid code duplication, do not reuse it externally
  */
-class HierarchyDiscriminatorResolver
+final class HierarchyDiscriminatorResolver
 {
+    private function __construct()
+    {
+    }
+
+    /**
+     * This method is needed to make INSTANCEOF work correctly with inheritance: if the class at hand has inheritance,
+     * it extracts all the discriminators from the child classes and returns them
+     */
     public static function resolveDiscriminatorsForClass(
         ClassMetadata $rootClassMetadata,
         EntityManagerInterface $entityManager
-    ) {
+    ): array {
         $hierarchyClasses = $rootClassMetadata->subClasses;
         $hierarchyClasses[] = $rootClassMetadata->name;
 
         $discriminators = [];
+
         foreach ($hierarchyClasses as $class) {
             $currentMetadata = $entityManager->getClassMetadata($class);
             $currentDiscriminator = $currentMetadata->discriminatorValue;
