6f40d2ec73f01997099f9396d449dcf9e5d58546
Marco Pivetta <ocramius@gmail.com>
2017-08-25 19:50:18 +0200

    #6638 #6648 ignoring setup schema exceptions

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH6638Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH6638Test.php
@@ -2,6 +2,7 @@
 
 namespace Doctrine\Tests\Functional\Ticket;
 
+use Doctrine\ORM\Tools\ToolsException;
 use Doctrine\Tests\OrmFunctionalTestCase;
 
 final class GH6638Test extends OrmFunctionalTestCase
@@ -10,17 +11,20 @@ final class GH6638Test extends OrmFunctionalTestCase
     {
         parent::setUp();
 
-        $this->_schemaTool->createSchema([
-            $this->_em->getClassMetadata(GH6638Customer::class),
-            $this->_em->getClassMetadata(GH6638Cart::class),
-        ]);
+        try {
+            $this->_schemaTool->createSchema([
+                $this->_em->getClassMetadata(GH6638Customer::class),
+                $this->_em->getClassMetadata(GH6638Cart::class),
+            ]);
+        } catch (ToolsException $ignored) {
+        }
     }
 
     public function testFetchingOfOneToOneRelations() : void
     {
         $initialCustomer = new GH6638Customer();
+        $initialCart     = new GH6638Cart();
 
-        $initialCart = new GH6638Cart();
         $initialCustomer->cart = $initialCart;
         $initialCart->customer = $initialCustomer;
 
