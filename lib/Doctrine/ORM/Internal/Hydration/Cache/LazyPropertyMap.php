6dd5063daf7c93dc9e3f01e4e6b2c22e5a91db24
Marco Pivetta <ocramius@gmail.com>
2017-08-26 14:28:15 +0200

    #6638 #6648 documenting why private-ish property is defined in userland rather than statically
    Fixes comment reported by @lcobucci at https://github.com/doctrine/doctrine2/pull/6649#discussion_r135386762

--- lib/Doctrine/ORM/Internal/Hydration/Cache/LazyPropertyMap.php
+++ lib/Doctrine/ORM/Internal/Hydration/Cache/LazyPropertyMap.php
@@ -13,7 +13,10 @@ final class LazyPropertyMap
 {
     public function __construct(callable $instantiate)
     {
-        $this->{__CLASS__ . "\0callback"} = $instantiate;
+        // Note: the reason why we use dynamic access to create a private-ish property is
+        //       that we do not want this property to be statically defined. If that
+        //       happens, then a consumer of `__get` may access it, and that's no good
+        $this->{self::class . "\0callback"} = $instantiate;
     }
 
     /**
@@ -23,7 +26,7 @@ final class LazyPropertyMap
      */
     public function __get(string $name)
     {
-        $this->$name = ($this->{__CLASS__ . "\0callback"})($name);
+        $this->$name = ($this->{self::class . "\0callback"})($name);
 
         return $this->$name;
     }
