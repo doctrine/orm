5ef465820cc168ff51717194a1f8017782fe47c9
Marco Pivetta <ocramius@gmail.com>
2017-09-22 00:45:27 +0200

    Corrected `DDC1228Test`, which was quite broken and relying on the old `Proxy` API

--- tests/Doctrine/Tests/ORM/Functional/Ticket/DDC1228Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC1228Test.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
 use Doctrine\ORM\Annotation as ORM;
+use ProxyManager\Proxy\GhostObjectInterface;
 
 /**
  * @group DDC-1228
@@ -39,20 +40,30 @@ class DDC1228Test extends \Doctrine\Tests\OrmFunctionalTestCase
         $this->em->flush();
         $this->em->clear();
 
-        $user = $this->em->find(DDC1228User::class, $user->id);
+        /* @var $fetchedUser DDC1228User */
+        $fetchedUser = $this->em->find(DDC1228User::class, $user->id);
 
-        self::assertFalse($user->getProfile()->__isInitialized(), "Proxy is not initialized");
-        $user->getProfile()->setName("Bar");
-        self::assertTrue($user->getProfile()->__isInitialized(), "Proxy is not initialized");
+        /* @var $fetchedProfile DDC1228Profile|GhostObjectInterface */
+        $fetchedProfile = $fetchedUser->getProfile();
 
-        self::assertEquals("Bar", $user->getProfile()->getName());
-        self::assertEquals(["id" => 1, "name" => "Foo"], $this->em->getUnitOfWork()->getOriginalEntityData($user->getProfile()));
+        self::assertInstanceOf(GhostObjectInterface::class, $fetchedProfile);
+        self::assertInstanceOf(DDC1228Profile::class, $fetchedProfile);
+        self::assertFalse($fetchedProfile->isProxyInitialized());
+
+        $fetchedProfile->setName("Bar");
+
+        self::assertTrue($fetchedProfile->isProxyInitialized());
+
+        self::assertEquals("Bar", $fetchedProfile->getName());
+        self::assertEquals(["id" => 1, "name" => "Foo"], $this->em->getUnitOfWork()->getOriginalEntityData($fetchedProfile));
 
         $this->em->flush();
         $this->em->clear();
 
-        $user = $this->em->find(DDC1228User::class, $user->id);
-        self::assertEquals("Bar", $user->getProfile()->getName());
+        /* @var $otherFetchedUser DDC1228User */
+        $otherFetchedUser = $this->em->find(DDC1228User::class, $fetchedUser->id);
+
+        self::assertEquals("Bar", $otherFetchedUser->getProfile()->getName());
     }
 
     public function testRefresh()
