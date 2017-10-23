e1b851f2e93a4afd82fb7e7f32c6f114d0ea7b71
Jonathan H. Wage <jwage@opensky.com>
2017-10-23 13:26:03 -0500

    Bumping development version to v2.5.13-DEV

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.5.12';
+    const VERSION = '2.5.13-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
