19db7510a7b49605a9139fc2964342a98a682a8c
Luís Cobucci <lcobucci@gmail.com>
2017-12-17 04:03:07 +0100

    Bumping development version to v2.5.15-DEV

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.5.14';
+    const VERSION = '2.5.15-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
