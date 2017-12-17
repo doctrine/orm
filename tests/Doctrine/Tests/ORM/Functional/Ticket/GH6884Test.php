3d65c2b59ac2792859298a9ff32db89340594268
andy obrien <andy@1bg.com>
2017-12-17 16:20:36 -0700

    Use ID from entity

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH6884Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH6884Test.php
@@ -51,8 +51,8 @@ final class GH6884Test extends OrmFunctionalTestCase
         $this->_em->clear();
 
         $people = [
-            1,
-            2
+            $person->id,
+            $person2->id
         ];
 
         foreach ($people as $person) {
