90d19b4131559955ce0604affd49fc0d6ba9e400
Luís Cobucci <lcobucci@gmail.com>
2017-12-20 02:01:05 +0100

    Bumping development version to v2.6.1-DEV

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -35,7 +35,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.6.0';
+    const VERSION = '2.6.1-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
