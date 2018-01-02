3c9172bad82e2acee24fc667062f449bfe54a954
Michael Moravec <mail@majkl578.cz>
2018-01-03 00:12:54 +0100

    [CS] Port NonUniqueResultException

--- lib/Doctrine/ORM/NonUniqueResultException.php
+++ lib/Doctrine/ORM/NonUniqueResultException.php
@@ -6,15 +6,12 @@ namespace Doctrine\ORM;
 
 /**
  * Exception thrown when an ORM query unexpectedly returns more than one result.
- *
- * @author robo
- * @since 2.0
  */
 class NonUniqueResultException extends UnexpectedResultException
 {
-    const DEFAULT_MESSAGE = 'More than one result was found for query although one row or none was expected.';
+    public const DEFAULT_MESSAGE = 'More than one result was found for query although one row or none was expected.';
 
-    public function __construct(string $message = null)
+    public function __construct(?string $message = null)
     {
         parent::__construct($message ?? self::DEFAULT_MESSAGE);
     }
