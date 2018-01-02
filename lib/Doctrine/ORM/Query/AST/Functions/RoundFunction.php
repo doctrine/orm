ee8e2159a3454a8f78fce961230ab82994cd0448
Cédric GARDIENNET <cedric@fioulreduc.com>
2018-01-02 11:32:55 +0100

    ref #6942 Add class RoundFunction

---
+++ lib/Doctrine/ORM/Query/AST/Functions/RoundFunction.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ORM\Query\AST\Functions;
+
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * "ROUND" "(" SimpleArithmeticExpression "," SimpleArithmeticExpression ")"
+ *
+ *
+ * @link    www.doctrine-project.org
+ * @since   2.0
+ * @author  Cédric Gardiennet <cedric.gardiennet@gmail.com>
+ */
+class RoundFunction extends FunctionNode
+{
+    /**
+     * @var \Doctrine\ORM\Query\AST\SimpleArithmeticExpression
+     */
+    public $simpleArithmeticExpressionColumn;
+
+    /**
+     * @var \Doctrine\ORM\Query\AST\SimpleArithmeticExpression
+     */
+    public $simpleArithmeticExpressionDecimals;
+
+    /**
+     * @override
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return $sqlWalker->getConnection()->getDatabasePlatform()->getRoundExpression(
+               $sqlWalker->walkSimpleArithmeticExpression($this->simpleArithmeticExpressionColumn),
+               $sqlWalker->walkSimpleArithmeticExpression($this->simpleArithmeticExpressionDecimals)
+        );
+    }
+
+    /**
+     * @override
+     */
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->simpleArithmeticExpressionColumn = $parser->SimpleArithmeticExpression();
+
+        $parser->match(Lexer::T_COMMA);
+
+        $this->simpleArithmeticExpressionDecimals = $parser->SimpleArithmeticExpression();
+
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
