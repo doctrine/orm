9f8af8ee4cb3c621c4d721954dd90cfdb9075b7c
Marco Pivetta <ocramius@gmail.com>
2018-03-04 13:24:20 +0100

    #7113 #7095 removed unused imports

--- lib/Doctrine/ORM/Mapping/ClassMetadata.php
+++ lib/Doctrine/ORM/Mapping/ClassMetadata.php
@@ -17,7 +17,6 @@ use function array_map;
 use function array_merge;
 use function class_exists;
 use function count;
-use function explode;
 use function get_class;
 use function in_array;
 use function interface_exists;
@@ -25,7 +24,6 @@ use function is_subclass_of;
 use function method_exists;
 use function spl_object_id;
 use function sprintf;
-use function strpos;
 
 /**
  * A <tt>ClassMetadata</tt> instance holds all the object-relational mapping metadata
