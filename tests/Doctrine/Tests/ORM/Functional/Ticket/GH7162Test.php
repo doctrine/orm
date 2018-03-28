d9815c00980abcb0ba895e2ccd8247ff24466eab
Cvetomir <cvetomir.lazarov@gmail.com>
2018-03-28 18:22:09 +0300

    Change the column name, used in the test

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH7162Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH7162Test.php
@@ -132,7 +132,7 @@ class GH7162Child
 
     /**
      * @ORM\ManyToOne(targetEntity=GH7162Parent::class, inversedBy="children")
-     * @ORM\JoinColumn(referencedColumnName="parentId")
+     * @ORM\JoinColumn(referencedColumnName="parent_id")
      *
      * @var GH7162Parent
      */
