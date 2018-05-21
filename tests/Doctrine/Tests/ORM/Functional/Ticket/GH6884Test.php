c909657770f431f11d5ec094d4d28b0b50c5433f
andy obrien <andy@1bg.com>
2018-05-21 00:43:17 -0600

    Add whitespace before return type colon

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH6884Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH6884Test.php
@@ -18,7 +18,7 @@ final class GH6884Test extends OrmFunctionalTestCase
     /**
      * {@inheritdoc}
      */
-    protected function setUp(): void
+    protected function setUp() : void
     {
         parent::setUp();
 
@@ -34,7 +34,7 @@ final class GH6884Test extends OrmFunctionalTestCase
                   ->addEntityListener(Events::postUpdate, GH6884Person::class, 'onPostUpdate');
     }
 
-    public function testIssue(): void
+    public function testIssue() : void
     {
         $person  = new GH6884Person();
         $person2 = new GH6884Person();
@@ -68,7 +68,7 @@ class GH6884Person
     /** @var bool */
     public $nonOrmProperty = false;
 
-    public function onPostUpdate(GH6884Person $person, LifecycleEventArgs $eventArgs): void
+    public function onPostUpdate(GH6884Person $person, LifecycleEventArgs $eventArgs) : void
     {
         $person->nonOrmProperty = true;
 
