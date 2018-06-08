17bc1ad5c01f1bdf2eace42ce92387c8ba3b2994
Zacharias Luiten <zacharias.luiten@karify.com>
2018-06-09 00:56:21 +0200

    Fix #4252 Cascade first before refreshing the entity itself so toMany associations are not reset to empty collections

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH4252Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH4252Test.php
@@ -1,27 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\Tests\OrmFunctionalTestCase;
+
+use function assert;
 
 /**
  * @group GH-4252
  */
-class GH4252Test extends \Doctrine\Tests\OrmFunctionalTestCase
+class GH4252Test extends OrmFunctionalTestCase
 {
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
-        $this->_schemaTool->createSchema(array(
+        $this->_schemaTool->createSchema([
             $this->_em->getClassMetadata(GH4252City::class),
             $this->_em->getClassMetadata(GH4252Resident::class),
             $this->_em->getClassMetadata(GH4252Address::class),
-        ));
+        ]);
     }
 
-    public function testIssue()
+    public function testIssue(): void
     {
         $city = new GH4252City([new GH4252Resident([new GH4252Address()])]);
 
@@ -29,20 +41,20 @@ class GH4252Test extends \Doctrine\Tests\OrmFunctionalTestCase
         $this->_em->flush();
         $this->_em->clear();
 
-        /** @var GH4252City $city */
         $city = $this->_em->find(GH4252City::class, $city->getId());
+        assert($city instanceof GH4252City);
         $city->setFlag(false);
-        /** @var GH4252Resident $resident */
         $resident = $city->getResidents()->first();
+        assert($resident instanceof GH4252Resident);
         $resident->setFlag(false);
-        /** @var GH4252Address $address */
         $address = $resident->getAddresses()->first();
+        assert($address instanceof GH4252Address);
         $address->setFlag(false);
 
         $this->_em->refresh($city);
 
         $resident = $city->getResidents()->first();
-        $address = $resident->getAddresses()->first();
+        $address  = $resident->getAddresses()->first();
 
         $this->assertTrue($city->getFlag());
         $this->assertTrue($resident->getFlag());
@@ -57,7 +69,9 @@ class GH4252City
 {
     /**
      * @var int
-     * @Id @Column(type="integer") @GeneratedValue
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
      */
     private $id;
 
@@ -69,7 +83,6 @@ class GH4252City
 
     /**
      * @var GH4252Resident[]|Collection
-     *
      * @OneToMany(targetEntity="Doctrine\Tests\ORM\Functional\Ticket\GH4252Resident", mappedBy="city", cascade={"persist","refresh"})
      */
     private $residents;
@@ -81,25 +94,26 @@ class GH4252City
             $this->residents->add($resident);
             $resident->setCity($this);
         }
+
         $this->flag = true;
     }
 
-    public function getId() : int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getFlag() : bool
+    public function getFlag(): bool
     {
         return $this->flag;
     }
 
-    public function setFlag(bool $flag) : void
+    public function setFlag(bool $flag): void
     {
         $this->flag = $flag;
     }
 
-    public function getResidents() : Collection
+    public function getResidents(): Collection
     {
         return $this->residents;
     }
@@ -130,7 +144,6 @@ class GH4252Resident
 
     /**
      * @var GH4252Address[]|Collection
-     *
      * @ManyToMany(targetEntity="Doctrine\Tests\ORM\Functional\Ticket\GH4252Address", fetch="EXTRA_LAZY", cascade={"persist","refresh"})
      */
     private $addresses;
@@ -141,35 +154,36 @@ class GH4252Resident
         foreach ($addresses as $address) {
             $this->addresses->add($address);
         }
+
         $this->flag = true;
     }
 
-    public function getId() : int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getCity() : GH4252City
+    public function getCity(): GH4252City
     {
         return $this->city;
     }
 
-    public function setCity(GH4252City $city) : void
+    public function setCity(GH4252City $city): void
     {
         $this->city = $city;
     }
 
-    public function getFlag() : bool
+    public function getFlag(): bool
     {
         return $this->flag;
     }
 
-    public function setFlag(bool $flag) : void
+    public function setFlag(bool $flag): void
     {
         $this->flag = $flag;
     }
 
-    public function getAddresses() : Collection
+    public function getAddresses(): Collection
     {
         return $this->addresses;
     }
@@ -180,7 +194,9 @@ class GH4252Address
 {
     /**
      * @var int
-     * @Id @Column(type="integer") @GeneratedValue
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
      */
     private $id;
 
@@ -195,17 +211,17 @@ class GH4252Address
         $this->flag = true;
     }
 
-    public function getId() : int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getFlag() : bool
+    public function getFlag(): bool
     {
         return $this->flag;
     }
 
-    public function setFlag(bool $flag) : void
+    public function setFlag(bool $flag): void
     {
         $this->flag = $flag;
     }
