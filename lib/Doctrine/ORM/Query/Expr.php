816588bc32eb5670620811fdfe5222eb384e1674
Einar Gangsø <mail@einargangso.no>
2019-01-10 08:30:10 +0100

    Andx is defined in Andx::$allowedClasses

--- lib/Doctrine/ORM/Query/Expr.php
+++ lib/Doctrine/ORM/Query/Expr.php
@@ -41,8 +41,8 @@ class Expr
      *     // (u.type = ?1) AND (u.role = ?2)
      *     $expr->andX($expr->eq('u.type', ':1'), $expr->eq('u.role', ':2'));
      *
-     * @param Expr\Comparison|Expr\Func|Expr\Orx|string $x Optional clause. Defaults to null, but requires at least one
-     *                                                     defined when converting to string.
+     * @param Expr\Comparison|Expr\Func|Expr\Orx|Expr\Andx|string $x Optional clause. Defaults to null, but requires at least one
+     *                                                               defined when converting to string.
      *
      * @return Expr\Andx
      */
