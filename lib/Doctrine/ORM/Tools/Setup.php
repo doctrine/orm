c74f5474e68e9c6ee24f00fb65122d3e8b67fdb2
Paul Court <g@rgoyle.com>
2019-07-11 11:21:27 +0100

    Fixed redis and memcached references.

--- lib/Doctrine/ORM/Tools/Setup.php
+++ lib/Doctrine/ORM/Tools/Setup.php
@@ -177,7 +177,7 @@ class Setup
             }
 
             if (extension_loaded('memcached')) {
-                $memcached = new Memcached();
+                $memcached = new \Memcached();
                 $memcached->addServer('127.0.0.1', 11211);
 
                 $cache = new \Doctrine\Common\Cache\MemcachedCache();
@@ -187,7 +187,7 @@ class Setup
             }
 
             if (extension_loaded('redis')) {
-                $redis = new Redis();
+                $redis = new \Redis();
                 $redis->connect('127.0.0.1');
 
                 $cache = new \Doctrine\Common\Cache\RedisCache();
