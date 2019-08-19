3c0a048f932a287390aa6144edb2edc8bc453a5f
Sébastien ALFAIATE <s.alfaiate@webarea.fr>
2019-08-19 18:42:15 +0200

    Fix CS

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH7805Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH7805Test.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Tests\Models\CMS\CmsArticle;
 use Doctrine\Tests\OrmFunctionalTestCase;
+use function count;
+use function iterator_to_array;
 
 class GH7805Test extends OrmFunctionalTestCase
 {
@@ -16,9 +20,10 @@ class GH7805Test extends OrmFunctionalTestCase
 
     public function testPaginationWithSimpleArithmetic()
     {
-        $article = new CmsArticle;
-        $article->topic = 'Test SimpleArithmetic ORDER BY';
-        $article->text = 'This test fails on MySQL.';
+        $article = new CmsArticle();
+
+        $article->topic   = 'Test SimpleArithmetic ORDER BY';
+        $article->text    = 'This test fails on MySQL.';
         $article->version = 1;
 
         $this->_em->persist($article);
