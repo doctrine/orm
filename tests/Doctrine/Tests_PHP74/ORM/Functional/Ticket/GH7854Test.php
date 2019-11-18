e244b12d17b38dfb0ba020093bb32c4d73f7d0f9
Patrik Foldes <studio22@mail.ru>
2019-11-19 00:14:58 +0100

    Add test case using typed properties in embeddables

---
+++ tests/Doctrine/Tests_PHP74/ORM/Functional/Ticket/GH7854Test.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Tests_PHP74\ORM\Functional\Ticket;
+
+use Doctrine\Tests\OrmFunctionalTestCase;
+
+/**
+ * @group GH7854
+ */
+class GH7854Test extends OrmFunctionalTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->_schemaTool->createSchema(
+            [
+                $this->_em->getClassMetadata(GH7854TestEntity::class),
+                $this->_em->getClassMetadata(GH7854ValueObject::class),
+            ]
+        );
+    }
+
+    public function testTypedPropertyContainingEmbeddable() : void
+    {
+        $entity = new GH7854TestEntity();
+        $this->_em->persist($entity);
+        $this->_em->flush();
+        $this->_em->clear();
+
+        $entities = $this->_em->getRepository(GH7854TestEntity::class)->findAll();
+    
+        self::assertEquals($entity, $entities[0]);
+    }
+}
+
+/**
+ * @Entity()
+ */
+class GH7854TestEntity
+{
+    /**
+     * @Embedded(class=GH7854ValueObject::class)
+     */
+    public GH7854ValueObject $valueObject;
+}
+
+/**
+ * @Embeddable()
+ */
+class GH7854ValueObject
+{
+    /** @Id() @Column(type="integer") @GeneratedValue(strategy="AUTO") */
+    public int $value;
+}
