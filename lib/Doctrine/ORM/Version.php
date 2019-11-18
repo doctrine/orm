4fae126459d7deb6cb2877fc0c0f8ecfa4658516
Luís Cobucci <lcobucci@gmail.com>
2019-11-18 23:05:16 +0100

    Bump up version

--- lib/Doctrine/ORM/Version.php
+++ lib/Doctrine/ORM/Version.php
@@ -35,7 +35,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.6.6';
+    const VERSION = '2.6.7-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
