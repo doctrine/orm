f810c775adf0f7046ea34d43e79f24f7fb8c4a71
Michał Jusięga <michal.jusiega@gmail.com>
2019-11-18 18:45:28 +0100

    Add missing imports

--- lib/Doctrine/ORM/Mapping/AssociationOverride.php
+++ lib/Doctrine/ORM/Mapping/AssociationOverride.php
@@ -19,6 +19,8 @@
 
 namespace Doctrine\ORM\Mapping;
 
+use Doctrine\Common\Annotations\Annotation\Enum;
+
 /**
  * This annotation is used to override association mapping of property for an entity relationship.
  *
--- lib/Doctrine/ORM/Mapping/Cache.php
+++ lib/Doctrine/ORM/Mapping/Cache.php
@@ -19,6 +19,8 @@
 
 namespace Doctrine\ORM\Mapping;
 
+use Doctrine\Common\Annotations\Annotation\Enum;
+
 /**
  * Caching to an entity or a collection.
  *
