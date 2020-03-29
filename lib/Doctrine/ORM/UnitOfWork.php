a51f9aad8dad8f752cb5df4cdfd33baede6d11df
octavius karma <octavius@localhost.localdomain>
2020-03-29 14:17:39 +0200

    replace isset by is_string

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2848,7 +2848,7 @@ class UnitOfWork implements PropertyChangedListener
                     $this->originalEntityData[$oid][$field] = $newValue;
                     $class->reflFields[$field]->setValue($entity, $newValue);
 
-                    if ($assoc['inversedBy'] && $assoc['type'] & ClassMetadata::ONE_TO_ONE && isset($targetClass->associationMappings[$assoc['inversedBy']])) {
+                    if ($assoc['inversedBy'] && $assoc['type'] & ClassMetadata::ONE_TO_ONE && is_string($targetClass->associationMappings[$assoc['inversedBy']] )) {
                         $inverseAssoc = $targetClass->associationMappings[$assoc['inversedBy']];
                         $targetClass->reflFields[$inverseAssoc['fieldName']]->setValue($newValue, $entity);
                     }
