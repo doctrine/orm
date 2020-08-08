9de2df7068a77f3045b4063fea7caf5fe08bcedc
Crossjoin <crossjoin@users.noreply.github.com>
2020-08-08 18:46:02 +0200

    Moved test to "Ticket" sub directory

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH8229Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH8229Test.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Doctrine\Tests\ORM\Functional;
+namespace Doctrine\Tests\ORM\Functional\Ticket;
 
 use Doctrine\ORM\Annotation\AttributeOverride;
 use Doctrine\ORM\Annotation\AttributeOverrides;
