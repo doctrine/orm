7d2b43116a825d71659ebe204b0db6f01dfdc5ce
Benjamin Eberlei <kontakt@beberlei.de>
2020-11-15 13:46:10 +0100

    [GH-8265] Skip AttributeDriverTest on PHP 7.

--- tests/Doctrine/Tests/ORM/Mapping/AttributeDriverTest.php
+++ tests/Doctrine/Tests/ORM/Mapping/AttributeDriverTest.php
@@ -6,6 +6,14 @@ use Doctrine\ORM\Mapping\Driver\AttributesDriver;
 
 class AttributeDriverTest extends AbstractMappingDriverTest
 {
+    /** @before */
+    public function requiresPhp8Assertion()
+    {
+        if (PHP_VERSION_ID <= 80000) {
+            $this->markTestSkipped('requies PHP 8.0');
+        }
+    }
+
     protected function _loadDriver()
     {
         $paths = [];
