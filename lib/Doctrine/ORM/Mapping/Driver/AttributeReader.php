78d999fe1aba853d38f908f7f94220faadeba261
Benjamin Eberlei <kontakt@beberlei.de>
2020-11-15 18:46:48 +0100

    [GH-8265] Fix AttributeReader styles.

--- lib/Doctrine/ORM/Mapping/Driver/AttributeReader.php
+++ lib/Doctrine/ORM/Mapping/Driver/AttributeReader.php
@@ -1,47 +1,64 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\ORM\Mapping\Driver;
 
-use Doctrine\Common\Annotations\Reader;
+use Attribute;
 use Doctrine\ORM\Mapping\Annotation;
+use ReflectionAttribute;
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionProperty;
+
+use function count;
+use function is_subclass_of;
 
 // TODO: Should we move this to doctrine/annotations?
-class AttributeReader implements Reader
+class AttributeReader
 {
     /** @var array<string,bool> */
-    private $isRepeatableAttribute = [];
+    private array $isRepeatableAttribute = [];
 
-    function getClassAnnotations(\ReflectionClass $class)
+    /** @return array<object> */
+    public function getClassAnnotations(ReflectionClass $class): array
     {
         return $this->convertToAttributeInstances($class->getAttributes());
     }
 
-    function getClassAnnotation(\ReflectionClass $class, $annotationName)
+    public function getClassAnnotation(ReflectionClass $class, $annotationName): ?object
     {
         return $this->getClassAnnotations($class)[$annotationName] ?? ($this->isRepeatable($annotationName) ? [] : null);
     }
 
-    function getMethodAnnotations(\ReflectionMethod $method)
+    /** @return array<object> */
+    public function getMethodAnnotations(ReflectionMethod $method): array
     {
         return $this->convertToAttributeInstances($method->getAttributes());
     }
 
-    function getMethodAnnotation(\ReflectionMethod $method, $annotationName)
+    public function getMethodAnnotation(ReflectionMethod $method, $annotationName): ?object
     {
         return $this->getMethodAnnotations($method)[$annotationName] ?? ($this->isRepeatable($annotationName) ? [] : null);
     }
 
-    function getPropertyAnnotations(\ReflectionProperty $property)
+    /** @return array<object> */
+    public function getPropertyAnnotations(ReflectionProperty $property): array
     {
         return $this->convertToAttributeInstances($property->getAttributes());
     }
 
-    function getPropertyAnnotation(\ReflectionProperty $property, $annotationName)
+    public function getPropertyAnnotation(ReflectionProperty $property, $annotationName): ?object
     {
         return $this->getPropertyAnnotations($property)[$annotationName] ?? ($this->isRepeatable($annotationName) ? [] : null);
     }
 
-    private function convertToAttributeInstances(array $attributes)
+    /**
+     * @param array<ReflectionAttribute> $attributes
+     *
+     * @return array<Annotation>
+     */
+    private function convertToAttributeInstances(array $attributes): array
     {
         $instances = [];
 
@@ -49,8 +66,8 @@ class AttributeReader implements Reader
             // Make sure we only get Doctrine Annotations
             if (is_subclass_of($attribute->getName(), Annotation::class)) {
                 $attributeClassName = $attribute->getName();
-                $instance = new $attributeClassName;
-                $arguments = $attribute->getArguments();
+                $instance           = new $attributeClassName();
+                $arguments          = $attribute->getArguments();
 
                 // unnamed argument is automatically "value" in Doctrine Annotations
                 if (count($arguments) >= 1 && isset($arguments[0])) {
@@ -74,15 +91,15 @@ class AttributeReader implements Reader
         return $instances;
     }
 
-    private function isRepeatable(string $attributeClassName) : bool
+    private function isRepeatable(string $attributeClassName): bool
     {
         if (isset($this->isRepeatableAttribute[$attributeClassName])) {
             return $this->isRepeatableAttribute[$attributeClassName];
         }
 
-        $reflectionClass = new \ReflectionClass($attributeClassName);
-        $attribute = $reflectionClass->getAttributes()[0]->newInstance();
+        $reflectionClass = new ReflectionClass($attributeClassName);
+        $attribute       = $reflectionClass->getAttributes()[0]->newInstance();
 
-        return $this->isRepeatableAttribute[$attributeClassName] = $attribute->flags & \Attribute::IS_REPEATABLE;
+        return $this->isRepeatableAttribute[$attributeClassName] = ($attribute->flags & Attribute::IS_REPEATABLE) > 0;
     }
 }
