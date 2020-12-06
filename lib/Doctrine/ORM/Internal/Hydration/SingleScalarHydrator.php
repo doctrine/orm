d83c0a6b6cb915b5066cf32d2aab7700d6593421
Bruce <32240357+Mediagone@users.noreply.github.com>
2020-12-06 03:41:01 +0100

    Fix Hidden fields triggering error when using getSingleScalarResult() (#8340)
    * Fix Hidden fields triggering error when using getSingleScalarResult()
    
    Fixes #4257
    HIDDEN fields was causing the "unicity" check to fail (NonUniqueResultException), because we was counting raw data instead of gathered row data.
    
    * Fix Coding Standards (7.4)
    
    * Fix Coding Standards (7.4) #2
    
    * Fix Coding Standards (7.4) - Fix whitespaces
    
    * Fix Coding Standards (7.4) - Fix whitespaces in tests
    
    * Fix Coding Standards (7.4) - Fix more things
    
    * Refactor tests into separate methods
    
    * Fix Coding Standards (7.4) - Equals sign not aligned with surrounding assignments

--- lib/Doctrine/ORM/Internal/Hydration/SingleScalarHydrator.php
+++ lib/Doctrine/ORM/Internal/Hydration/SingleScalarHydrator.php
@@ -21,6 +21,8 @@ namespace Doctrine\ORM\Internal\Hydration;
 
 use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\NonUniqueResultException;
+use function count;
+use function key;
 
 /**
  * Hydrator that hydrates a single scalar value from the result set.
@@ -46,12 +48,12 @@ class SingleScalarHydrator extends AbstractHydrator
         if ($numRows > 1) {
             throw new NonUniqueResultException('The query returned multiple rows. Change the query or use a different result function like getScalarResult().');
         }
-        
-        if (count($data[key($data)]) > 1) {
-            throw new NonUniqueResultException('The query returned a row containing multiple columns. Change the query or use a different result function like getScalarResult().');
-        }
 
         $result = $this->gatherScalarRowData($data[key($data)]);
+
+        if (count($result) > 1) {
+            throw new NonUniqueResultException('The query returned a row containing multiple columns. Change the query or use a different result function like getScalarResult().');
+        }
 
         return array_shift($result);
     }
