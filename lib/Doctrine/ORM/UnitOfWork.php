8b749642cdda49136a36303d1205dea29b2956e0
Benjamin Eberlei <kontakt@beberlei.de>
2020-12-14 11:00:07 +0100

    [GH-8231] Bugfix: Missed dirty check synchronization check. (#8392)
    When an entity with change tracking policy "deferred explicit" gets
    removed, then persisted again, it is not schedulded for a dirty check
    synchronization. This is not the case for entities that are persisted
    and are already in the managed state.

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -1796,6 +1796,10 @@ class UnitOfWork implements PropertyChangedListener
                 $this->addToIdentityMap($entity);
 
                 $this->entityStates[$oid] = self::STATE_MANAGED;
+
+                if ($class->isChangeTrackingDeferredExplicit()) {
+                    $this->scheduleForDirtyCheck($entity);
+                }
                 break;
 
             case self::STATE_DETACHED:
