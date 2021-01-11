d28a9ac7ac736058a5f508832be11048fb183a64
David Buchmann <david.buchmann@liip.ch>
2021-01-11 15:45:53 +0100

    fix incorrect type annotation

--- lib/Doctrine/ORM/LazyCriteriaCollection.php
+++ lib/Doctrine/ORM/LazyCriteriaCollection.php
@@ -23,7 +23,6 @@ use Doctrine\Common\Collections\AbstractLazyCollection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\Common\Collections\Selectable;
-use Doctrine\ORM\Persisters\Entity\BasicEntityPersister;
 use Doctrine\ORM\Persisters\Entity\EntityPersister;
 
 /**
@@ -39,7 +38,7 @@ use Doctrine\ORM\Persisters\Entity\EntityPersister;
 class LazyCriteriaCollection extends AbstractLazyCollection implements Selectable
 {
     /**
-     * @var BasicEntityPersister
+     * @var EntityPersister
      */
     protected $entityPersister;
 
