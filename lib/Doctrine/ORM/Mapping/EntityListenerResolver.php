1a46ed8901b43810bf3c7aede0cbb1a86033bd80
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2021-01-30 13:37:26 +0100

    Relax contract of EntityListenerResolver so it doesn't require class name (#8448)
    Co-authored-by: Grégoire Paris <postmaster@greg0ire.fr>

--- lib/Doctrine/ORM/Mapping/EntityListenerResolver.php
+++ lib/Doctrine/ORM/Mapping/EntityListenerResolver.php
@@ -29,24 +29,20 @@ namespace Doctrine\ORM\Mapping;
 interface EntityListenerResolver
 {
     /**
-     * Clear all instances from the set, or a specific class when given.
+     * Clear all instances from the set, or a specific instance when given its identifier.
      *
-     * @param string $className The fully-qualified class name
+     * @param string $className May be any arbitrary string. Name kept for BC only.
      *
      * @return void
-     *
-     * @psalm-param class-string $className
      */
     function clear($className = null);
 
     /**
-     * Returns a entity listener instance for the given class name.
+     * Returns a entity listener instance for the given identifier.
      *
-     * @param string $className The fully-qualified class name
+     * @param string $className May be any arbitrary string. Name kept for BC only.
      *
      * @return object An entity listener
-     *
-     * @psalm-param class-string $className
      */
     function resolve($className);
 
