210c2ee6a443d76bbe48f1d5d357bc43ed9ecf13
Grégoire Paris <postmaster@greg0ire.fr>
2021-01-30 11:32:03 +0100

    Remove strict types

--- tests/Doctrine/Tests/ORM/Functional/SecondLevelCacheCompositePrimaryKeyTest.php
+++ tests/Doctrine/Tests/ORM/Functional/SecondLevelCacheCompositePrimaryKeyTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Doctrine\Tests\ORM\Functional;
 
 use DateTime;
--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH6531Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH6531Test.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
 use Doctrine\Common\Collections\ArrayCollection;
