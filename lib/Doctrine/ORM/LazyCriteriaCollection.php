8724589c6e61aee77830b6637ae6961e65d4a402
Benjamin Eberlei <kontakt@beberlei.de>
2021-02-06 00:23:45 +0100

    Housekeeping: Fix wrong typehint - Closes #8421

--- lib/Doctrine/ORM/LazyCriteriaCollection.php
+++ lib/Doctrine/ORM/LazyCriteriaCollection.php
@@ -24,7 +24,6 @@ use Doctrine\Common\Collections\AbstractLazyCollection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\Common\Collections\Selectable;
-use Doctrine\ORM\Persisters\Entity\BasicEntityPersister;
 use Doctrine\ORM\Persisters\Entity\EntityPersister;
 
 /**
@@ -35,7 +34,7 @@ use Doctrine\ORM\Persisters\Entity\EntityPersister;
  */
 class LazyCriteriaCollection extends AbstractLazyCollection implements Selectable
 {
-    /** @var BasicEntityPersister */
+    /** @var EntityPersister */
     protected $entityPersister;
 
     /** @var Criteria */
