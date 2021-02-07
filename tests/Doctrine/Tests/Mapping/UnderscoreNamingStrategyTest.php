b3a267f6b2e9bb613e62b55bf51fcaf648810a1b
Benjamin Eberlei <kontakt@beberlei.de>
2021-02-07 17:46:25 +0100

    Replace more usages of VerifyDeprecations (akwardkly for now)

--- tests/Doctrine/Tests/Mapping/UnderscoreNamingStrategyTest.php
+++ tests/Doctrine/Tests/Mapping/UnderscoreNamingStrategyTest.php
@@ -4,20 +4,35 @@ declare(strict_types=1);
 
 namespace Doctrine\Tests\Mapping;
 
+use Doctrine\Deprecations\Deprecation;
 use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
-use Doctrine\Tests\VerifyDeprecations;
 use PHPUnit\Framework\TestCase;
 
 use const CASE_LOWER;
 
 final class UnderscoreNamingStrategyTest extends TestCase
 {
-    use VerifyDeprecations;
-
     /** @test */
     public function checkDeprecationMessage(): void
     {
-        $this->expectDeprecationMessageSame('Creating Doctrine\ORM\Mapping\UnderscoreNamingStrategy without making it number aware is deprecated and will be removed in Doctrine ORM 3.0.');
+        $before = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/pull/7908'] ?? 0;
+
         new UnderscoreNamingStrategy(CASE_LOWER, false);
+
+        $after = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/pull/7908'] ?? 0;
+
+        $this->assertSame($before + 1, $after);
+    }
+
+    /** @test */
+    public function checNoDeprecationMessageWhenNumberAwareEnabled(): void
+    {
+        $before = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/pull/7908'] ?? 0;
+
+        new UnderscoreNamingStrategy(CASE_LOWER, true);
+
+        $after = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/pull/7908'] ?? 0;
+
+        $this->assertSame($before, $after);
     }
 }
--- tests/Doctrine/Tests/ORM/EntityManagerTest.php
+++ tests/Doctrine/Tests/ORM/EntityManagerTest.php
@@ -7,6 +7,7 @@ namespace Doctrine\Tests\ORM;
 use BadMethodCallException;
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Connection;
+use Doctrine\Deprecations\Deprecation;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadataFactory;
@@ -23,7 +24,6 @@ use Doctrine\Persistence\Mapping\MappingException;
 use Doctrine\Tests\Models\CMS\CmsUser;
 use Doctrine\Tests\Models\GeoNames\Country;
 use Doctrine\Tests\OrmTestCase;
-use Doctrine\Tests\VerifyDeprecations;
 use InvalidArgumentException;
 use stdClass;
 use TypeError;
@@ -34,8 +34,6 @@ use function uniqid;
 
 class EntityManagerTest extends OrmTestCase
 {
-    use VerifyDeprecations;
-
     /** @var EntityManager */
     private $_em;
 
@@ -323,8 +321,13 @@ class EntityManagerTest extends OrmTestCase
         $entity = new Country(456, 'United Kingdom');
         $this->_em->persist($entity);
 
-        $this->expectDeprecationMessageSame('Calling Doctrine\ORM\EntityManager::clear() with any arguments to clear specific entities is deprecated and will not be supported in Doctrine ORM 3.0.');
+        $before = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/issues/8460'] ?? 0;
+
         $this->_em->clear(Country::class);
+
+        $after = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/issues/8460'] ?? 0;
+
+        $this->assertSame($before + 1, $after);
     }
 
     public function testDeprecatedFlushWithArguments(): void
@@ -332,38 +335,12 @@ class EntityManagerTest extends OrmTestCase
         $entity = new Country(456, 'United Kingdom');
         $this->_em->persist($entity);
 
-        $this->expectDeprecationMessageSame('Calling Doctrine\ORM\EntityManager::flush() with any arguments to flush specific entities is deprecated and will not be supported in Doctrine ORM 3.0.');
+        $before = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/issues/8459'] ?? 0;
+
         $this->_em->flush($entity);
-    }
 
-    public function testDeprecatedMerge(): void
-    {
-        $entity = new Country(456, 'United Kingdom');
-        $this->_em->persist($entity);
+        $after = Deprecation::getTriggeredDeprecations()['https://github.com/doctrine/orm/issues/8459'] ?? 0;
 
-        $this->expectDeprecationMessageSame('Method Doctrine\ORM\EntityManager::merge() is deprecated and will be removed in Doctrine ORM 3.0.');
-        $this->_em->merge($entity);
-    }
-
-    public function testDeprecatedDetach(): void
-    {
-        $entity = new Country(456, 'United Kingdom');
-        $this->_em->persist($entity);
-
-        $this->expectDeprecationMessageSame('Method Doctrine\ORM\EntityManager::detach() is deprecated and will be removed in Doctrine ORM 3.0.');
-        $this->_em->detach($entity);
-    }
-
-    public function testDeprecatedCopy(): void
-    {
-        $entity = new Country(456, 'United Kingdom');
-        $this->_em->persist($entity);
-
-        try {
-            $this->expectDeprecationMessageSame('Method Doctrine\ORM\EntityManager::copy() is deprecated and will be removed in Doctrine ORM 3.0.');
-            $this->_em->copy($entity);
-        } catch (BadMethodCallException $e) {
-            // do nothing
-        }
+        $this->assertSame($before + 1, $after);
     }
 }
