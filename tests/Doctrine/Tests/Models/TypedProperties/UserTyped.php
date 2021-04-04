0246ac23f2111b5cd08f3f4f5c6d8b1bfbefa540
Jakub Caban <kuba.iluvatar@gmail.com>
2021-04-04 13:10:47 +0200

    Add tests for mapped typed properties (type and nullable)

--- tests/Doctrine/Tests/Models/TypedProperties/UserTyped.php
+++ tests/Doctrine/Tests/Models/TypedProperties/UserTyped.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Doctrine\Tests\Models\TypedProperties;
 
+use Doctrine\ORM\Mapping as ORM;
+
 use DateInterval;
 use DateTime;
 use DateTimeImmutable;
@@ -13,40 +15,51 @@ use Doctrine\Tests\Models\CMS\CmsEmail;
  * @Entity
  * @Table(name="cms_users_typed")
  */
+#[ORM\Entity, ORM\Table(name: "cms_users_typed")]
 class UserTyped
 {
     /**
      * @Id @Column
      * @GeneratedValue
      */
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     public int $id;
     /** @Column(length=50) */
+    #[ORM\Column(length: 50)]
     public ?string $status;
 
     /** @Column(length=255, unique=true) */
+    #[ORM\Column(length: 255, unique: true)]
     public string $username;
 
     /** @Column */
+    #[ORM\Column]
     public DateInterval $dateInterval;
 
     /** @Column */
+    #[ORM\Column]
     public DateTime $dateTime;
 
     /** @Column */
+    #[ORM\Column]
     public DateTimeImmutable $dateTimeImmutable;
 
     /** @Column */
+    #[ORM\Column]
     public array $array;
 
     /** @Column */
+    #[ORM\Column]
     public bool $boolean;
 
     /** @Column */
+    #[ORM\Column]
     public float $float;
 
     /**
      * @OneToOne(cascade={"persist"}, orphanRemoval=true)
      * @JoinColumn
      */
+    #[ORM\OneToOne(cascade: ["persist"], orphanRemoval: true), ORM\JoinColumn]
     public CmsEmail $email;
 }
