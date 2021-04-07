da5877d60c6829089359da9ab94e966111cade18
acoulton <andrew@ingenerator.com>
2021-04-07 12:02:00 +0100

    Only polyfill older phpunit methods when required

--- tests/Doctrine/Tests/DoctrineTestCase.php
+++ tests/Doctrine/Tests/DoctrineTestCase.php
@@ -4,52 +4,9 @@ declare(strict_types=1);
 
 namespace Doctrine\Tests;
 
-use PHPUnit\Framework\TestCase;
-use PHPUnit\Runner\Version;
-
 /**
  * Base testcase class for all Doctrine testcases.
  */
-abstract class DoctrineTestCase extends TestCase
+abstract class DoctrineTestCase extends PolyfillableTestCase
 {
-    public static function assertDoesNotMatchRegularExpression(
-        string $pattern,
-        string $string,
-        string $message = ''
-    ): void {
-        // Forward-compatibility wrapper for phpunit 9 : can be removed once phpunit 8 / php 7.2 support is dropped.
-        if (self::isPhpunit9()) {
-            parent::assertDoesNotMatchRegularExpression($pattern, $string, $message);
-        } else {
-            self::assertNotRegExp($pattern, $string, $message);
-        }
-    }
-
-    public static function assertMatchesRegularExpression(string $pattern, string $string, string $message = ''): void
-    {
-        // Forward-compatibility wrapper for phpunit 9 : can be removed once phpunit 8 / php 7.2 support is dropped.
-        if (self::isPhpunit9()) {
-            parent::assertMatchesRegularExpression($pattern, $string, $message);
-        } else {
-            self::assertRegExp($pattern, $string, $message);
-        }
-    }
-
-    public static function assertFileDoesNotExist(string $filename, string $message = ''): void
-    {
-        // Forward-compatibility wrapper for phpunit 9 : can be removed once phpunit 8 / php 7.2 support is dropped.
-        if (self::isPhpunit9()) {
-            parent::assertFileDoesNotExist($filename, $message);
-        } else {
-            self::assertFileNotExists($filename, $message);
-        }
-    }
-
-    /**
-     * Check if we're running in phpunit >= 9.0
-     */
-    private static function isPhpunit9(): bool
-    {
-        return (int) Version::series() >= 9;
-    }
 }
