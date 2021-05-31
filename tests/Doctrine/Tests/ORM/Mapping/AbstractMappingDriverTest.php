d9e59d68626ed7acc15b90935213dc60f1cf7f41
Benjamin Eberlei <kontakt@beberlei.de>
2021-05-31 10:19:16 +0200

    [GH-8723] Remove use of nullability to automatically detect nullable status (#8732)
    * [GH-8723] Remove use of nullability to automatically detect nullable status.
    
    * [GH-8723] Make Column::$nullable default to false again, fix tests.

--- tests/Doctrine/Tests/ORM/Mapping/AbstractMappingDriverTest.php
+++ tests/Doctrine/Tests/ORM/Mapping/AbstractMappingDriverTest.php
@@ -265,24 +265,6 @@ abstract class AbstractMappingDriverTest extends OrmTestCase
         return $class;
     }
 
-    public function testFieldIsNullableByType(): void
-    {
-        if (PHP_VERSION_ID < 70400) {
-            $this->markTestSkipped('requies PHP 7.4');
-        }
-
-        $class = $this->createClassMetadata(UserTyped::class);
-
-        // Explicit Nullable
-        $this->assertTrue($class->isNullable('status'));
-
-        // Explicit Not Nullable
-        $this->assertFalse($class->isNullable('username'));
-
-        $this->assertEquals(CmsEmail::class, $class->getAssociationMapping('email')['targetEntity']);
-        $this->assertEquals(CmsEmail::class, $class->getAssociationMapping('mainEmail')['targetEntity']);
-    }
-
     public function testFieldTypeFromReflection(): void
     {
         if (PHP_VERSION_ID < 70400) {
@@ -299,6 +281,9 @@ abstract class AbstractMappingDriverTest extends OrmTestCase
         $this->assertEquals('json', $class->getTypeOfField('array'));
         $this->assertEquals('boolean', $class->getTypeOfField('boolean'));
         $this->assertEquals('float', $class->getTypeOfField('float'));
+
+        $this->assertEquals(CmsEmail::class, $class->getAssociationMapping('email')['targetEntity']);
+        $this->assertEquals(CmsEmail::class, $class->getAssociationMapping('mainEmail')['targetEntity']);
     }
 
     /**
