6c2d0a79df8ba22bd4b071cdb2ff09d02af70769
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-09-22 09:17:32 +0100

    Fix formatting based on PR feedback

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH9027Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH9027Test.php
@@ -7,7 +7,6 @@ namespace Doctrine\Tests\ORM\Functional\Ticket;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\UnitOfWork;
 use Doctrine\Tests\OrmFunctionalTestCase;
-use Exception;
 
 class GH9027Test extends OrmFunctionalTestCase
 {
@@ -15,15 +14,20 @@ class GH9027Test extends OrmFunctionalTestCase
     {
         parent::setUp();
 
-        try {
-            $this->_schemaTool->createSchema(
-                [
-                    $this->_em->getClassMetadata(GH9027Customer::class),
-                    $this->_em->getClassMetadata(GH9027Cart::class),
-                ]
-            );
-        } catch (Exception $e) {
-        }
+        $this->_schemaTool->createSchema([
+            $this->_em->getClassMetadata(GH9027Cart::class),
+            $this->_em->getClassMetadata(GH9027Customer::class),
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->_schemaTool->dropSchema([
+            $this->_em->getClassMetadata(GH9027Cart::class),
+            $this->_em->getClassMetadata(GH9027Customer::class),
+        ]);
+
+        parent::tearDown();
     }
 
     /**
@@ -31,9 +35,14 @@ class GH9027Test extends OrmFunctionalTestCase
      */
     public function testUnitOfWorkHandlesNullRelations(): void
     {
-        $uow = new UnitOfWork($this->_em);
+        $uow   = new UnitOfWork($this->_em);
         $hints = ['fetchMode' => [GH9027Cart::class => ['customer' => ClassMetadata::FETCH_EAGER]]];
-        $cart = $uow->createEntity(GH9027Cart::class, ['id' => 1, 'customer' => 24252], $hints);
+
+        $cart = $uow->createEntity(
+            GH9027Cart::class,
+            ['id' => 1, 'customer' => 24252],
+            $hints
+        );
 
         $this->assertEquals(null, $cart->customer);
     }
