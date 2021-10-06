483e09cf1cd1173b7a661b6ede817b440fd88e6f
Antonio J. García Lagar <ajgarcia@c17.net>
2021-10-06 08:35:56 +0200

    Fix Query::toIterable to honor INDEX BY construct

--- tests/Doctrine/Tests/ORM/Functional/QueryIterableTest.php
+++ tests/Doctrine/Tests/ORM/Functional/QueryIterableTest.php
@@ -6,6 +6,7 @@ namespace Doctrine\Tests\ORM\Functional;
 
 use Doctrine\Tests\IterableTester;
 use Doctrine\Tests\Models\CMS\CmsAddress;
+use Doctrine\Tests\Models\CMS\CmsArticle;
 use Doctrine\Tests\Models\CMS\CmsUser;
 use Doctrine\Tests\OrmFunctionalTestCase;
 
@@ -66,6 +67,50 @@ final class QueryIterableTest extends OrmFunctionalTestCase
 
         $this->_em->clear();
 
+        IterableTester::assertResultsAreTheSame($query);
+    }
+
+    public function testIndexByQueryWithOneResult(): void
+    {
+        $user           = new CmsUser();
+        $user->name     = 'Antonio J.';
+        $user->username = 'ajgarlag';
+        $user->status   = 'developer';
+
+        $this->_em->persist($user);
+        $this->_em->flush();
+
+        $query = $this->_em->createQuery('SELECT u FROM ' . CmsUser::class . ' u INDEX BY u.username');
+        IterableTester::assertResultsAreTheSame($query);
+    }
+
+    public function testIndexByQueryWithMultipleResults(): void
+    {
+        $article1        = new CmsArticle();
+        $article1->topic = 'Doctrine 2';
+        $article1->text  = 'This is an introduction to Doctrine 2.';
+
+        $article2        = new CmsArticle();
+        $article2->topic = 'Symfony 2';
+        $article2->text  = 'This is an introduction to Symfony 2.';
+
+        $article3        = new CmsArticle();
+        $article3->topic = 'Laminas';
+        $article3->text  = 'This is an introduction to Laminas.';
+
+        $article4        = new CmsArticle();
+        $article4->topic = 'CodeIgniter';
+        $article4->text  = 'This is an introduction to CodeIgniter.';
+
+        $this->_em->persist($article1);
+        $this->_em->persist($article2);
+        $this->_em->persist($article3);
+        $this->_em->persist($article4);
+
+        $this->_em->flush();
+        $this->_em->clear();
+
+        $query = $this->_em->createQuery('select a from ' . CmsArticle::class . ' a INDEX BY a.topic');
         IterableTester::assertResultsAreTheSame($query);
     }
 }
