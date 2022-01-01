ab1f114e141cf8550682d5be813cdc54256fbb45
Benjamin Eberlei <kontakt@beberlei.de>
2022-01-01 17:31:43 +0100

    Remove deprecated PHPDriver mapping driver, use StaticPHPDriver instead.

--- tests/Doctrine/Tests/Models/Cache/City.php
+++ tests/Doctrine/Tests/Models/Cache/City.php
@@ -138,6 +138,69 @@ class City
 
     public static function loadMetadata(ClassMetadataInfo $metadata): void
     {
-        include __DIR__ . '/../../ORM/Mapping/php/Doctrine.Tests.Models.Cache.City.php';
+        $metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_NONE);
+        $metadata->setPrimaryTable(['name' => 'cache_city']);
+        $metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_IDENTITY);
+        $metadata->setChangeTrackingPolicy(ClassMetadataInfo::CHANGETRACKING_DEFERRED_IMPLICIT);
+
+        $metadata->enableCache(
+            [
+                'usage' => ClassMetadataInfo::CACHE_USAGE_READ_ONLY,
+            ]
+        );
+
+        $metadata->mapField(
+            [
+                'fieldName' => 'id',
+                'type' => 'integer',
+                'id' => true,
+            ]
+        );
+
+        $metadata->mapField(
+            [
+                'fieldName' => 'name',
+                'type' => 'string',
+            ]
+        );
+
+
+        $metadata->mapOneToOne(
+            [
+                'fieldName'      => 'state',
+                'targetEntity'   => State::class,
+                'inversedBy'     => 'cities',
+                'joinColumns'    =>
+                    [
+                        [
+                            'name' => 'state_id',
+                            'referencedColumnName' => 'id',
+                        ],
+                    ],
+            ]
+        );
+        $metadata->enableAssociationCache('state', [
+            'usage' => ClassMetadataInfo::CACHE_USAGE_READ_ONLY,
+        ]);
+
+        $metadata->mapManyToMany(
+            [
+                'fieldName' => 'travels',
+                'targetEntity' => Travel::class,
+                'mappedBy' => 'visitedCities',
+            ]
+        );
+
+        $metadata->mapOneToMany(
+            [
+                'fieldName' => 'attractions',
+                'targetEntity' => Attraction::class,
+                'mappedBy' => 'city',
+                'orderBy' => ['name' => 'ASC'],
+            ]
+        );
+        $metadata->enableAssociationCache('attractions', [
+            'usage' => ClassMetadataInfo::CACHE_USAGE_READ_ONLY,
+        ]);
     }
 }
