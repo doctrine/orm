5adf89bf1cb039da969fc53f6b4d47978bdca2ca
Ruben Rögels <ruben.roegels@jumping-frog.org>
2022-01-13 19:06:59 +0100

    Fixed persisting versioned entities using class table inheritance (#9378)
    Resolved logic error:
    Moved re-fetching entity data after persisting (inserting)
    all involved entities.
    
    Fixes #9378

--- lib/Doctrine/ORM/Persisters/Entity/JoinedSubclassPersister.php
+++ lib/Doctrine/ORM/Persisters/Entity/JoinedSubclassPersister.php
@@ -169,10 +169,6 @@ class JoinedSubclassPersister extends AbstractEntityInheritancePersister
                 $id = $this->em->getUnitOfWork()->getEntityIdentifier($entity);
             }
 
-            if ($this->class->requiresFetchAfterChange) {
-                $this->assignDefaultVersionAndUpsertableValues($entity, $id);
-            }
-
             // Execute inserts on subtables.
             // The order doesn't matter because all child tables link to the root table via FK.
             foreach ($subTableStmts as $tableName => $stmt) {
@@ -192,6 +188,10 @@ class JoinedSubclassPersister extends AbstractEntityInheritancePersister
                 }
 
                 $stmt->executeStatement();
+            }
+
+            if ($this->class->requiresFetchAfterChange) {
+                $this->assignDefaultVersionAndUpsertableValues($entity, $id);
             }
         }
 
