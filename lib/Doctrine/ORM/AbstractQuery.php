599832cb8144fcf0f10752cc422cf4a42d53f52e
David Maicher <mail@dmaicher.de>
2022-02-12 21:49:30 +0100

    Fix AbstractQuery::setParameter phpdoc (#9504)
    * Fix AbstractQuery::setParameter phpdoc
    
    * Fix AbstractQuery::setParameter phpdoc

--- lib/Doctrine/ORM/AbstractQuery.php
+++ lib/Doctrine/ORM/AbstractQuery.php
@@ -371,11 +371,11 @@ abstract class AbstractQuery
     /**
      * Sets a query parameter.
      *
-     * @param string|int  $key   The parameter position or name.
-     * @param mixed       $value The parameter value.
-     * @param string|null $type  The parameter type. If specified, the given value will be run through
-     *                           the type conversion of this type. This is usually not needed for
-     *                           strings and numeric types.
+     * @param string|int      $key   The parameter position or name.
+     * @param mixed           $value The parameter value.
+     * @param string|int|null $type  The parameter type. If specified, the given value will be run through
+     *                               the type conversion of this type. This is usually not needed for
+     *                               strings and numeric types.
      *
      * @return $this
      */
