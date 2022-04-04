f655ec73408ccf37a5ce576c0bba37348c69000c
Tomas <michnovka@gmail.com>
2022-04-04 22:27:23 +0200

    minor PHPCS fix

--- lib/Doctrine/ORM/Utility/IdentifierFlattener.php
+++ lib/Doctrine/ORM/Utility/IdentifierFlattener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\ORM\Utility;
 
+use BackedEnum;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\UnitOfWork;
 use Doctrine\Persistence\Mapping\ClassMetadataFactory;
@@ -76,7 +77,7 @@ final class IdentifierFlattener
 
                 $flatId[$field] = implode(' ', $associatedId);
             } else {
-                if ($id[$field] instanceof \BackedEnum) {
+                if ($id[$field] instanceof BackedEnum) {
                     $flatId[$field] = $id[$field]->value;
                 } else {
                     $flatId[$field] = $id[$field];
