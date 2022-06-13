c16a7964362e5ccdb116d7fe11dbb355dbf1e833
Alexander M. Turek <me@derrabus.de>
2022-06-13 15:55:20 +0200

    Flip conditional extension of legacy AnnotationDriver class

--- lib/Doctrine/ORM/Mapping/Driver/CompatibilityAnnotationDriver.php
+++ lib/Doctrine/ORM/Mapping/Driver/CompatibilityAnnotationDriver.php
@@ -9,18 +9,18 @@ use Doctrine\Persistence\Mapping\Driver\MappingDriver;
 
 use function class_exists;
 
-if (class_exists(PersistenceAnnotationDriver::class)) {
+if (! class_exists(PersistenceAnnotationDriver::class)) {
     /**
      * @internal This class will be removed in ORM 3.0.
      */
-    abstract class CompatibilityAnnotationDriver extends PersistenceAnnotationDriver
+    abstract class CompatibilityAnnotationDriver implements MappingDriver
     {
     }
 } else {
     /**
      * @internal This class will be removed in ORM 3.0.
      */
-    abstract class CompatibilityAnnotationDriver implements MappingDriver
+    abstract class CompatibilityAnnotationDriver extends PersistenceAnnotationDriver
     {
     }
 }
