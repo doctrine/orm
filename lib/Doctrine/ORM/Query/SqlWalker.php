33f4db840548ae86043090212ade7f737226effc
Romain Canon <romain.hydrocanon@gmail.com>
2022-08-04 00:33:46 +0200

    Support native enum hydration when using `NEW` operator (#9936)
    Using the `NEW` operator with the query builder now properly converts
    scalar values to native enums inside data transfer objects.

--- lib/Doctrine/ORM/Query/SqlWalker.php
+++ lib/Doctrine/ORM/Query/SqlWalker.php
@@ -1682,6 +1682,11 @@ class SqlWalker implements TreeWalker
                     }
 
                     $sqlSelectExpressions[] = $col . ' AS ' . $columnAlias;
+
+                    if (! empty($fieldMapping['enumType'])) {
+                        $this->rsm->addEnumResult($columnAlias, $fieldMapping['enumType']);
+                    }
+
                     break;
 
                 case $e instanceof AST\Literal:
