3ef002267b87ec222a740097601d5157a61c7f43
s-bernstein <bernstein@redlink.de>
2022-08-09 14:04:45 +0200

    Added a functional test for preUpdate event on collection clear

---
+++ tests/Doctrine/Tests/ORM/Functional/PreUpdateEventTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @author   : Sören Bernstein
+ * @copyright:  Copyright (c) 2022, Redlink GmbH
+ * @license  : proprietary
+ */
+
+namespace Doctrine\Tests\ORM\Functional;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Doctrine\Tests\Models\CMS\CmsGroup;
+use Doctrine\Tests\Models\CMS\CmsUser;
+use Doctrine\Tests\OrmFunctionalTestCase;
+
+class PreUpdateEventTest extends OrmFunctionalTestCase
+{
+    /** @var PreUpdateListener */
+    private $listener;
+
+    public function testListenerShouldBeNotifiedIfCollectionIsCleared(): void
+    {
+        $user = $this->createNewValidUser();
+        $this->_em->persist($user);
+        $this->_em->flush();
+        self::assertFalse($this->listener->wasNotified);
+
+        $user->getGroups()->clear();
+        $this->_em->flush();
+        self::assertTrue($this->listener->wasNotified);
+    }
+
+    protected function setUp(): void
+    {
+        $this->useModelSet('cms');
+        parent::setUp();
+        $this->listener = new PreUpdateListener();
+        $evm = $this->_em->getEventManager();
+        $evm->addEventListener(Events::preUpdate, $this->listener);
+    }
+
+    private function createNewValidUser(): CmsUser
+    {
+        $user = new CmsUser();
+        $user->username = 'dfreudenberger';
+        $user->name = 'Daniel Freudenberger';
+
+        /** @var CmsGroup $group */
+        $group = new CmsGroup();
+        $group->addUser($user);
+        $group->setName('test group');
+
+        $user->addGroup($group);
+
+        return $user;
+    }
+}
+
+class PreUpdateListener
+{
+    /** @var bool */
+    public $wasNotified = false;
+
+    /** @var LifecycleEventArgs */
+    public $receivedArgs;
+
+    public function preUpdate(LifecycleEventArgs $args): void
+    {
+        $this->wasNotified = true;
+        $this->receivedArgs = $args;
+    }
+}
