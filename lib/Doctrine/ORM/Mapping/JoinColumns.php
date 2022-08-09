36c70c0cc5426feb7c21a3e1a051ca5d24229c88
Arnaud Durand <arnaud.durand@live.com>
2022-08-09 17:56:12 +0200

    Add attribute support to JoinColumns

--- lib/Doctrine/ORM/Mapping/JoinColumns.php
+++ lib/Doctrine/ORM/Mapping/JoinColumns.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace Doctrine\ORM\Mapping;
 
+use Attribute;
+
 /**
  * @Annotation
  * @Target("PROPERTY")
  */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 final class JoinColumns implements Annotation
 {
     /** @var array<\Doctrine\ORM\Mapping\JoinColumn> */
     public $value;
+
+    public function __construct(array $value)
+    {
+        $this->value = $value;
+    }
 }
