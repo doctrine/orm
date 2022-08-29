b4f857e658b8d9512dd12c7a15f181bdec197c6b
JohJohan <johan.vlaar.1994@gmail.com>
2022-08-29 09:40:41 +0200

    8294 Improve code style

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH8294Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH8294Test.php
@@ -1,18 +1,22 @@
 <?php
 
+use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\Tests\Models\GH8294\GH8294Entity;
+use Doctrine\Tests\OrmFunctionalTestCase;
+
 /**
  * @group GH-8294
  */
-class GH8294Test extends \Doctrine\Tests\OrmFunctionalTestCase
+class GH8294Test extends OrmFunctionalTestCase
 {
     /**
      * @group GH8294
      */
     public function testPersitingEntityWithInvalidTableName()
     {
-        $entity = new \Doctrine\Tests\Models\GH8294\GH8294Entity();
+        $entity = new GH8294Entity();
 
-        $this->expectException(Doctrine\ORM\Mapping\MappingException::class);
+        $this->expectException(MappingException::class);
         $this->expectExceptionMessage("Invalid table name for class 'Doctrine\Tests\Models\GH8294\GH8294Entity' expected 'string' got 'array'.");
         $this->_em->persist($entity);
     }
