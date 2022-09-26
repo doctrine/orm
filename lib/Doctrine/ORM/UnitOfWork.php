b42f94957bb89fb215b12e19588b0059537d4014
Asmir Mustafic <goetas@gmail.com>
2022-09-26 13:16:38 +0200

    attempt a fix

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2219,12 +2219,12 @@ class UnitOfWork implements PropertyChangedListener
             throw ORMInvalidArgumentException::entityNotManaged($entity);
         }
 
+        $this->cascadeRefresh($entity, $visited);
+
         $this->getEntityPersister($class->name)->refresh(
             array_combine($class->getIdentifierFieldNames(), $this->entityIdentifiers[$oid]),
             $entity
         );
-
-        $this->cascadeRefresh($entity, $visited);
     }
 
     /**
