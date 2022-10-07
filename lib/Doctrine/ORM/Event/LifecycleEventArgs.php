84213b9f05c90161f7d82e40c32bbf39a06e2258
David Romaní <david@flux.cat>
2022-10-07 18:03:42 +0200

    fix deprecated trigger help comment

--- lib/Doctrine/ORM/Event/LifecycleEventArgs.php
+++ lib/Doctrine/ORM/Event/LifecycleEventArgs.php
@@ -42,7 +42,7 @@ class LifecycleEventArgs extends BaseLifecycleEventArgs
         Deprecation::trigger(
             'doctrine/orm',
             'https://github.com/doctrine/orm/issues/9875',
-            'Method %s() is deprecated and will be removed in Doctrine ORM 3.0. Use getObjectManager() instead.',
+            'Method %s() is deprecated and will be removed in Doctrine ORM 3.0. Use getObject() instead.',
             __METHOD__
         );
 
