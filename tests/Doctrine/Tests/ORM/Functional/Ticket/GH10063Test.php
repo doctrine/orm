6ef3b2b63b76ee0de2b28ac52049f42ffbcecc8a
Thomas Landauer <thomas@landauer.at>
2022-10-08 15:57:55 +0200

    Moving entity and enum out of the test file

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH10063Test.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH10063Test.php
@@ -4,15 +4,8 @@ declare(strict_types=1);
 
 namespace Doctrine\Tests\ORM\Functional\Ticket;
 
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
 use Doctrine\Tests\OrmFunctionalTestCase;
 
-/**
- * @requires PHP 8.1
- */
 final class GH10063Test extends OrmFunctionalTestCase
 {
     protected function setUp(): void
@@ -22,6 +15,9 @@ final class GH10063Test extends OrmFunctionalTestCase
         $this->createSchemaForModels(GH10063Entity::class);
     }
 
+    /**
+     * @requires PHP 8.1
+     */
     public function testArrayOfEnums(): void
     {
         $entity = (new GH10063Entity())->setColors([GH10063Enum::Red, GH10063Enum::Green]);
@@ -34,49 +30,4 @@ final class GH10063Test extends OrmFunctionalTestCase
         assert($entity instanceof GH10063Entity);
         self::assertEquals([GH10063Enum::Red, GH10063Enum::Green], $entity->getColors());
     }
-}
-
-/**
- * @Entity
- * @requires PHP 8.1
- */
-class GH10063Entity
-{
-    /**
-     * @Column
-     * @Id
-     * @GeneratedValue
-     */
-    public int $id;
-
-    /**
-     * @Column(type="simple_array", length=255, nullable=true, enumType=GH10063Enum::class)
-     */
-    private array $colors = [];
-
-    /**
-     * @param array<int, GH10063Enum> $colors
-     */
-    public function setColors(array $colors): self
-    {
-        $this->colors = $colors;
-        return $this;
-    }
-    /**
-     * @return array<int, GH10063Enum>
-     */
-    public function getColors(): array
-    {
-        return $this->colors;
-    }
-}
-
-/**
- * @requires PHP 8.1
- */
-enum GH10063Enum: string
-{
-    case Red = 'red';
-    case Green = 'green';
-    case Blue = 'blue';
 }
