bfefcf0b61a024b52afccc04622bfc367d1dee63
Thomas Landauer <thomas@landauer.at>
2022-10-08 15:59:11 +0200

    Create GH10063Entity.php

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH10063Entity.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+/** @Entity */
+class GH10063Entity
+{
+    /**
+     * @Column
+     * @Id
+     * @GeneratedValue
+     */
+    public int $id;
+
+    /**
+     * @Column(type="simple_array", length=255, nullable=true, enumType=GH10063Enum::class)
+     */
+    private array $colors = [];
+
+    /**
+     * @param array<int, GH10063Enum> $colors
+     */
+    public function setColors(array $colors): self
+    {
+        $this->colors = $colors;
+        return $this;
+    }
+    /**
+     * @return array<int, GH10063Enum>
+     */
+    public function getColors(): array
+    {
+        return $this->colors;
+    }
+}
