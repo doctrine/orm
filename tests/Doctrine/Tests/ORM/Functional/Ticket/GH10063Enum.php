a2ca4f2ffc2e4dcc34318afdc0bcf11fc414493d
Thomas Landauer <thomas@landauer.at>
2022-10-08 17:01:56 +0200

    Update GH10063Enum.php

--- tests/Doctrine/Tests/ORM/Functional/Ticket/GH10063Enum.php
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH10063Enum.php
@@ -6,7 +6,7 @@ namespace Doctrine\Tests\ORM\Functional\Ticket;
 
 enum GH10063Enum: string
 {
-    case Red = 'red';
+    case Red   = 'red';
     case Green = 'green';
-    case Blue = 'blue';
+    case Blue  = 'blue';
 }
