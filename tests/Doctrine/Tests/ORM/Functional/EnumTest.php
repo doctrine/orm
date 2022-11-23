c3cdf48e2b7363a37388a17ecca0598e520ae0e4
Warxcell <warxcell@gmail.com>
2022-11-23 15:13:32 +0200

    Add testcase

--- tests/Doctrine/Tests/ORM/Functional/EnumTest.php
+++ tests/Doctrine/Tests/ORM/Functional/EnumTest.php
@@ -436,7 +436,7 @@ EXCEPTION
         $table  = $this->_em->getClassMetadata(CardReadOnlyCache::class)->getTableName();
         $cardId = uniqid('', true);
 
-        $this->_em->getConnection()->insert($table, ['id' => $cardId, 'suit' => Suit::Hearts->value]);
+        $this->_em->getConnection()->insert($table, ['id' => $cardId, 'suit' => 'H']);
 
         $tableAssoc = $this->_em->getClassMetadata(AssocToCardReadOnlyCache::class)->getTableName();
         $assocId    = uniqid('', true);
