f7708e7f5e2a4fcd811041ee157bebc75d85772a
Jordan Schnur <actjay1898@gmail.com>
2022-12-06 14:39:34 -0500

    Added MultiManagerProvider

---
+++ lib/Doctrine/ORM/Tools/Console/EntityManagerProvider/MultiManagerProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ORM\Tools\Console\EntityManagerProvider;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider;
+
+final class MultiManagerProvider implements EntityManagerProvider
+{
+    /** @var EntityManagerInterface[] */
+    private $entityManagers;
+
+    /** @var string */
+    private $defaultManagerName;
+
+    public function __construct(array $entityManagers, string $defaultManagerName = 'default')
+    {
+        $this->entityManagers     = $entityManagers;
+        $this->defaultManagerName = $defaultManagerName;
+    }
+
+    public function getDefaultManager(): EntityManagerInterface
+    {
+        return $this->entityManagers[$this->defaultManagerName];
+    }
+
+    public function getManager(string $name): EntityManagerInterface
+    {
+        if (!isset($this->entityManagers[$name])) {
+            throw UnknownManagerException::unknownManager($name, $this->entityManagers);
+        }
+
+        return $this->entityManagers[$this->defaultManagerName];
+    }
+}
