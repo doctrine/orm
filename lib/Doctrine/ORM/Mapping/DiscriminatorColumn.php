b3f9acd1bf3663f6d4684dc130b6f94eac510c65
michnovka <michnovka@gmail.com>
2022-12-15 11:50:18 +0100

    Clean-up code after PR10288 merge (#10305)

--- lib/Doctrine/ORM/Mapping/DiscriminatorColumn.php
+++ lib/Doctrine/ORM/Mapping/DiscriminatorColumn.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Doctrine\ORM\Mapping;
 
 use Attribute;
+use BackedEnum;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 final class DiscriminatorColumn implements MappingAttribute
@@ -14,6 +15,7 @@ final class DiscriminatorColumn implements MappingAttribute
         public readonly string|null $type = null,
         public readonly int|null $length = null,
         public readonly string|null $columnDefinition = null,
+        /** @var class-string<BackedEnum>|null */
         public readonly string|null $enumType = null,
     ) {
     }
