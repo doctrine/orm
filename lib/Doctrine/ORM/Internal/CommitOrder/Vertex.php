8f360f08c5588423f4fd9e0ebe471922989a34d8
Alexander M. Turek <me@derrabus.de>
2022-12-20 07:28:33 +0100

    Migrate Doctrine\ORM\Internal\CommitOrder to PHP 8.1 (#10323)

--- lib/Doctrine/ORM/Internal/CommitOrder/Vertex.php
+++ lib/Doctrine/ORM/Internal/CommitOrder/Vertex.php
@@ -9,30 +9,14 @@ use Doctrine\ORM\Mapping\ClassMetadata;
 /** @internal */
 final class Vertex
 {
-    /**
-     * @var string
-     * @readonly
-     */
-    public $hash;
-
-    /**
-     * @var int
-     * @psalm-var VertexState::*
-     */
-    public $state = VertexState::NOT_VISITED;
-
-    /**
-     * @var ClassMetadata
-     * @readonly
-     */
-    public $value;
+    public VertexState $state = VertexState::NotVisited;
 
     /** @var array<string, Edge> */
-    public $dependencyList = [];
+    public array $dependencyList = [];
 
-    public function __construct(string $hash, ClassMetadata $value)
-    {
-        $this->hash  = $hash;
-        $this->value = $value;
+    public function __construct(
+        public readonly string $hash,
+        public readonly ClassMetadata $value,
+    ) {
     }
 }
--- lib/Doctrine/ORM/Internal/CommitOrder/VertexState.php
+++ lib/Doctrine/ORM/Internal/CommitOrder/VertexState.php
@@ -5,13 +5,9 @@ declare(strict_types=1);
 namespace Doctrine\ORM\Internal\CommitOrder;
 
 /** @internal */
-final class VertexState
+enum VertexState
 {
-    public const NOT_VISITED = 0;
-    public const IN_PROGRESS = 1;
-    public const VISITED     = 2;
-
-    private function __construct()
-    {
-    }
+    case NotVisited;
+    case InProgress;
+    case Visited;
 }
