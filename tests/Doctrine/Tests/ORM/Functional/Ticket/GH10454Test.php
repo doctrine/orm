8fb83bfc4e85e5d638acf544c3b8e8c31f373946
Matthias Pigulla <mp@webfactory.de>
2023-01-24 21:17:55 +0000

    Failing test: Re-configured protected property not rejected

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/GH10454Test.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\Tests\OrmTestCase;
+use Generator;
+
+class GH10454Test extends OrmTestCase
+{
+    /**
+     * @param class-string $className
+     *
+     * @dataProvider classesThatOverrideFieldNames
+     */
+    public function testProtectedPropertyMustNotBeInheritedAndReconfigured(string $className): void
+    {
+        $em = $this->getTestEntityManager();
+
+        $this->expectException(MappingException::class);
+        $this->expectExceptionMessageMatches('/Property "field" .* was already declared, but it must be declared only once/');
+
+        $em->getClassMetadata($className);
+    }
+
+    public function classesThatOverrideFieldNames(): Generator
+    {
+        yield 'Entity class that redeclares a protected field inherited from a base entity' => [GH10454EntityChildProtected::class];
+        yield 'Entity class that redeclares a protected field inherited from a mapped superclass' => [GH10454MappedSuperclassChildProtected::class];
+    }
+
+    /**
+     * Override for BC with PHPUnit <8
+     */
+    public function expectExceptionMessageMatches(string $regularExpression): void
+    {
+        if (method_exists(get_parent_class($this), 'expectExceptionMessageMatches')) {
+            parent::expectExceptionMessageMatches($regularExpression);
+        } else {
+            parent::expectExceptionMessageRegExp($regularExpression);
+        }
+    }
+}
+
+/**
+ * @ORM\Entity
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorMap({ "base": "GH10454BaseEntityProtected", "child": "GH10454EntityChildProtected" })
+ * @ORM\DiscriminatorColumn(name="type")
+ */
+class GH10454BaseEntityProtected
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="text", name="base")
+     *
+     * @var string
+     */
+    protected $field;
+}
+
+/**
+ * @ORM\Entity
+ */
+class GH10454EntityChildProtected extends GH10454BaseEntityProtected
+{
+    /**
+     * @ORM\Column(type="text", name="child")
+     *
+     * @var string
+     */
+    protected $field;
+}
+
+/**
+ * @ORM\MappedSuperclass
+ */
+class GH10454BaseMappedSuperclassProtected
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="text", name="base")
+     *
+     * @var string
+     */
+    protected $field;
+}
+
+/**
+ * @ORM\Entity
+ */
+class GH10454MappedSuperclassChildProtected extends GH10454BaseMappedSuperclassProtected
+{
+    /**
+     * @ORM\Column(type="text", name="child")
+     *
+     * @var string
+     */
+    protected $field;
+}
