doctrine/orm

commit 6b220e3c90c4cffca5ec0feff8990f569aa9b120

Fix return type of `getSingleScalarResult` (#10870)

lib/Doctrine/ORM/AbstractQuery.php
@@ -1010,9 +1010,8 @@ abstract class AbstractQuery
      *
      * Alias for getSingleResult(HYDRATE_SINGLE_SCALAR).
      *
-     * @return bool|float|int|string The scalar result.
+     * @return bool|float|int|string|null The scalar result.
      *
-     * @throws NoResultException        If the query returned no result.
      * @throws NonUniqueResultException If the query result is not unique.
      */
     public function getSingleScalarResult()

commit 3bf0bfc0be5f1f1d49cb627f4e7072c9788a8c63

Add back throws annotation to getSingleScalarResult
Fix regression introduced in #10870

`$result = $this->execute(null, $hydrationMode);` in `getSingleResult` can still throw NoResultException exception.

lib/Doctrine/ORM/AbstractQuery.php
@@ -1012,6 +1012,7 @@ abstract class AbstractQuery
      *
      * @return bool|float|int|string|null The scalar result.
      *
+     * @throws NoResultException        If the query returned no result.
      * @throws NonUniqueResultException If the query result is not unique.
      */
     public function getSingleScalarResult()