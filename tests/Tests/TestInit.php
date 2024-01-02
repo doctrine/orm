0a3fbac8af27751e548de3788aee0f057f6dcf71
Grégoire Paris <postmaster@greg0ire.fr>
2024-01-02 19:15:04 +0100

    Flatten directory tree
    It will make fuzzy matchers more efficient, and configuration files more
    readable.

--- tests/Tests/TestInit.php
+++ tests/Tests/TestInit.php
@@ -21,12 +21,12 @@ use const E_STRICT;
 error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('UTC');
 
-if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
     // dependencies were installed via composer - this is the main project
-    require __DIR__ . '/../../../vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../../../../../autoload.php')) {
+    require __DIR__ . '/../../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../../autoload.php')) {
     // installed as a dependency in `vendor`
-    require __DIR__ . '/../../../../../autoload.php';
+    require __DIR__ . '/../../../../autoload.php';
 } else {
     throw new Exception('Can\'t find autoload.php. Did you install dependencies via composer?');
 }
