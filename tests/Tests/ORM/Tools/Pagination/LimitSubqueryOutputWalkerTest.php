09f0966ad63f11f7f8c43c3e435cb369cc150164
Matthias Pigulla <mp@webfactory.de>
2024-01-31 22:34:35 +0100

    Fix CS after merge-up

--- tests/Tests/ORM/Tools/Pagination/LimitSubqueryOutputWalkerTest.php
+++ tests/Tests/ORM/Tools/Pagination/LimitSubqueryOutputWalkerTest.php
@@ -19,7 +19,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT m0_.id AS id_0, m0_.title AS title_1, c1_.id AS id_2, a2_.id AS id_3, a2_.name AS name_4, m0_.author_id AS author_id_5, m0_.category_id AS category_id_6 FROM MyBlogPost m0_ INNER JOIN Category c1_ ON m0_.category_id = c1_.id INNER JOIN Author a2_ ON m0_.author_id = a2_.id) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -31,7 +31,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0, MIN(sclr_5) AS dctrn_minrownum FROM (SELECT m0_.id AS id_0, m0_.title AS title_1, c1_.id AS id_2, a2_.id AS id_3, a2_.name AS name_4, ROW_NUMBER() OVER(ORDER BY m0_.title ASC) AS sclr_5, m0_.author_id AS author_id_6, m0_.category_id AS category_id_7 FROM MyBlogPost m0_ INNER JOIN Category c1_ ON m0_.category_id = c1_.id INNER JOIN Author a2_ ON m0_.author_id = a2_.id) dctrn_result GROUP BY id_0 ORDER BY dctrn_minrownum ASC LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -43,7 +43,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_1, MIN(sclr_3) AS dctrn_minrownum FROM (SELECT COUNT(g0_.id) AS sclr_0, u1_.id AS id_1, g0_.id AS id_2, ROW_NUMBER() OVER(ORDER BY COUNT(g0_.id) ASC) AS sclr_3 FROM User u1_ INNER JOIN user_group u2_ ON u1_.id = u2_.user_id INNER JOIN groups g0_ ON g0_.id = u2_.group_id) dctrn_result GROUP BY id_1 ORDER BY dctrn_minrownum ASC LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -55,7 +55,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_1, MIN(sclr_3) AS dctrn_minrownum FROM (SELECT COUNT(g0_.id) AS sclr_0, u1_.id AS id_1, g0_.id AS id_2, ROW_NUMBER() OVER(ORDER BY COUNT(g0_.id) ASC, u1_.id DESC) AS sclr_3 FROM User u1_ INNER JOIN user_group u2_ ON u1_.id = u2_.user_id INNER JOIN groups g0_ ON g0_.id = u2_.group_id) dctrn_result GROUP BY id_1 ORDER BY dctrn_minrownum ASC LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -67,7 +67,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_1, MIN(sclr_3) AS dctrn_minrownum FROM (SELECT COUNT(g0_.id) AS sclr_0, u1_.id AS id_1, g0_.id AS id_2, ROW_NUMBER() OVER(ORDER BY COUNT(g0_.id) ASC, u1_.id DESC) AS sclr_3 FROM User u1_ INNER JOIN user_group u2_ ON u1_.id = u2_.user_id INNER JOIN groups g0_ ON g0_.id = u2_.group_id) dctrn_result GROUP BY id_1 ORDER BY dctrn_minrownum ASC LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -86,7 +86,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT ID_0, MIN(SCLR_5) AS dctrn_minrownum FROM (SELECT m0_.id AS ID_0, m0_.title AS TITLE_1, c1_.id AS ID_2, a2_.id AS ID_3, a2_.name AS NAME_4, ROW_NUMBER() OVER(ORDER BY m0_.title ASC) AS SCLR_5, m0_.author_id AS AUTHOR_ID_6, m0_.category_id AS CATEGORY_ID_7 FROM MyBlogPost m0_ INNER JOIN Category c1_ ON m0_.category_id = c1_.id INNER JOIN Author a2_ ON m0_.author_id = a2_.id) dctrn_result GROUP BY ID_0 ORDER BY dctrn_minrownum ASC OFFSET 10 ROWS FETCH NEXT 20 ROWS ONLY',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -98,7 +98,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT ID_1, MIN(SCLR_3) AS dctrn_minrownum FROM (SELECT COUNT(g0_.id) AS SCLR_0, u1_.id AS ID_1, g0_.id AS ID_2, ROW_NUMBER() OVER(ORDER BY COUNT(g0_.id) ASC) AS SCLR_3 FROM User u1_ INNER JOIN user_group u2_ ON u1_.id = u2_.user_id INNER JOIN groups g0_ ON g0_.id = u2_.group_id) dctrn_result GROUP BY ID_1 ORDER BY dctrn_minrownum ASC OFFSET 10 ROWS FETCH NEXT 20 ROWS ONLY',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -110,7 +110,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT ID_1, MIN(SCLR_3) AS dctrn_minrownum FROM (SELECT COUNT(g0_.id) AS SCLR_0, u1_.id AS ID_1, g0_.id AS ID_2, ROW_NUMBER() OVER(ORDER BY COUNT(g0_.id) ASC, u1_.id DESC) AS SCLR_3 FROM User u1_ INNER JOIN user_group u2_ ON u1_.id = u2_.user_id INNER JOIN groups g0_ ON g0_.id = u2_.group_id) dctrn_result GROUP BY ID_1 ORDER BY dctrn_minrownum ASC OFFSET 10 ROWS FETCH NEXT 20 ROWS ONLY',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -122,7 +122,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT ID_0 FROM (SELECT m0_.id AS ID_0, m0_.title AS TITLE_1, c1_.id AS ID_2, a2_.id AS ID_3, a2_.name AS NAME_4, m0_.author_id AS AUTHOR_ID_5, m0_.category_id AS CATEGORY_ID_6 FROM MyBlogPost m0_ INNER JOIN Category c1_ ON m0_.category_id = c1_.id INNER JOIN Author a2_ ON m0_.author_id = a2_.id) dctrn_result OFFSET 10 ROWS FETCH NEXT 20 ROWS ONLY',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -132,7 +132,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT a0_.id AS id_0, a0_.name AS name_1, sum(a0_.name) AS sclr_2 FROM Author a0_) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -145,7 +145,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, (1 - 1000) * 1 FROM (SELECT a0_.id AS id_0, a0_.name AS name_1 FROM Author a0_) dctrn_result_inner ORDER BY (1 - 1000) * 1 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -157,7 +157,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, imageHeight_2 * imageWidth_3 FROM (SELECT a0_.id AS id_0, a0_.image AS image_1, a0_.imageHeight AS imageHeight_2, a0_.imageWidth AS imageWidth_3, a0_.imageAltDesc AS imageAltDesc_4, a0_.user_id AS user_id_5 FROM Avatar a0_) dctrn_result_inner ORDER BY imageHeight_2 * imageWidth_3 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -169,7 +169,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, imageHeight_3 * imageWidth_4 FROM (SELECT u0_.id AS id_0, a1_.id AS id_1, a1_.image AS image_2, a1_.imageHeight AS imageHeight_3, a1_.imageWidth AS imageWidth_4, a1_.imageAltDesc AS imageAltDesc_5, a1_.user_id AS user_id_6 FROM User u0_ INNER JOIN Avatar a1_ ON u0_.id = a1_.user_id) dctrn_result_inner ORDER BY imageHeight_3 * imageWidth_4 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -181,7 +181,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT ID_0, MIN(SCLR_5) AS dctrn_minrownum FROM (SELECT a0_.id AS ID_0, a0_.image AS IMAGE_1, a0_.imageHeight AS IMAGEHEIGHT_2, a0_.imageWidth AS IMAGEWIDTH_3, a0_.imageAltDesc AS IMAGEALTDESC_4, ROW_NUMBER() OVER(ORDER BY a0_.imageHeight * a0_.imageWidth DESC) AS SCLR_5, a0_.user_id AS USER_ID_6 FROM Avatar a0_) dctrn_result GROUP BY ID_0 ORDER BY dctrn_minrownum ASC OFFSET 10 ROWS FETCH NEXT 20 ROWS ONLY',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -189,23 +189,26 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
     public function testLimitSubqueryWithHiddenSelectionInOrderBy(): void
     {
         $query = $this->createQuery(
-            'SELECT a, a.name AS HIDDEN ord FROM Doctrine\Tests\ORM\Tools\Pagination\Author a ORDER BY ord DESC'
+            'SELECT a, a.name AS HIDDEN ord FROM Doctrine\Tests\ORM\Tools\Pagination\Author a ORDER BY ord DESC',
         );
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, name_2 FROM (SELECT a0_.id AS id_0, a0_.name AS name_1, a0_.name AS name_2 FROM Author a0_) dctrn_result_inner ORDER BY name_2 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
     public function testLimitSubqueryWithColumnWithSortDirectionInName(): void
     {
         $this->entityManager = $this->createTestEntityManagerWithPlatform(new MySQLPlatform());
-        $query = $this->createQuery('SELECT a FROM Doctrine\Tests\ORM\Tools\Pagination\Avatar a ORDER BY a.imageAltDesc DESC');
+
+        $query = $this->createQuery(
+            'SELECT a FROM Doctrine\Tests\ORM\Tools\Pagination\Avatar a ORDER BY a.imageAltDesc DESC',
+        );
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, imageAltDesc_4 FROM (SELECT a0_.id AS id_0, a0_.image AS image_1, a0_.imageHeight AS imageHeight_2, a0_.imageWidth AS imageWidth_3, a0_.imageAltDesc AS imageAltDesc_4, a0_.user_id AS user_id_5 FROM Avatar a0_) dctrn_result_inner ORDER BY imageAltDesc_4 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -215,7 +218,7 @@ final class LimitSubqueryOutputWalkerTest extends PaginationTestCase
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, name_2 FROM (SELECT b0_.id AS id_0, a1_.id AS id_1, a1_.name AS name_2, b0_.author_id AS author_id_3, b0_.category_id AS category_id_4 FROM BlogPost b0_ INNER JOIN Author a1_ ON b0_.author_id = a1_.id) dctrn_result_inner ORDER BY name_2 ASC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -231,7 +234,7 @@ ORDER BY b.id DESC',
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0 FROM (SELECT b0_.id AS id_0, b0_.author_id AS author_id_1, b0_.category_id AS category_id_2 FROM BlogPost b0_ WHERE ((SELECT COUNT(b1_.id) AS sclr_3 FROM BlogPost b1_) = 1)) dctrn_result_inner ORDER BY id_0 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -247,7 +250,7 @@ ORDER BY b.id DESC',
 
         self::assertSame(
             'SELECT DISTINCT id_0, MIN(sclr_1) AS dctrn_minrownum FROM (SELECT b0_.id AS id_0, ROW_NUMBER() OVER(ORDER BY b0_.id DESC) AS sclr_1, b0_.author_id AS author_id_2, b0_.category_id AS category_id_3 FROM BlogPost b0_ WHERE ((SELECT COUNT(b1_.id) AS sclr_4 FROM BlogPost b1_) = 1)) dctrn_result GROUP BY id_0 ORDER BY dctrn_minrownum ASC LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -260,12 +263,12 @@ ORDER BY b.id DESC',
 
         // now use the third one in query
         $query = $this->createQuery(
-            'SELECT b FROM Doctrine\Tests\ORM\Tools\Pagination\Banner b ORDER BY b.id DESC'
+            'SELECT b FROM Doctrine\Tests\ORM\Tools\Pagination\Banner b ORDER BY b.id DESC',
         );
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0 FROM (SELECT b0_.id AS id_0, b0_.name AS name_1 FROM Banner b0_) dctrn_result_inner ORDER BY id_0 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -289,7 +292,7 @@ ORDER BY b.id DESC',
 
         self::assertSame(
             'SELECT DISTINCT id_0 FROM (SELECT DISTINCT id_0, sclr_2 FROM (SELECT a0_.id AS id_0, a0_.name AS name_1, (SELECT MIN(m1_.title) AS sclr_3 FROM MyBlogPost m1_ WHERE m1_.author_id = a0_.id) AS sclr_2 FROM Author a0_) dctrn_result_inner ORDER BY sclr_2 DESC) dctrn_result LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -313,7 +316,7 @@ ORDER BY b.id DESC',
 
         self::assertSame(
             'SELECT DISTINCT id_0, MIN(sclr_4) AS dctrn_minrownum FROM (SELECT a0_.id AS id_0, a0_.name AS name_1, (SELECT MIN(m1_.title) AS sclr_3 FROM MyBlogPost m1_ WHERE m1_.author_id = a0_.id) AS sclr_2, ROW_NUMBER() OVER(ORDER BY (SELECT MIN(m1_.title) AS sclr_5 FROM MyBlogPost m1_ WHERE m1_.author_id = a0_.id) DESC) AS sclr_4 FROM Author a0_) dctrn_result GROUP BY id_0 ORDER BY dctrn_minrownum ASC LIMIT 20 OFFSET 10',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
@@ -337,7 +340,7 @@ ORDER BY b.id DESC',
 
         self::assertSame(
             'SELECT DISTINCT ID_0, MIN(SCLR_4) AS dctrn_minrownum FROM (SELECT a0_.id AS ID_0, a0_.name AS NAME_1, (SELECT MIN(m1_.title) AS SCLR_3 FROM MyBlogPost m1_ WHERE m1_.author_id = a0_.id) AS SCLR_2, ROW_NUMBER() OVER(ORDER BY (SELECT MIN(m1_.title) AS SCLR_5 FROM MyBlogPost m1_ WHERE m1_.author_id = a0_.id) DESC) AS SCLR_4 FROM Author a0_) dctrn_result GROUP BY ID_0 ORDER BY dctrn_minrownum ASC OFFSET 10 ROWS FETCH NEXT 20 ROWS ONLY',
-            $query->getSQL()
+            $query->getSQL(),
         );
     }
 
