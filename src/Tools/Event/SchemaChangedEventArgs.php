94db6a3ecf7d889451fd9e4f635cfbeff513a698
Benjamin Eberlei <kontakt@beberlei.de>
2024-04-27 18:53:35 +0200

    Housekeeping: phpcs

--- src/Tools/Event/SchemaChangedEventArgs.php
+++ src/Tools/Event/SchemaChangedEventArgs.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\ORM\Tools\Event;
 
 use Doctrine\Common\EventArgs;
@@ -8,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class SchemaChangedEventArgs extends EventArgs
 {
+    /** @param array<string> $sqls */
     public function __construct(
         private readonly EntityManagerInterface $em,
         private readonly Schema $schema,
