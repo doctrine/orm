83084e64269835c5709328c4839a185549910d58
Alexander M. Turek <me@derrabus.de>
2024-10-08 11:59:10 +0200

    Make nullable parameters explicit in generated entities

--- src/Tools/EntityGenerator.php
+++ src/Tools/EntityGenerator.php
@@ -767,6 +767,9 @@ public function __construct(<params>)
 
             if ($fieldMapping['type'] === 'datetime') {
                 $param = $this->getType($fieldMapping['type']) . ' ' . $param;
+                if (! empty($fieldMapping['nullable'])) {
+                    $param = '?' . $param;
+                }
             }
 
             if (! empty($fieldMapping['nullable'])) {
@@ -1385,6 +1388,9 @@ public function __construct(<params>)
         if ($typeHint && ! isset($types[$typeHint])) {
             $variableType   =  '\\' . ltrim($variableType, '\\');
             $methodTypeHint =  '\\' . $typeHint . ' ';
+            if ($defaultValue === 'null') {
+                $methodTypeHint = '?' . $methodTypeHint;
+            }
         }
 
         $replacements = [
