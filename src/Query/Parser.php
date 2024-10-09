896c65504d8bfba1cdcf58ee9a9946f171400baa
Matthias Pigulla <mp@webfactory.de>
2024-10-09 16:12:41 +0200

    Deprecate the `\Doctrine\ORM\Query\Parser::setCustomOutputTreeWalker()` method (#11641)
    We use this method only from within one of our own test cases, and I don't see how it would be useful to anybody else outside – it has to be called on the `Parser` instance which exists internally in the `Query` only.
    
    Deprecating and removing it in 3.x allows for a slight simplification in the `Parser` there, since we do no longer need the field (it can be a local variable).

--- src/Query/Parser.php
+++ src/Query/Parser.php
@@ -200,6 +200,13 @@ class Parser
      */
     public function setCustomOutputTreeWalker($className)
     {
+        Deprecation::trigger(
+            'doctrine/orm',
+            'https://github.com/doctrine/orm/pull/11641',
+            '%s is deprecated, set the output walker class with the \Doctrine\ORM\Query::HINT_CUSTOM_OUTPUT_WALKER query hint instead',
+            __METHOD__
+        );
+
         $this->customOutputWalker = $className;
     }
 
