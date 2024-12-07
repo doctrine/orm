cfc5c47014e40f53ff45089d9194d8d44d08ca20
Grégoire Paris <postmaster@greg0ire.fr>
2024-12-07 12:46:50 +0100

    Remove invalid annotation
    I do not think it achieves anything.

--- tests/Tests/Models/Enums/FaultySwitch.php
+++ tests/Tests/Models/Enums/FaultySwitch.php
@@ -14,8 +14,6 @@ class FaultySwitch
     /**
      * The following line is ignored on psalm and phpstan so that we can test
      * that the mapping is throwing an exception when a non-backed enum is used.
-     *
-     * @phpstan-suppress InvalidArgument
      */
     #[Column(enumType: SwitchStatus::class)]
     public SwitchStatus $status;
