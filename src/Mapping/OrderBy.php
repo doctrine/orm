a7c35dc5b381f6cf004ef1ce172e295365522537
Niels Keurentjes <niels.keurentjes@omines.com>
2025-01-06 14:49:22 +0100

    Fix type hint on OrderBy attribute
    PHPStan 2.0 does not accept a string backed enum as a string.

--- src/Mapping/OrderBy.php
+++ src/Mapping/OrderBy.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Doctrine\ORM\Mapping;
 
 use Attribute;
+use Doctrine\Common\Collections\Order;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 final class OrderBy implements MappingAttribute
 {
-    /** @param array<string> $value */
+    /** @param array<string, string|Order> $value */
     public function __construct(
         public readonly array $value,
     ) {
