5f6896a2f9d963167ba99533ce757399b3d8bfa0
Matteo Beccati <matteo@beccati.com>
2025-10-11 11:56:29 +0200

    Avoid using LIBRARY as table name (#12170)
    It is a reserved word since MySQL 9.2:
    https://dev.mysql.com/doc/refman/9.2/en/create-library.html

--- tests/Tests/Models/Enums/Library.php
+++ tests/Tests/Models/Enums/Library.php
@@ -11,8 +11,10 @@ use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\Table;
 
 #[Entity]
+#[Table('`library`')]
 class Library
 {
     #[Id]
