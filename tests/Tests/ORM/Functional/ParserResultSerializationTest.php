78dd074266e8b47a83bcf60ab5fe06c91a639168
Alexander M. Turek <me@derrabus.de>
2025-11-30 00:11:02 +0100

    Remove obsolete VarExporter feature detection (#12309)

--- tests/Tests/ORM/Functional/ParserResultSerializationTest.php
+++ tests/Tests/ORM/Functional/ParserResultSerializationTest.php
@@ -16,7 +16,6 @@ use Generator;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\IgnoreDeprecations;
 use ReflectionMethod;
-use Symfony\Component\VarExporter\Instantiator;
 use Symfony\Component\VarExporter\VarExporter;
 
 use function file_get_contents;
@@ -77,11 +76,6 @@ class ParserResultSerializationTest extends OrmFunctionalTestCase
                 return unserialize(serialize($parserResult));
             },
         ];
-
-        $instantiatorMethod = new ReflectionMethod(Instantiator::class, 'instantiate');
-        if ($instantiatorMethod->getReturnType() === null) {
-            self::markTestSkipped('symfony/var-exporter 5.4+ is required.');
-        }
 
         yield 'symfony/var-exporter' => [
             static function (ParserResult $parserResult): ParserResult {
