728035f0dfea8f7e7ba36336d5e9dfa17137f650
Alexander M. Turek <alexander.m.turek@easybill.de>
2025-11-29 22:52:11 +0100

    Fix more PHPStan errors

--- src/Proxy/ProxyFactory.php
+++ src/Proxy/ProxyFactory.php
@@ -162,6 +162,7 @@ EOPHP;
                 );
             }
 
+            // @phpstan-ignore function.impossibleType (This feature has been removed in Symfony 8)
             if (! method_exists(ProxyHelper::class, 'generateLazyGhost')) {
                 throw ORMInvalidArgumentException::lazyGhostUnavailable();
             }
@@ -469,7 +470,7 @@ EOPHP;
 
     private function generateUseLazyGhostTrait(ClassMetadata $class): string
     {
-        // @phpstan-ignore staticMethod.deprecated (Because we support Symfony < 7.3)
+        // @phpstan-ignore staticMethod.notFound (Because we support Symfony < 7.3)
         $code = ProxyHelper::generateLazyGhost($class->getReflectionClass());
         $code = substr($code, 7 + (int) strpos($code, "\n{"));
         $code = substr($code, 0, (int) strpos($code, "\n}"));
