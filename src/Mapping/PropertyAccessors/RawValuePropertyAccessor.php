df9290479b20603f8b038e62b1a17088137721b0
Gerhard Seidel <gs@weareindeed.com>
2026-02-16 12:01:08 +0800

    chore: remove key from constructor

--- src/Mapping/PropertyAccessors/RawValuePropertyAccessor.php
+++ src/Mapping/PropertyAccessors/RawValuePropertyAccessor.php
@@ -23,13 +23,10 @@ class RawValuePropertyAccessor implements PropertyAccessor
 {
     public static function fromReflectionProperty(ReflectionProperty $reflectionProperty): self
     {
-        $name = $reflectionProperty->getName();
-        $key  = $reflectionProperty->isPrivate() ? "\0" . ltrim($reflectionProperty->getDeclaringClass()->getName(), '\\') . "\0" . $name : ($reflectionProperty->isProtected() ? "\0*\0" . $name : $name);
-
-        return new self($reflectionProperty, $key);
+        return new self($reflectionProperty);
     }
 
-    private function __construct(private ReflectionProperty $reflectionProperty, private string $key)
+    private function __construct(private ReflectionProperty $reflectionProperty)
     {
         if (PHP_VERSION_ID < 80400) {
             throw new LogicException('This class requires PHP 8.4 or higher.');
@@ -54,7 +51,7 @@ class RawValuePropertyAccessor implements PropertyAccessor
     public function getValue(object $object): mixed
     {
         if ($this->reflectionProperty->isInitialized($object)) {
-            
+
             return $this->reflectionProperty->getRawValue($object);
         }
 
